807381b8321c74bfdb9e420d3388649d9cd3bbe6
SDKAuto <sdkautomation@microsoft.com>
2022-04-28 15:36:02 +0000

    CodeGen from PR 18675 in Azure/azure-rest-api-specs Merge 5bb21b7fd1d4c1c46536b0f9c289b7efb55963b5 into 76c134298bae467af16f3e4dcab697ab2a4dba22

--- sdk/securityinsights/azure-resourcemanager-securityinsights/src/main/java/com/azure/resourcemanager/securityinsights/models/Constant76.java
+++ sdk/securityinsights/azure-resourcemanager-securityinsights/src/main/java/com/azure/resourcemanager/securityinsights/models/Constant76.java
@@ -7,28 +7,28 @@ package com.azure.resourcemanager.securityinsights.models;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
-/** Defines values for Constant74. */
-public enum Constant74 {
+/** Defines values for Constant76. */
+public enum Constant76 {
     /** Enum value Activity. */
     ACTIVITY("Activity");
 
-    /** The actual serialized value for a Constant74 instance. */
+    /** The actual serialized value for a Constant76 instance. */
     private final String value;
 
-    Constant74(String value) {
+    Constant76(String value) {
         this.value = value;
     }
 
     /**
-     * Parses a serialized value to a Constant74 instance.
+     * Parses a serialized value to a Constant76 instance.
      *
      * @param value the serialized value to parse.
-     * @return the parsed Constant74 object, or null if unable to parse.
+     * @return the parsed Constant76 object, or null if unable to parse.
      */
     @JsonCreator
-    public static Constant74 fromString(String value) {
-        Constant74[] items = Constant74.values();
-        for (Constant74 item : items) {
+    public static Constant76 fromString(String value) {
+        Constant76[] items = Constant76.values();
+        for (Constant76 item : items) {
             if (item.toString().equalsIgnoreCase(value)) {
                 return item;
             }
