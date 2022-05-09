1614db844b877a6412f7993f63b1efe853161b35
SDKAuto <sdkautomation@microsoft.com>
2022-05-09 10:33:39 +0000

    CodeGen from PR 18647 in Azure/azure-rest-api-specs Merge 8064e85d89824ae11e294668bde2b96adc050a35 into 6315d72e6172e5233d6e085ca4d59c8e88c8593b

--- sdk/recoveryservices/azure-resourcemanager-recoveryservices/src/main/java/com/azure/resourcemanager/recoveryservices/models/VaultProperties.java
+++ sdk/recoveryservices/azure-resourcemanager-recoveryservices/src/main/java/com/azure/resourcemanager/recoveryservices/models/VaultProperties.java
@@ -5,16 +5,12 @@
 package com.azure.resourcemanager.recoveryservices.models;
 
 import com.azure.core.annotation.Fluent;
-import com.azure.core.util.logging.ClientLogger;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 /** Properties of the vault. */
 @Fluent
 public final class VaultProperties {
-    @JsonIgnore private final ClientLogger logger = new ClientLogger(VaultProperties.class);
-
     /*
      * Provisioning State.
      */
@@ -44,6 +40,36 @@ public final class VaultProperties {
      */
     @JsonProperty(value = "privateEndpointStateForSiteRecovery", access = JsonProperty.Access.WRITE_ONLY)
     private VaultPrivateEndpointState privateEndpointStateForSiteRecovery;
+
+    /*
+     * Customer Managed Key details of the resource.
+     */
+    @JsonProperty(value = "encryption")
+    private VaultPropertiesEncryption encryption;
+
+    /*
+     * Monitoring Settings of the vault
+     */
+    @JsonProperty(value = "monitoringSettings")
+    private MonitoringSettings monitoringSettings;
+
+    /*
+     * The details of the latest move operation performed on the Azure Resource
+     */
+    @JsonProperty(value = "moveDetails")
+    private VaultPropertiesMoveDetails moveDetails;
+
+    /*
+     * The State of the Resource after the move operation
+     */
+    @JsonProperty(value = "moveState", access = JsonProperty.Access.WRITE_ONLY)
+    private ResourceMoveState moveState;
+
+    /*
+     * Backup storage version
+     */
+    @JsonProperty(value = "backupStorageVersion", access = JsonProperty.Access.WRITE_ONLY)
+    private BackupStorageVersion backupStorageVersion;
 
     /**
      * Get the provisioningState property: Provisioning State.
@@ -102,6 +128,84 @@ public final class VaultProperties {
     }
 
     /**
+     * Get the encryption property: Customer Managed Key details of the resource.
+     *
+     * @return the encryption value.
+     */
+    public VaultPropertiesEncryption encryption() {
+        return this.encryption;
+    }
+
+    /**
+     * Set the encryption property: Customer Managed Key details of the resource.
+     *
+     * @param encryption the encryption value to set.
+     * @return the VaultProperties object itself.
+     */
+    public VaultProperties withEncryption(VaultPropertiesEncryption encryption) {
+        this.encryption = encryption;
+        return this;
+    }
+
+    /**
+     * Get the monitoringSettings property: Monitoring Settings of the vault.
+     *
+     * @return the monitoringSettings value.
+     */
+    public MonitoringSettings monitoringSettings() {
+        return this.monitoringSettings;
+    }
+
+    /**
+     * Set the monitoringSettings property: Monitoring Settings of the vault.
+     *
+     * @param monitoringSettings the monitoringSettings value to set.
+     * @return the VaultProperties object itself.
+     */
+    public VaultProperties withMonitoringSettings(MonitoringSettings monitoringSettings) {
+        this.monitoringSettings = monitoringSettings;
+        return this;
+    }
+
+    /**
+     * Get the moveDetails property: The details of the latest move operation performed on the Azure Resource.
+     *
+     * @return the moveDetails value.
+     */
+    public VaultPropertiesMoveDetails moveDetails() {
+        return this.moveDetails;
+    }
+
+    /**
+     * Set the moveDetails property: The details of the latest move operation performed on the Azure Resource.
+     *
+     * @param moveDetails the moveDetails value to set.
+     * @return the VaultProperties object itself.
+     */
+    public VaultProperties withMoveDetails(VaultPropertiesMoveDetails moveDetails) {
+        this.moveDetails = moveDetails;
+        return this;
+    }
+
+    /**
+     * Get the moveState property: The State of the Resource after the move operation.
+     *
+     * @return the moveState value.
+     */
+    public ResourceMoveState moveState() {
+        return this.moveState;
+    }
+
+    /**
+     * Get the backupStorageVersion property: Backup storage version.
+     *
+     * @return the backupStorageVersion value.
+     */
+    public BackupStorageVersion backupStorageVersion() {
+        return this.backupStorageVersion;
+    }
+
+    /**
      * Validates the instance.
      *
      * @throws IllegalArgumentException thrown if the instance is not valid.
@@ -112,6 +216,15 @@ public final class VaultProperties {
         }
         if (privateEndpointConnections() != null) {
             privateEndpointConnections().forEach(e -> e.validate());
+        }
+        if (encryption() != null) {
+            encryption().validate();
+        }
+        if (monitoringSettings() != null) {
+            monitoringSettings().validate();
+        }
+        if (moveDetails() != null) {
+            moveDetails().validate();
         }
     }
 }
