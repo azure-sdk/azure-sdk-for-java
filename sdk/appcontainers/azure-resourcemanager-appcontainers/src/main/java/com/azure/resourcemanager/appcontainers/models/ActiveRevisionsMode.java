053a2fe33085bfda8eeb42066b3250a47b92ab0d
SDKAuto <sdkautomation@microsoft.com>
2022-05-12 08:25:18 +0000

    CodeGen from PR 19014 in Azure/azure-rest-api-specs Merge c68c9f8916967b4339799f8349878ea3b292f039 into fabf9a52dff3cff4e61f6c6e8743c0a5f848fe8a

--- sdk/appcontainers/azure-resourcemanager-appcontainers/src/main/java/com/azure/resourcemanager/appcontainers/models/ActiveRevisionsMode.java
+++ sdk/appcontainers/azure-resourcemanager-appcontainers/src/main/java/com/azure/resourcemanager/appcontainers/models/ActiveRevisionsMode.java
@@ -10,11 +10,11 @@ import java.util.Collection;
 
 /** Defines values for ActiveRevisionsMode. */
 public final class ActiveRevisionsMode extends ExpandableStringEnum<ActiveRevisionsMode> {
-    /** Static value Multiple for ActiveRevisionsMode. */
-    public static final ActiveRevisionsMode MULTIPLE = fromString("Multiple");
+    /** Static value multiple for ActiveRevisionsMode. */
+    public static final ActiveRevisionsMode MULTIPLE = fromString("multiple");
 
-    /** Static value Single for ActiveRevisionsMode. */
-    public static final ActiveRevisionsMode SINGLE = fromString("Single");
+    /** Static value single for ActiveRevisionsMode. */
+    public static final ActiveRevisionsMode SINGLE = fromString("single");
 
     /**
      * Creates or finds a ActiveRevisionsMode from its string representation.
--- sdk/appcontainers/azure-resourcemanager-appcontainers/src/main/java/com/azure/resourcemanager/appcontainers/models/Type.java
+++ sdk/appcontainers/azure-resourcemanager-appcontainers/src/main/java/com/azure/resourcemanager/appcontainers/models/Type.java
@@ -10,14 +10,14 @@ import java.util.Collection;
 
 /** Defines values for Type. */
 public final class Type extends ExpandableStringEnum<Type> {
-    /** Static value Liveness for Type. */
-    public static final Type LIVENESS = fromString("Liveness");
+    /** Static value liveness for Type. */
+    public static final Type LIVENESS = fromString("liveness");
 
-    /** Static value Readiness for Type. */
-    public static final Type READINESS = fromString("Readiness");
+    /** Static value readiness for Type. */
+    public static final Type READINESS = fromString("readiness");
 
-    /** Static value Startup for Type. */
-    public static final Type STARTUP = fromString("Startup");
+    /** Static value startup for Type. */
+    public static final Type STARTUP = fromString("startup");
 
     /**
      * Creates or finds a Type from its string representation.
