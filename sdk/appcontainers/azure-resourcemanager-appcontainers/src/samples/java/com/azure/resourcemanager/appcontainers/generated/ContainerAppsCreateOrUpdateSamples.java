8973f5ef6641b75ca0bbaf2e43bf54d96596f83f
SDKAuto <sdkautomation@microsoft.com>
2022-07-19 16:49:19 +0000

    CodeGen from PR 19702 in Azure/azure-rest-api-specs Merge 39119efa9aaffdc97f5821dfdbf6177fb64d8e29 into 5421cc7ec7f24747f30052e00db749c0f378438d

--- sdk/appcontainers/azure-resourcemanager-appcontainers/src/samples/java/com/azure/resourcemanager/appcontainers/generated/ContainerAppsCreateOrUpdateSamples.java
+++ sdk/appcontainers/azure-resourcemanager-appcontainers/src/samples/java/com/azure/resourcemanager/appcontainers/generated/ContainerAppsCreateOrUpdateSamples.java
@@ -11,10 +11,12 @@ import com.azure.resourcemanager.appcontainers.models.Container;
 import com.azure.resourcemanager.appcontainers.models.ContainerAppProbe;
 import com.azure.resourcemanager.appcontainers.models.ContainerAppProbeHttpGet;
 import com.azure.resourcemanager.appcontainers.models.ContainerAppProbeHttpGetHttpHeadersItem;
+import com.azure.resourcemanager.appcontainers.models.ContainerResources;
 import com.azure.resourcemanager.appcontainers.models.CustomDomain;
 import com.azure.resourcemanager.appcontainers.models.CustomScaleRule;
 import com.azure.resourcemanager.appcontainers.models.Dapr;
 import com.azure.resourcemanager.appcontainers.models.Ingress;
+import com.azure.resourcemanager.appcontainers.models.InitContainer;
 import com.azure.resourcemanager.appcontainers.models.Scale;
 import com.azure.resourcemanager.appcontainers.models.ScaleRule;
 import com.azure.resourcemanager.appcontainers.models.Template;
@@ -27,7 +29,7 @@ import java.util.Map;
 /** Samples for ContainerApps CreateOrUpdate. */
 public final class ContainerAppsCreateOrUpdateSamples {
     /*
-     * x-ms-original-file: specification/app/resource-manager/Microsoft.App/stable/2022-03-01/examples/ContainerApps_CreateOrUpdate.json
+     * x-ms-original-file: specification/app/resource-manager/Microsoft.App/stable/2022-05-01/examples/ContainerApps_CreateOrUpdate.json
      */
     /**
      * Sample code: Create or Update Container App.
@@ -69,9 +71,23 @@ public final class ContainerAppsCreateOrUpdateSamples {
                                             .withBindingType(BindingType.SNI_ENABLED)
                                             .withCertificateId(
                                                 "/subscriptions/34adfa4f-cedf-4dc0-ba29-b6d1a69ab345/resourceGroups/rg/providers/Microsoft.App/managedEnvironments/demokube/certificates/my-certificate-for-my-other-name-dot-com"))))
-                    .withDapr(new Dapr().withEnabled(true).withAppProtocol(AppProtocol.HTTP).withAppPort(3000)))
+                    .withDapr(
+                        new Dapr()
+                            .withEnabled(true)
+                            .withAppProtocol(AppProtocol.HTTP)
+                            .withAppPort(3000)
+                            .withHttpReadBufferSize(30)))
             .withTemplate(
                 new Template()
+                    .withInitContainers(
+                        Arrays
+                            .asList(
+                                new InitContainer()
+                                    .withImage("repo/testcontainerApp0:v4")
+                                    .withName("testinitcontainerApp0")
+                                    .withCommand(Arrays.asList("/bin/sh"))
+                                    .withArgs(Arrays.asList("-c", "while true; do echo hello; sleep 10;done"))
+                                    .withResources(new ContainerResources().withCpu(0.2).withMemory("100Mi"))))
                     .withContainers(
                         Arrays
                             .asList(
