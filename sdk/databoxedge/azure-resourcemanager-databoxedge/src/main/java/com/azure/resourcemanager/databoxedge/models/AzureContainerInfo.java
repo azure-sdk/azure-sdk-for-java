5ba4699bc03b9b3cc2acb997ef126509e71fc8ab
SDKAuto <sdkautomation@microsoft.com>
2022-08-04 08:46:48 +0000

    CodeGen from PR 19557 in Azure/azure-rest-api-specs Merge 90655d60760e329e98a590e10ef643075976fae7 into dfc5a559bb4c0c97ba2218e0333f63d5fdf970f6

--- sdk/databoxedge/azure-resourcemanager-databoxedge/src/main/java/com/azure/resourcemanager/databoxedge/models/AzureContainerInfo.java
+++ sdk/databoxedge/azure-resourcemanager-databoxedge/src/main/java/com/azure/resourcemanager/databoxedge/models/AzureContainerInfo.java
@@ -6,14 +6,11 @@ package com.azure.resourcemanager.databoxedge.models;
 
 import com.azure.core.annotation.Fluent;
 import com.azure.core.util.logging.ClientLogger;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /** Azure container mapping of the endpoint. */
 @Fluent
 public final class AzureContainerInfo {
-    @JsonIgnore private final ClientLogger logger = new ClientLogger(AzureContainerInfo.class);
-
     /*
      * ID of the storage account credential used to access storage.
      */
@@ -21,8 +18,8 @@ public final class AzureContainerInfo {
     private String storageAccountCredentialId;
 
     /*
-     * Container name (Based on the data format specified, this represents the
-     * name of Azure Files/Page blob/Block blob).
+     * Container name (Based on the data format specified, this represents the name of Azure Files/Page blob/Block
+     * blob).
      */
     @JsonProperty(value = "containerName", required = true)
     private String containerName;
@@ -102,21 +99,23 @@ public final class AzureContainerInfo {
      */
     public void validate() {
         if (storageAccountCredentialId() == null) {
-            throw logger
+            throw LOGGER
                 .logExceptionAsError(
                     new IllegalArgumentException(
                         "Missing required property storageAccountCredentialId in model AzureContainerInfo"));
         }
         if (containerName() == null) {
-            throw logger
+            throw LOGGER
                 .logExceptionAsError(
                     new IllegalArgumentException(
                         "Missing required property containerName in model AzureContainerInfo"));
         }
         if (dataFormat() == null) {
-            throw logger
+            throw LOGGER
                 .logExceptionAsError(
                     new IllegalArgumentException("Missing required property dataFormat in model AzureContainerInfo"));
         }
     }
+
+    private static final ClientLogger LOGGER = new ClientLogger(AzureContainerInfo.class);
 }
