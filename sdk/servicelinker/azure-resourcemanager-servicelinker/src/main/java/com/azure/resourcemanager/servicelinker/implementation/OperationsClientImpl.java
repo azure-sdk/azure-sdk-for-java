1e59df14ad3d91a83a462d9d6f6badc686385db6
SDKAuto <sdkautomation@microsoft.com>
2022-08-15 06:54:13 +0000

    CodeGen from PR 458 in test-repo-billy/azure-rest-api-specs-pr Merge d45f441c6f50ca9710dd13799a6d56ef78b65f81 into 84ad5cfa2c65ea444ef87ea4fa64fb6d8f00f7ae

--- sdk/servicelinker/azure-resourcemanager-servicelinker/src/main/java/com/azure/resourcemanager/servicelinker/implementation/OperationsClientImpl.java
+++ sdk/servicelinker/azure-resourcemanager-servicelinker/src/main/java/com/azure/resourcemanager/servicelinker/implementation/OperationsClientImpl.java
@@ -36,25 +36,25 @@ public final class OperationsClientImpl implements OperationsClient {
     private final OperationsService service;
 
     /** The service client containing this operation class. */
-    private final ServiceLinkerManagementClientImpl client;
+    private final MicrosoftServiceLinkerImpl client;
 
     /**
      * Initializes an instance of OperationsClientImpl.
      *
      * @param client the instance of the service client containing this operation class.
      */
-    OperationsClientImpl(ServiceLinkerManagementClientImpl client) {
+    OperationsClientImpl(MicrosoftServiceLinkerImpl client) {
         this.service =
             RestProxy.create(OperationsService.class, client.getHttpPipeline(), client.getSerializerAdapter());
         this.client = client;
     }
 
     /**
-     * The interface defining all the services for ServiceLinkerManagementClientOperations to be used by the proxy
-     * service to perform REST calls.
+     * The interface defining all the services for MicrosoftServiceLinkerOperations to be used by the proxy service to
+     * perform REST calls.
      */
     @Host("{$host}")
-    @ServiceInterface(name = "ServiceLinkerManagem")
+    @ServiceInterface(name = "MicrosoftServiceLink")
     private interface OperationsService {
         @Headers({"Content-Type: application/json"})
         @Get("/providers/Microsoft.ServiceLinker/operations")
