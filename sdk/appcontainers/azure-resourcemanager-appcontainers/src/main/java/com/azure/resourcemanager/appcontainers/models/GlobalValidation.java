a2a7c71977bf4faedc40e9ba0c8a9859319b739e
SDKAuto <sdkautomation@microsoft.com>
2022-08-17 07:38:52 +0000

    CodeGen from PR 19670 in Azure/azure-rest-api-specs Merge 3fdf2f5670fc188e6445fadfde9edbbd8a751b13 into dd4b93e97700567f74af3786afb0ac7d0e685601

--- sdk/appcontainers/azure-resourcemanager-appcontainers/src/main/java/com/azure/resourcemanager/appcontainers/models/GlobalValidation.java
+++ sdk/appcontainers/azure-resourcemanager-appcontainers/src/main/java/com/azure/resourcemanager/appcontainers/models/GlobalValidation.java
@@ -15,25 +15,21 @@ import java.util.List;
 @Fluent
 public final class GlobalValidation {
     /*
-     * The action to take when an unauthenticated client attempts to access the
-     * app.
+     * The action to take when an unauthenticated client attempts to access the app.
      */
     @JsonProperty(value = "unauthenticatedClientAction")
     private UnauthenticatedClientActionV2 unauthenticatedClientAction;
 
     /*
-     * The default authentication provider to use when multiple providers are
-     * configured.
-     * This setting is only needed if multiple providers are configured and the
-     * unauthenticated client
+     * The default authentication provider to use when multiple providers are configured.
+     * This setting is only needed if multiple providers are configured and the unauthenticated client
      * action is set to "RedirectToLoginPage".
      */
     @JsonProperty(value = "redirectToProvider")
     private String redirectToProvider;
 
     /*
-     * The paths for which unauthenticated flow would not be redirected to the
-     * login page.
+     * The paths for which unauthenticated flow would not be redirected to the login page.
      */
     @JsonProperty(value = "excludedPaths")
     private List<String> excludedPaths;
