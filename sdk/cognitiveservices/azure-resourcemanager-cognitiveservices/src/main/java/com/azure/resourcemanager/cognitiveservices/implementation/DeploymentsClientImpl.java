5c826b0401bc0acddfe3e20786cb076e82e777f4
SDKAuto <sdkautomation@microsoft.com>
2022-08-22 05:11:08 +0000

    CodeGen from PR 2594 in test-repo-billy/azure-rest-api-specs Merge a031979d10f02c767cd844df3190a0a64b003f60 into a4bf1e830c545e3ac3e7175c70fbcf6ba6602cf1

--- sdk/cognitiveservices/azure-resourcemanager-cognitiveservices/src/main/java/com/azure/resourcemanager/cognitiveservices/implementation/DeploymentsClientImpl.java
+++ sdk/cognitiveservices/azure-resourcemanager-cognitiveservices/src/main/java/com/azure/resourcemanager/cognitiveservices/implementation/DeploymentsClientImpl.java
@@ -1042,7 +1042,8 @@ public final class DeploymentsClientImpl implements DeploymentsClient {
     /**
      * Get the next page of items.
      *
-     * @param nextLink The nextLink parameter.
+     * @param nextLink The URL to get the next list of items
+     *     <p>The nextLink parameter.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
      * @throws ManagementException thrown if the request is rejected by server.
      * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
@@ -1078,7 +1079,8 @@ public final class DeploymentsClientImpl implements DeploymentsClient {
     /**
      * Get the next page of items.
      *
-     * @param nextLink The nextLink parameter.
+     * @param nextLink The URL to get the next list of items
+     *     <p>The nextLink parameter.
      * @param context The context to associate with this operation.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
      * @throws ManagementException thrown if the request is rejected by server.
