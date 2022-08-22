77f88c266e428eae3a09d0cf6fc8761564190258
SDKAuto <sdkautomation@microsoft.com>
2022-08-22 05:32:50 +0000

    CodeGen from PR 2594 in test-repo-billy/azure-rest-api-specs Merge a031979d10f02c767cd844df3190a0a64b003f60 into a4bf1e830c545e3ac3e7175c70fbcf6ba6602cf1

--- sdk/notificationhubs/azure-resourcemanager-notificationhubs/src/main/java/com/azure/resourcemanager/notificationhubs/models/SkuName.java
+++ sdk/notificationhubs/azure-resourcemanager-notificationhubs/src/main/java/com/azure/resourcemanager/notificationhubs/models/SkuName.java
@@ -8,7 +8,7 @@ import com.azure.core.util.ExpandableStringEnum;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import java.util.Collection;
 
-/** Defines values for SkuName. */
+/** Name of the notification hub sku. */
 public final class SkuName extends ExpandableStringEnum<SkuName> {
     /** Static value Free for SkuName. */
     public static final SkuName FREE = fromString("Free");
@@ -30,7 +30,11 @@ public final class SkuName extends ExpandableStringEnum<SkuName> {
         return fromString(name, SkuName.class);
     }
 
-    /** @return known SkuName values. */
+    /**
+     * Gets known SkuName values.
+     *
+     * @return known SkuName values.
+     */
     public static Collection<SkuName> values() {
         return values(SkuName.class);
     }
