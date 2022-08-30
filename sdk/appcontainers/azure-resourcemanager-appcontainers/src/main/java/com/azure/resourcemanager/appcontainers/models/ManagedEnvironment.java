934e4ede31c1b635c2e6a0316f5c556f0c678177
SDKAuto <sdkautomation@microsoft.com>
2022-08-30 02:43:21 +0000

    CodeGen from PR 19770 in Azure/azure-rest-api-specs Merge c69da245a6367df437ca3b171b2a54fc56dd6cb7 into 68a1c9e2563c5784bae4fbdfbedf0629796f45a7

--- sdk/appcontainers/azure-resourcemanager-appcontainers/src/main/java/com/azure/resourcemanager/appcontainers/models/ManagedEnvironment.java
+++ sdk/appcontainers/azure-resourcemanager-appcontainers/src/main/java/com/azure/resourcemanager/appcontainers/models/ManagedEnvironment.java
@@ -8,6 +8,7 @@ import com.azure.core.management.Region;
 import com.azure.core.management.SystemData;
 import com.azure.core.util.Context;
 import com.azure.resourcemanager.appcontainers.fluent.models.ManagedEnvironmentInner;
+import java.util.List;
 import java.util.Map;
 
 /** An immutable client-side representation of ManagedEnvironment. */
@@ -46,6 +47,13 @@ public interface ManagedEnvironment {
      * @return the tags value.
      */
     Map<String, String> tags();
+
+    /**
+     * Gets the sku property: SKU properties of the Environment.
+     *
+     * @return the sku value.
+     */
+    EnvironmentSkuProperties sku();
 
     /**
      * Gets the systemData property: Azure Resource Manager metadata containing createdBy and modifiedBy information.
@@ -121,6 +129,20 @@ public interface ManagedEnvironment {
     Boolean zoneRedundant();
 
     /**
+     * Gets the customDomainConfiguration property: Custom domain configuration for the environment.
+     *
+     * @return the customDomainConfiguration value.
+     */
+    CustomDomainConfiguration customDomainConfiguration();
+
+    /**
+     * Gets the workloadProfiles property: Workload profiles configured for the Managed Environment.
+     *
+     * @return the workloadProfiles value.
+     */
+    List<WorkloadProfile> workloadProfiles();
+
+    /**
      * Gets the region of the resource.
      *
      * @return the region of the resource.
@@ -194,11 +216,14 @@ public interface ManagedEnvironment {
          */
         interface WithCreate
             extends DefinitionStages.WithTags,
+                DefinitionStages.WithSku,
                 DefinitionStages.WithDaprAIInstrumentationKey,
                 DefinitionStages.WithDaprAIConnectionString,
                 DefinitionStages.WithVnetConfiguration,
                 DefinitionStages.WithAppLogsConfiguration,
-                DefinitionStages.WithZoneRedundant {
+                DefinitionStages.WithZoneRedundant,
+                DefinitionStages.WithCustomDomainConfiguration,
+                DefinitionStages.WithWorkloadProfiles {
             /**
              * Executes the create request.
              *
@@ -223,6 +248,16 @@ public interface ManagedEnvironment {
              * @return the next definition stage.
              */
             WithCreate withTags(Map<String, String> tags);
+        }
+        /** The stage of the ManagedEnvironment definition allowing to specify sku. */
+        interface WithSku {
+            /**
+             * Specifies the sku property: SKU properties of the Environment..
+             *
+             * @param sku SKU properties of the Environment.
+             * @return the next definition stage.
+             */
+            WithCreate withSku(EnvironmentSkuProperties sku);
         }
         /** The stage of the ManagedEnvironment definition allowing to specify daprAIInstrumentationKey. */
         interface WithDaprAIInstrumentationKey {
@@ -280,6 +315,26 @@ public interface ManagedEnvironment {
              */
             WithCreate withZoneRedundant(Boolean zoneRedundant);
         }
+        /** The stage of the ManagedEnvironment definition allowing to specify customDomainConfiguration. */
+        interface WithCustomDomainConfiguration {
+            /**
+             * Specifies the customDomainConfiguration property: Custom domain configuration for the environment.
+             *
+             * @param customDomainConfiguration Custom domain configuration for the environment.
+             * @return the next definition stage.
+             */
+            WithCreate withCustomDomainConfiguration(CustomDomainConfiguration customDomainConfiguration);
+        }
+        /** The stage of the ManagedEnvironment definition allowing to specify workloadProfiles. */
+        interface WithWorkloadProfiles {
+            /**
+             * Specifies the workloadProfiles property: Workload profiles configured for the Managed Environment..
+             *
+             * @param workloadProfiles Workload profiles configured for the Managed Environment.
+             * @return the next definition stage.
+             */
+            WithCreate withWorkloadProfiles(List<WorkloadProfile> workloadProfiles);
+        }
     }
     /**
      * Begins update for the ManagedEnvironment resource.
@@ -290,7 +345,14 @@ public interface ManagedEnvironment {
 
     /** The template for ManagedEnvironment update. */
     interface Update
-        extends UpdateStages.WithTags, UpdateStages.WithVnetConfiguration, UpdateStages.WithAppLogsConfiguration {
+        extends UpdateStages.WithTags,
+            UpdateStages.WithSku,
+            UpdateStages.WithDaprAIInstrumentationKey,
+            UpdateStages.WithDaprAIConnectionString,
+            UpdateStages.WithVnetConfiguration,
+            UpdateStages.WithAppLogsConfiguration,
+            UpdateStages.WithCustomDomainConfiguration,
+            UpdateStages.WithWorkloadProfiles {
         /**
          * Executes the update request.
          *
@@ -318,6 +380,40 @@ public interface ManagedEnvironment {
              */
             Update withTags(Map<String, String> tags);
         }
+        /** The stage of the ManagedEnvironment update allowing to specify sku. */
+        interface WithSku {
+            /**
+             * Specifies the sku property: SKU properties of the Environment..
+             *
+             * @param sku SKU properties of the Environment.
+             * @return the next definition stage.
+             */
+            Update withSku(EnvironmentSkuProperties sku);
+        }
+        /** The stage of the ManagedEnvironment update allowing to specify daprAIInstrumentationKey. */
+        interface WithDaprAIInstrumentationKey {
+            /**
+             * Specifies the daprAIInstrumentationKey property: Azure Monitor instrumentation key used by Dapr to export
+             * Service to Service communication telemetry.
+             *
+             * @param daprAIInstrumentationKey Azure Monitor instrumentation key used by Dapr to export Service to
+             *     Service communication telemetry.
+             * @return the next definition stage.
+             */
+            Update withDaprAIInstrumentationKey(String daprAIInstrumentationKey);
+        }
+        /** The stage of the ManagedEnvironment update allowing to specify daprAIConnectionString. */
+        interface WithDaprAIConnectionString {
+            /**
+             * Specifies the daprAIConnectionString property: Application Insights connection string used by Dapr to
+             * export Service to Service communication telemetry.
+             *
+             * @param daprAIConnectionString Application Insights connection string used by Dapr to export Service to
+             *     Service communication telemetry.
+             * @return the next definition stage.
+             */
+            Update withDaprAIConnectionString(String daprAIConnectionString);
+        }
         /** The stage of the ManagedEnvironment update allowing to specify vnetConfiguration. */
         interface WithVnetConfiguration {
             /**
@@ -339,6 +435,26 @@ public interface ManagedEnvironment {
              * @return the next definition stage.
              */
             Update withAppLogsConfiguration(AppLogsConfiguration appLogsConfiguration);
+        }
+        /** The stage of the ManagedEnvironment update allowing to specify customDomainConfiguration. */
+        interface WithCustomDomainConfiguration {
+            /**
+             * Specifies the customDomainConfiguration property: Custom domain configuration for the environment.
+             *
+             * @param customDomainConfiguration Custom domain configuration for the environment.
+             * @return the next definition stage.
+             */
+            Update withCustomDomainConfiguration(CustomDomainConfiguration customDomainConfiguration);
+        }
+        /** The stage of the ManagedEnvironment update allowing to specify workloadProfiles. */
+        interface WithWorkloadProfiles {
+            /**
+             * Specifies the workloadProfiles property: Workload profiles configured for the Managed Environment..
+             *
+             * @param workloadProfiles Workload profiles configured for the Managed Environment.
+             * @return the next definition stage.
+             */
+            Update withWorkloadProfiles(List<WorkloadProfile> workloadProfiles);
         }
     }
     /**
--- sdk/appcontainers/azure-resourcemanager-appcontainers/src/samples/java/com/azure/resourcemanager/appcontainers/generated/ManagedEnvironmentsCreateOrUpdateSamples.java
+++ sdk/appcontainers/azure-resourcemanager-appcontainers/src/samples/java/com/azure/resourcemanager/appcontainers/generated/ManagedEnvironmentsCreateOrUpdateSamples.java
@@ -5,12 +5,17 @@
 package com.azure.resourcemanager.appcontainers.generated;
 
 import com.azure.resourcemanager.appcontainers.models.AppLogsConfiguration;
+import com.azure.resourcemanager.appcontainers.models.CustomDomainConfiguration;
+import com.azure.resourcemanager.appcontainers.models.EnvironmentSkuProperties;
 import com.azure.resourcemanager.appcontainers.models.LogAnalyticsConfiguration;
+import com.azure.resourcemanager.appcontainers.models.SkuName;
+import com.azure.resourcemanager.appcontainers.models.WorkloadProfile;
+import java.util.Arrays;
 
 /** Samples for ManagedEnvironments CreateOrUpdate. */
 public final class ManagedEnvironmentsCreateOrUpdateSamples {
     /*
-     * x-ms-original-file: specification/app/resource-manager/Microsoft.App/stable/2022-03-01/examples/ManagedEnvironments_CreateOrUpdate.json
+     * x-ms-original-file: specification/app/resource-manager/Microsoft.App/preview/2022-06-01-preview/examples/ManagedEnvironments_CreateOrUpdate.json
      */
     /**
      * Sample code: Create environments.
@@ -23,6 +28,7 @@ public final class ManagedEnvironmentsCreateOrUpdateSamples {
             .define("testcontainerenv")
             .withRegion("East US")
             .withExistingResourceGroup("examplerg")
+            .withSku(new EnvironmentSkuProperties().withName(SkuName.PREMIUM))
             .withDaprAIConnectionString(
                 "InstrumentationKey=00000000-0000-0000-0000-000000000000;IngestionEndpoint=https://northcentralus-0.in.applicationinsights.azure.com/")
             .withAppLogsConfiguration(
@@ -30,6 +36,26 @@ public final class ManagedEnvironmentsCreateOrUpdateSamples {
                     .withLogAnalyticsConfiguration(
                         new LogAnalyticsConfiguration().withCustomerId("string").withSharedKey("string")))
             .withZoneRedundant(true)
+            .withCustomDomainConfiguration(
+                new CustomDomainConfiguration()
+                    .withDnsSuffix("www.my-name.com")
+                    .withCertificateValue("PFX-or-PEM-blob".getBytes())
+                    .withCertificatePassword("private key password".getBytes()))
+            .withWorkloadProfiles(
+                Arrays
+                    .asList(
+                        new WorkloadProfile()
+                            .withWorkloadProfileType("GeneralPurpose")
+                            .withMinimumCount(3)
+                            .withMaximumCount(12),
+                        new WorkloadProfile()
+                            .withWorkloadProfileType("MemoryOptimized")
+                            .withMinimumCount(3)
+                            .withMaximumCount(6),
+                        new WorkloadProfile()
+                            .withWorkloadProfileType("ComputeOptimized")
+                            .withMinimumCount(3)
+                            .withMaximumCount(6)))
             .create();
     }
 }
