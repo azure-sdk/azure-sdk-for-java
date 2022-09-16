39c95fd0a0f1956dd3c29f2de9cfd0fcd2cd7dca
SDKAuto <sdkautomation@microsoft.com>
2022-09-16 07:32:46 +0000

    CodeGen from PR 20545 in Azure/azure-rest-api-specs Merge e31bf749a95eed51816c3839412f6b2714152c97 into 668dd2e3444584745f4a1a97fbceacc95ffaa665

--- sdk/synapse/azure-resourcemanager-synapse/src/main/java/com/azure/resourcemanager/synapse/implementation/AzureADOnlyAuthenticationsImpl.java
+++ sdk/synapse/azure-resourcemanager-synapse/src/main/java/com/azure/resourcemanager/synapse/implementation/AzureADOnlyAuthenticationsImpl.java
@@ -88,9 +88,8 @@ public final class AzureADOnlyAuthenticationsImpl implements AzureADOnlyAuthenti
                     new IllegalArgumentException(
                         String.format("The resource ID '%s' is not valid. Missing path segment 'workspaces'.", id)));
         }
-        AzureADOnlyAuthenticationName azureADOnlyAuthenticationName =
-            AzureADOnlyAuthenticationName.fromString(Utils.getValueFromIdByName(id, "azureADOnlyAuthentications"));
-        if (azureADOnlyAuthenticationName == null) {
+        String azureADOnlyAuthenticationNameLocal = Utils.getValueFromIdByName(id, "azureADOnlyAuthentications");
+        if (azureADOnlyAuthenticationNameLocal == null) {
             throw LOGGER
                 .logExceptionAsError(
                     new IllegalArgumentException(
@@ -99,6 +98,8 @@ public final class AzureADOnlyAuthenticationsImpl implements AzureADOnlyAuthenti
                                 "The resource ID '%s' is not valid. Missing path segment 'azureADOnlyAuthentications'.",
                                 id)));
         }
+        AzureADOnlyAuthenticationName azureADOnlyAuthenticationName =
+            AzureADOnlyAuthenticationName.fromString(azureADOnlyAuthenticationNameLocal);
         return this
             .getWithResponse(resourceGroupName, workspaceName, azureADOnlyAuthenticationName, Context.NONE)
             .getValue();
@@ -120,9 +121,8 @@ public final class AzureADOnlyAuthenticationsImpl implements AzureADOnlyAuthenti
                     new IllegalArgumentException(
                         String.format("The resource ID '%s' is not valid. Missing path segment 'workspaces'.", id)));
         }
-        AzureADOnlyAuthenticationName azureADOnlyAuthenticationName =
-            AzureADOnlyAuthenticationName.fromString(Utils.getValueFromIdByName(id, "azureADOnlyAuthentications"));
-        if (azureADOnlyAuthenticationName == null) {
+        String azureADOnlyAuthenticationNameLocal = Utils.getValueFromIdByName(id, "azureADOnlyAuthentications");
+        if (azureADOnlyAuthenticationNameLocal == null) {
             throw LOGGER
                 .logExceptionAsError(
                     new IllegalArgumentException(
@@ -131,6 +131,8 @@ public final class AzureADOnlyAuthenticationsImpl implements AzureADOnlyAuthenti
                                 "The resource ID '%s' is not valid. Missing path segment 'azureADOnlyAuthentications'.",
                                 id)));
         }
+        AzureADOnlyAuthenticationName azureADOnlyAuthenticationName =
+            AzureADOnlyAuthenticationName.fromString(azureADOnlyAuthenticationNameLocal);
         return this.getWithResponse(resourceGroupName, workspaceName, azureADOnlyAuthenticationName, context);
     }
 
