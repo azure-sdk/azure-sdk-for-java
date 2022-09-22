d82e1baed31244e9dda95c4d380d28fa1fe04174
SDKAuto <sdkautomation@microsoft.com>
2022-09-22 17:14:59 +0000

    CodeGen from PR 20505 in Azure/azure-rest-api-specs Merge 9b93db3a6c0545c4916c9270aac2f700f43899d8 into 23051523abd6e0fc850517aa6b5c6c3c42fd3251

--- sdk/applicationinsights/azure-resourcemanager-applicationinsights/src/main/java/com/azure/resourcemanager/applicationinsights/fluent/models/WorkbookTemplateProperties.java
+++ sdk/applicationinsights/azure-resourcemanager-applicationinsights/src/main/java/com/azure/resourcemanager/applicationinsights/fluent/models/WorkbookTemplateProperties.java
@@ -17,8 +17,7 @@ import java.util.Map;
 @Fluent
 public final class WorkbookTemplateProperties {
     /*
-     * Priority of the template. Determines which template to open when a
-     * workbook gallery is opened in viewer mode.
+     * Priority of the template. Determines which template to open when a workbook gallery is opened in viewer mode.
      */
     @JsonProperty(value = "priority")
     private Integer priority;
@@ -42,8 +41,7 @@ public final class WorkbookTemplateProperties {
     private List<WorkbookTemplateGallery> galleries;
 
     /*
-     * Key value pair of localized gallery. Each key is the locale code of
-     * languages supported by the Azure portal.
+     * Key value pair of localized gallery. Each key is the locale code of languages supported by the Azure portal.
      */
     @JsonProperty(value = "localized")
     @JsonInclude(value = JsonInclude.Include.NON_NULL, content = JsonInclude.Include.ALWAYS)
