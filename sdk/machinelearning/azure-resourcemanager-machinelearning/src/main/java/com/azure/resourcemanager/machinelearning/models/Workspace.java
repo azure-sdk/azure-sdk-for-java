a925e95e91a4e207dd6022f5e773e7b807cf6fa9
SDKAuto <sdkautomation@microsoft.com>
2022-09-22 18:17:22 +0000

    CodeGen from PR 20505 in Azure/azure-rest-api-specs Merge 9b93db3a6c0545c4916c9270aac2f700f43899d8 into 23051523abd6e0fc850517aa6b5c6c3c42fd3251

--- sdk/machinelearning/azure-resourcemanager-machinelearning/src/main/java/com/azure/resourcemanager/machinelearning/models/Workspace.java
+++ sdk/machinelearning/azure-resourcemanager-machinelearning/src/main/java/com/azure/resourcemanager/machinelearning/models/Workspace.java
@@ -135,7 +135,7 @@ public interface Workspace {
      *
      * @return the provisioningState value.
      */
-    ProvisioningState provisioningState();
+    WorkspaceProvisioningState provisioningState();
 
     /**
      * Gets the encryption property: The encryption settings of Azure ML workspace.
@@ -249,6 +249,28 @@ public interface Workspace {
     String mlFlowTrackingUri();
 
     /**
+     * Gets the v1LegacyMode property: Enabling v1_legacy_mode may prevent you from using features provided by the v2
+     * API.
+     *
+     * @return the v1LegacyMode value.
+     */
+    Boolean v1LegacyMode();
+
+    /**
+     * Gets the softDeletedAt property: The timestamp when the workspace was soft deleted.
+     *
+     * @return the softDeletedAt value.
+     */
+    String softDeletedAt();
+
+    /**
+     * Gets the scheduledPurgeDate property: The timestamp when the soft deleted workspace is going to be purged.
+     *
+     * @return the scheduledPurgeDate value.
+     */
+    String scheduledPurgeDate();
+
+    /**
      * Gets the region of the resource.
      *
      * @return the region of the resource.
@@ -318,7 +340,8 @@ public interface Workspace {
                 DefinitionStages.WithPublicNetworkAccess,
                 DefinitionStages.WithSharedPrivateLinkResources,
                 DefinitionStages.WithServiceManagedResourcesSettings,
-                DefinitionStages.WithPrimaryUserAssignedIdentity {
+                DefinitionStages.WithPrimaryUserAssignedIdentity,
+                DefinitionStages.WithV1LegacyMode {
             /**
              * Executes the create request.
              *
@@ -548,6 +571,17 @@ public interface Workspace {
              */
             WithCreate withPrimaryUserAssignedIdentity(String primaryUserAssignedIdentity);
         }
+        /** The stage of the Workspace definition allowing to specify v1LegacyMode. */
+        interface WithV1LegacyMode {
+            /**
+             * Specifies the v1LegacyMode property: Enabling v1_legacy_mode may prevent you from using features provided
+             * by the v2 API..
+             *
+             * @param v1LegacyMode Enabling v1_legacy_mode may prevent you from using features provided by the v2 API.
+             * @return the next definition stage.
+             */
+            WithCreate withV1LegacyMode(Boolean v1LegacyMode);
+        }
     }
     /**
      * Begins update for the Workspace resource.
@@ -568,7 +602,8 @@ public interface Workspace {
             UpdateStages.WithPrimaryUserAssignedIdentity,
             UpdateStages.WithPublicNetworkAccess,
             UpdateStages.WithApplicationInsights,
-            UpdateStages.WithContainerRegistry {
+            UpdateStages.WithContainerRegistry,
+            UpdateStages.WithEncryption {
         /**
          * Executes the update request.
          *
@@ -699,6 +734,16 @@ public interface Workspace {
              * @return the next definition stage.
              */
             Update withContainerRegistry(String containerRegistry);
+        }
+        /** The stage of the Workspace update allowing to specify encryption. */
+        interface WithEncryption {
+            /**
+             * Specifies the encryption property: The encryption settings of the workspace..
+             *
+             * @param encryption The encryption settings of the workspace.
+             * @return the next definition stage.
+             */
+            Update withEncryption(EncryptionUpdateProperties encryption);
         }
     }
     /**
