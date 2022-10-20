5e83fd3bedb020492f1af720d5e50f61b80468c8
SDKAuto <sdkautomation@microsoft.com>
2022-10-20 17:11:35 +0000

    CodeGen from PR 21176 in Azure/azure-rest-api-specs Merge 34a55e04cc3539eab6560f9b0a9bb6aa5767936c into 492cf91751be945ceae53cfdd53b1ff2fb878703

--- sdk/azurearcdata/azure-resourcemanager-azurearcdata/src/main/java/com/azure/resourcemanager/azurearcdata/fluent/SqlManagedInstancesClient.java
+++ sdk/azurearcdata/azure-resourcemanager-azurearcdata/src/main/java/com/azure/resourcemanager/azurearcdata/fluent/SqlManagedInstancesClient.java
@@ -21,7 +21,7 @@ public interface SqlManagedInstancesClient {
      *
      * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
      * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return a list of SqlManagedInstance.
+     * @return a list of SqlManagedInstance as paginated response with {@link PagedIterable}.
      */
     @ServiceMethod(returns = ReturnType.COLLECTION)
     PagedIterable<SqlManagedInstanceInner> list();
@@ -33,35 +33,54 @@ public interface SqlManagedInstancesClient {
      * @throws IllegalArgumentException thrown if parameters fail the validation.
      * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
      * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return a list of SqlManagedInstance.
+     * @return a list of SqlManagedInstance as paginated response with {@link PagedIterable}.
      */
     @ServiceMethod(returns = ReturnType.COLLECTION)
     PagedIterable<SqlManagedInstanceInner> list(Context context);
 
     /**
-     * Gets all sqlManagedInstances in a resource group.
+     * List sqlManagedInstance resources in the resource group
+     *
+     * <p>Gets all sqlManagedInstances in a resource group.
      *
      * @param resourceGroupName The name of the Azure resource group.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
      * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
      * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return all sqlManagedInstances in a resource group.
+     * @return all sqlManagedInstances in a resource group as paginated response with {@link PagedIterable}.
      */
     @ServiceMethod(returns = ReturnType.COLLECTION)
     PagedIterable<SqlManagedInstanceInner> listByResourceGroup(String resourceGroupName);
 
     /**
-     * Gets all sqlManagedInstances in a resource group.
+     * List sqlManagedInstance resources in the resource group
+     *
+     * <p>Gets all sqlManagedInstances in a resource group.
      *
      * @param resourceGroupName The name of the Azure resource group.
      * @param context The context to associate with this operation.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
      * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
      * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return all sqlManagedInstances in a resource group.
+     * @return all sqlManagedInstances in a resource group as paginated response with {@link PagedIterable}.
      */
     @ServiceMethod(returns = ReturnType.COLLECTION)
     PagedIterable<SqlManagedInstanceInner> listByResourceGroup(String resourceGroupName, Context context);
+
+    /**
+     * Retrieves a SQL Managed Instance resource.
+     *
+     * @param resourceGroupName The name of the Azure resource group.
+     * @param sqlManagedInstanceName Name of SQL Managed Instance.
+     * @param context The context to associate with this operation.
+     * @throws IllegalArgumentException thrown if parameters fail the validation.
+     * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
+     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
+     * @return a SqlManagedInstance along with {@link Response}.
+     */
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    Response<SqlManagedInstanceInner> getByResourceGroupWithResponse(
+        String resourceGroupName, String sqlManagedInstanceName, Context context);
 
     /**
      * Retrieves a SQL Managed Instance resource.
@@ -77,7 +96,88 @@ public interface SqlManagedInstancesClient {
     SqlManagedInstanceInner getByResourceGroup(String resourceGroupName, String sqlManagedInstanceName);
 
     /**
-     * Retrieves a SQL Managed Instance resource.
+     * Creates or replaces a SQL Managed Instance resource.
+     *
+     * @param resourceGroupName The name of the Azure resource group.
+     * @param sqlManagedInstanceName Name of SQL Managed Instance.
+     * @param sqlManagedInstance The SQL Managed Instance to be created or updated.
+     * @throws IllegalArgumentException thrown if parameters fail the validation.
+     * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
+     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
+     * @return the {@link SyncPoller} for polling of a SqlManagedInstance.
+     */
+    @ServiceMethod(returns = ReturnType.LONG_RUNNING_OPERATION)
+    SyncPoller<PollResult<SqlManagedInstanceInner>, SqlManagedInstanceInner> beginCreate(
+        String resourceGroupName, String sqlManagedInstanceName, SqlManagedInstanceInner sqlManagedInstance);
+
+    /**
+     * Creates or replaces a SQL Managed Instance resource.
+     *
+     * @param resourceGroupName The name of the Azure resource group.
+     * @param sqlManagedInstanceName Name of SQL Managed Instance.
+     * @param sqlManagedInstance The SQL Managed Instance to be created or updated.
+     * @param context The context to associate with this operation.
+     * @throws IllegalArgumentException thrown if parameters fail the validation.
+     * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
+     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
+     * @return the {@link SyncPoller} for polling of a SqlManagedInstance.
+     */
+    @ServiceMethod(returns = ReturnType.LONG_RUNNING_OPERATION)
+    SyncPoller<PollResult<SqlManagedInstanceInner>, SqlManagedInstanceInner> beginCreate(
+        String resourceGroupName,
+        String sqlManagedInstanceName,
+        SqlManagedInstanceInner sqlManagedInstance,
+        Context context);
+
+    /**
+     * Creates or replaces a SQL Managed Instance resource.
+     *
+     * @param resourceGroupName The name of the Azure resource group.
+     * @param sqlManagedInstanceName Name of SQL Managed Instance.
+     * @param sqlManagedInstance The SQL Managed Instance to be created or updated.
+     * @throws IllegalArgumentException thrown if parameters fail the validation.
+     * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
+     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
+     * @return a SqlManagedInstance.
+     */
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    SqlManagedInstanceInner create(
+        String resourceGroupName, String sqlManagedInstanceName, SqlManagedInstanceInner sqlManagedInstance);
+
+    /**
+     * Creates or replaces a SQL Managed Instance resource.
+     *
+     * @param resourceGroupName The name of the Azure resource group.
+     * @param sqlManagedInstanceName Name of SQL Managed Instance.
+     * @param sqlManagedInstance The SQL Managed Instance to be created or updated.
+     * @param context The context to associate with this operation.
+     * @throws IllegalArgumentException thrown if parameters fail the validation.
+     * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
+     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
+     * @return a SqlManagedInstance.
+     */
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    SqlManagedInstanceInner create(
+        String resourceGroupName,
+        String sqlManagedInstanceName,
+        SqlManagedInstanceInner sqlManagedInstance,
+        Context context);
+
+    /**
+     * Deletes a SQL Managed Instance resource.
+     *
+     * @param resourceGroupName The name of the Azure resource group.
+     * @param sqlManagedInstanceName Name of SQL Managed Instance.
+     * @throws IllegalArgumentException thrown if parameters fail the validation.
+     * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
+     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
+     * @return the {@link SyncPoller} for polling of long-running operation.
+     */
+    @ServiceMethod(returns = ReturnType.LONG_RUNNING_OPERATION)
+    SyncPoller<PollResult<Void>, Void> beginDelete(String resourceGroupName, String sqlManagedInstanceName);
+
+    /**
+     * Deletes a SQL Managed Instance resource.
      *
      * @param resourceGroupName The name of the Azure resource group.
      * @param sqlManagedInstanceName Name of SQL Managed Instance.
@@ -85,105 +185,9 @@ public interface SqlManagedInstancesClient {
      * @throws IllegalArgumentException thrown if parameters fail the validation.
      * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
      * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return a SqlManagedInstance.
+     * @return the {@link SyncPoller} for polling of long-running operation.
      */
-    @ServiceMethod(returns = ReturnType.SINGLE)
-    Response<SqlManagedInstanceInner> getByResourceGroupWithResponse(
-        String resourceGroupName, String sqlManagedInstanceName, Context context);
-
-    /**
-     * Creates or replaces a SQL Managed Instance resource.
-     *
-     * @param resourceGroupName The name of the Azure resource group.
-     * @param sqlManagedInstanceName The name of SQL Managed Instances.
-     * @param sqlManagedInstance The SQL Managed Instance to be created or updated.
-     * @throws IllegalArgumentException thrown if parameters fail the validation.
-     * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
-     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return a SqlManagedInstance.
-     */
-    @ServiceMethod(returns = ReturnType.SINGLE)
-    SyncPoller<PollResult<SqlManagedInstanceInner>, SqlManagedInstanceInner> beginCreate(
-        String resourceGroupName, String sqlManagedInstanceName, SqlManagedInstanceInner sqlManagedInstance);
-
-    /**
-     * Creates or replaces a SQL Managed Instance resource.
-     *
-     * @param resourceGroupName The name of the Azure resource group.
-     * @param sqlManagedInstanceName The name of SQL Managed Instances.
-     * @param sqlManagedInstance The SQL Managed Instance to be created or updated.
-     * @param context The context to associate with this operation.
-     * @throws IllegalArgumentException thrown if parameters fail the validation.
-     * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
-     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return a SqlManagedInstance.
-     */
-    @ServiceMethod(returns = ReturnType.SINGLE)
-    SyncPoller<PollResult<SqlManagedInstanceInner>, SqlManagedInstanceInner> beginCreate(
-        String resourceGroupName,
-        String sqlManagedInstanceName,
-        SqlManagedInstanceInner sqlManagedInstance,
-        Context context);
-
-    /**
-     * Creates or replaces a SQL Managed Instance resource.
-     *
-     * @param resourceGroupName The name of the Azure resource group.
-     * @param sqlManagedInstanceName The name of SQL Managed Instances.
-     * @param sqlManagedInstance The SQL Managed Instance to be created or updated.
-     * @throws IllegalArgumentException thrown if parameters fail the validation.
-     * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
-     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return a SqlManagedInstance.
-     */
-    @ServiceMethod(returns = ReturnType.SINGLE)
-    SqlManagedInstanceInner create(
-        String resourceGroupName, String sqlManagedInstanceName, SqlManagedInstanceInner sqlManagedInstance);
-
-    /**
-     * Creates or replaces a SQL Managed Instance resource.
-     *
-     * @param resourceGroupName The name of the Azure resource group.
-     * @param sqlManagedInstanceName The name of SQL Managed Instances.
-     * @param sqlManagedInstance The SQL Managed Instance to be created or updated.
-     * @param context The context to associate with this operation.
-     * @throws IllegalArgumentException thrown if parameters fail the validation.
-     * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
-     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return a SqlManagedInstance.
-     */
-    @ServiceMethod(returns = ReturnType.SINGLE)
-    SqlManagedInstanceInner create(
-        String resourceGroupName,
-        String sqlManagedInstanceName,
-        SqlManagedInstanceInner sqlManagedInstance,
-        Context context);
-
-    /**
-     * Deletes a SQL Managed Instance resource.
-     *
-     * @param resourceGroupName The name of the Azure resource group.
-     * @param sqlManagedInstanceName The name of Sql Managed Instances.
-     * @throws IllegalArgumentException thrown if parameters fail the validation.
-     * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
-     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return the completion.
-     */
-    @ServiceMethod(returns = ReturnType.SINGLE)
-    SyncPoller<PollResult<Void>, Void> beginDelete(String resourceGroupName, String sqlManagedInstanceName);
-
-    /**
-     * Deletes a SQL Managed Instance resource.
-     *
-     * @param resourceGroupName The name of the Azure resource group.
-     * @param sqlManagedInstanceName The name of Sql Managed Instances.
-     * @param context The context to associate with this operation.
-     * @throws IllegalArgumentException thrown if parameters fail the validation.
-     * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
-     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return the completion.
-     */
-    @ServiceMethod(returns = ReturnType.SINGLE)
+    @ServiceMethod(returns = ReturnType.LONG_RUNNING_OPERATION)
     SyncPoller<PollResult<Void>, Void> beginDelete(
         String resourceGroupName, String sqlManagedInstanceName, Context context);
 
@@ -191,7 +195,7 @@ public interface SqlManagedInstancesClient {
      * Deletes a SQL Managed Instance resource.
      *
      * @param resourceGroupName The name of the Azure resource group.
-     * @param sqlManagedInstanceName The name of Sql Managed Instances.
+     * @param sqlManagedInstanceName Name of SQL Managed Instance.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
      * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
      * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
@@ -203,7 +207,7 @@ public interface SqlManagedInstancesClient {
      * Deletes a SQL Managed Instance resource.
      *
      * @param resourceGroupName The name of the Azure resource group.
-     * @param sqlManagedInstanceName The name of Sql Managed Instances.
+     * @param sqlManagedInstanceName Name of SQL Managed Instance.
      * @param context The context to associate with this operation.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
      * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
@@ -216,7 +220,23 @@ public interface SqlManagedInstancesClient {
      * Updates a SQL Managed Instance resource.
      *
      * @param resourceGroupName The name of the Azure resource group.
-     * @param sqlManagedInstanceName Name of sqlManagedInstance.
+     * @param sqlManagedInstanceName Name of SQL Managed Instance.
+     * @param parameters The SQL Managed Instance.
+     * @param context The context to associate with this operation.
+     * @throws IllegalArgumentException thrown if parameters fail the validation.
+     * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
+     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
+     * @return a SqlManagedInstance along with {@link Response}.
+     */
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    Response<SqlManagedInstanceInner> updateWithResponse(
+        String resourceGroupName, String sqlManagedInstanceName, SqlManagedInstanceUpdate parameters, Context context);
+
+    /**
+     * Updates a SQL Managed Instance resource.
+     *
+     * @param resourceGroupName The name of the Azure resource group.
+     * @param sqlManagedInstanceName Name of SQL Managed Instance.
      * @param parameters The SQL Managed Instance.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
      * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
@@ -226,20 +246,4 @@ public interface SqlManagedInstancesClient {
     @ServiceMethod(returns = ReturnType.SINGLE)
     SqlManagedInstanceInner update(
         String resourceGroupName, String sqlManagedInstanceName, SqlManagedInstanceUpdate parameters);
-
-    /**
-     * Updates a SQL Managed Instance resource.
-     *
-     * @param resourceGroupName The name of the Azure resource group.
-     * @param sqlManagedInstanceName Name of sqlManagedInstance.
-     * @param parameters The SQL Managed Instance.
-     * @param context The context to associate with this operation.
-     * @throws IllegalArgumentException thrown if parameters fail the validation.
-     * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
-     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return a SqlManagedInstance.
-     */
-    @ServiceMethod(returns = ReturnType.SINGLE)
-    Response<SqlManagedInstanceInner> updateWithResponse(
-        String resourceGroupName, String sqlManagedInstanceName, SqlManagedInstanceUpdate parameters, Context context);
 }
