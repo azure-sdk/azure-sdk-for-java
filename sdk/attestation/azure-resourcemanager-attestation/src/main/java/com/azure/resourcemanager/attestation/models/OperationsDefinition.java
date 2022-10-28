8366917af6c60ff8013591254a8d52cabc92756b
SDKAuto <sdkautomation@microsoft.com>
2022-10-28 17:53:53 +0000

    CodeGen from PR 20703 in Azure/azure-rest-api-specs Merge e631f2651fbd2493c0b08b07ef36fdc7814ce763 into fffa955e35ff4a3a2bc97252325c615f55c23e18

--- sdk/attestation/azure-resourcemanager-attestation/src/main/java/com/azure/resourcemanager/attestation/models/OperationsDefinition.java
+++ sdk/attestation/azure-resourcemanager-attestation/src/main/java/com/azure/resourcemanager/attestation/models/OperationsDefinition.java
@@ -5,15 +5,11 @@
 package com.azure.resourcemanager.attestation.models;
 
 import com.azure.core.annotation.Fluent;
-import com.azure.core.util.logging.ClientLogger;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /** Definition object with the name and properties of an operation. */
 @Fluent
 public final class OperationsDefinition {
-    @JsonIgnore private final ClientLogger logger = new ClientLogger(OperationsDefinition.class);
-
     /*
      * Name of the operation.
      */
@@ -21,10 +17,32 @@ public final class OperationsDefinition {
     private String name;
 
     /*
+     * Indicates whether the operation is a data action
+     */
+    @JsonProperty(value = "isDataAction")
+    private Boolean isDataAction;
+
+    /*
      * Display object with properties of the operation.
      */
     @JsonProperty(value = "display")
     private OperationsDisplayDefinition display;
+
+    /*
+     * Origin of the operation
+     */
+    @JsonProperty(value = "origin")
+    private String origin;
+
+    /*
+     * Properties of the operation
+     */
+    @JsonProperty(value = "properties")
+    private OperationProperties properties;
+
+    /** Creates an instance of OperationsDefinition class. */
+    public OperationsDefinition() {
+    }
 
     /**
      * Get the name property: Name of the operation.
@@ -43,6 +61,26 @@ public final class OperationsDefinition {
      */
     public OperationsDefinition withName(String name) {
         this.name = name;
+        return this;
+    }
+
+    /**
+     * Get the isDataAction property: Indicates whether the operation is a data action.
+     *
+     * @return the isDataAction value.
+     */
+    public Boolean isDataAction() {
+        return this.isDataAction;
+    }
+
+    /**
+     * Set the isDataAction property: Indicates whether the operation is a data action.
+     *
+     * @param isDataAction the isDataAction value to set.
+     * @return the OperationsDefinition object itself.
+     */
+    public OperationsDefinition withIsDataAction(Boolean isDataAction) {
+        this.isDataAction = isDataAction;
         return this;
     }
 
@@ -67,6 +105,46 @@ public final class OperationsDefinition {
     }
 
     /**
+     * Get the origin property: Origin of the operation.
+     *
+     * @return the origin value.
+     */
+    public String origin() {
+        return this.origin;
+    }
+
+    /**
+     * Set the origin property: Origin of the operation.
+     *
+     * @param origin the origin value to set.
+     * @return the OperationsDefinition object itself.
+     */
+    public OperationsDefinition withOrigin(String origin) {
+        this.origin = origin;
+        return this;
+    }
+
+    /**
+     * Get the properties property: Properties of the operation.
+     *
+     * @return the properties value.
+     */
+    public OperationProperties properties() {
+        return this.properties;
+    }
+
+    /**
+     * Set the properties property: Properties of the operation.
+     *
+     * @param properties the properties value to set.
+     * @return the OperationsDefinition object itself.
+     */
+    public OperationsDefinition withProperties(OperationProperties properties) {
+        this.properties = properties;
+        return this;
+    }
+
+    /**
      * Validates the instance.
      *
      * @throws IllegalArgumentException thrown if the instance is not valid.
@@ -74,6 +152,9 @@ public final class OperationsDefinition {
     public void validate() {
         if (display() != null) {
             display().validate();
+        }
+        if (properties() != null) {
+            properties().validate();
         }
     }
 }
