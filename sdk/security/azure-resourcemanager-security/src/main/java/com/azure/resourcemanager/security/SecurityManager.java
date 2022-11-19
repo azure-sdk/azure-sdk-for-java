15f3f81f989db73fcf59a873e1c05fed1d24d2ef
SDKAuto <sdkautomation@microsoft.com>
2022-11-19 18:36:30 +0000

    CodeGen from PR 19565 in Azure/azure-rest-api-specs Merge 49f96e149a6c525ca0b8e188cdd041d43036b8e9 into be6cd9ccfcb6ba08c1c206627026eabfbff31fc1

--- sdk/security/azure-resourcemanager-security/src/main/java/com/azure/resourcemanager/security/SecurityManager.java
+++ sdk/security/azure-resourcemanager-security/src/main/java/com/azure/resourcemanager/security/SecurityManager.java
@@ -451,7 +451,7 @@ public final class SecurityManager {
                 .append("-")
                 .append("com.azure.resourcemanager.security")
                 .append("/")
-                .append("1.0.0-beta.3");
+                .append("1.0.0-beta.1");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder
                     .append(" (")
