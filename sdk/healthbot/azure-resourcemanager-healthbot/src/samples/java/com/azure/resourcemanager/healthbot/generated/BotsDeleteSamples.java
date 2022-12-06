7c948a6a6db6b6d49a7088463240abbfbd038fa7
SDKAuto <sdkautomation@microsoft.com>
2022-12-06 03:14:08 +0000

    CodeGen from PR 21767 in Azure/azure-rest-api-specs Merge fcebfb98e4a73e2fff61801ffa6c911d5518c908 into 700edefeae3fa585ef5ff6fc509a3b916784d6a1

--- sdk/healthbot/azure-resourcemanager-healthbot/src/samples/java/com/azure/resourcemanager/healthbot/generated/BotsDeleteSamples.java
+++ sdk/healthbot/azure-resourcemanager-healthbot/src/samples/java/com/azure/resourcemanager/healthbot/generated/BotsDeleteSamples.java
@@ -9,7 +9,7 @@ import com.azure.core.util.Context;
 /** Samples for Bots Delete. */
 public final class BotsDeleteSamples {
     /*
-     * x-ms-original-file: specification/healthbot/resource-manager/Microsoft.HealthBot/stable/2020-12-08/examples/ResourceDeletionDelete.json
+     * x-ms-original-file: specification/healthbot/resource-manager/Microsoft.HealthBot/stable/2022-08-08/examples/ResourceDeletionDelete.json
      */
     /**
      * Sample code: BotDelete.
