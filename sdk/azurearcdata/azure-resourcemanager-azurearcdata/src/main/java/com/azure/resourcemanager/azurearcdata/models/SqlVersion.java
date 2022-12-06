c04c312da1743206533c0d49648d9f9912ec4225
SDKAuto <sdkautomation@microsoft.com>
2022-12-06 02:18:37 +0000

    CodeGen from PR 21767 in Azure/azure-rest-api-specs Merge fcebfb98e4a73e2fff61801ffa6c911d5518c908 into 700edefeae3fa585ef5ff6fc509a3b916784d6a1

--- sdk/azurearcdata/azure-resourcemanager-azurearcdata/src/main/java/com/azure/resourcemanager/azurearcdata/models/SqlVersion.java
+++ sdk/azurearcdata/azure-resourcemanager-azurearcdata/src/main/java/com/azure/resourcemanager/azurearcdata/models/SqlVersion.java
@@ -8,16 +8,28 @@ import com.azure.core.util.ExpandableStringEnum;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import java.util.Collection;
 
-/** Defines values for SqlVersion. */
+/** SQL Server version. */
 public final class SqlVersion extends ExpandableStringEnum<SqlVersion> {
-    /** Static value SQL Server 2019 for SqlVersion. */
-    public static final SqlVersion SQL_SERVER_2019 = fromString("SQL Server 2019");
+    /** Static value SQL Server 2012 for SqlVersion. */
+    public static final SqlVersion SQL_SERVER_2012 = fromString("SQL Server 2012");
+
+    /** Static value SQL Server 2014 for SqlVersion. */
+    public static final SqlVersion SQL_SERVER_2014 = fromString("SQL Server 2014");
+
+    /** Static value SQL Server 2016 for SqlVersion. */
+    public static final SqlVersion SQL_SERVER_2016 = fromString("SQL Server 2016");
 
     /** Static value SQL Server 2017 for SqlVersion. */
     public static final SqlVersion SQL_SERVER_2017 = fromString("SQL Server 2017");
 
-    /** Static value SQL Server 2016 for SqlVersion. */
-    public static final SqlVersion SQL_SERVER_2016 = fromString("SQL Server 2016");
+    /** Static value SQL Server 2019 for SqlVersion. */
+    public static final SqlVersion SQL_SERVER_2019 = fromString("SQL Server 2019");
+
+    /** Static value SQL Server 2022 for SqlVersion. */
+    public static final SqlVersion SQL_SERVER_2022 = fromString("SQL Server 2022");
+
+    /** Static value Unknown for SqlVersion. */
+    public static final SqlVersion UNKNOWN = fromString("Unknown");
 
     /**
      * Creates or finds a SqlVersion from its string representation.
@@ -30,7 +42,11 @@ public final class SqlVersion extends ExpandableStringEnum<SqlVersion> {
         return fromString(name, SqlVersion.class);
     }
 
-    /** @return known SqlVersion values. */
+    /**
+     * Gets known SqlVersion values.
+     *
+     * @return known SqlVersion values.
+     */
     public static Collection<SqlVersion> values() {
         return values(SqlVersion.class);
     }
