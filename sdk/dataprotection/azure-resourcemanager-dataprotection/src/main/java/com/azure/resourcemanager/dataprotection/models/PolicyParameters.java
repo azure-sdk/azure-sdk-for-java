a1059d282236b1a84a16519899de280e86ecb4b2
SDKAuto <sdkautomation@microsoft.com>
2022-12-12 09:53:47 +0000

    CodeGen from PR 21858 in Azure/azure-rest-api-specs Merge 0d10b6a67a54ff3d864b425a72ca5f2648ae80bc into f5ca8a06d758442c068322857278c7a4ce9a8ca6

--- sdk/dataprotection/azure-resourcemanager-dataprotection/src/main/java/com/azure/resourcemanager/dataprotection/models/PolicyParameters.java
+++ sdk/dataprotection/azure-resourcemanager-dataprotection/src/main/java/com/azure/resourcemanager/dataprotection/models/PolicyParameters.java
@@ -5,21 +5,21 @@
 package com.azure.resourcemanager.dataprotection.models;
 
 import com.azure.core.annotation.Fluent;
-import com.azure.core.util.logging.ClientLogger;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 /** Parameters in Policy. */
 @Fluent
 public final class PolicyParameters {
-    @JsonIgnore private final ClientLogger logger = new ClientLogger(PolicyParameters.class);
-
     /*
      * Gets or sets the DataStore Parameters
      */
     @JsonProperty(value = "dataStoreParametersList")
     private List<DataStoreParameters> dataStoreParametersList;
+
+    /** Creates an instance of PolicyParameters class. */
+    public PolicyParameters() {
+    }
 
     /**
      * Get the dataStoreParametersList property: Gets or sets the DataStore Parameters.
