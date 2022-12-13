795fb0daed54440aef53e132fb96c9c3dd9f6f61
SDKAuto <sdkautomation@microsoft.com>
2022-12-13 23:31:44 +0000

    CodeGen from PR 21700 in Azure/azure-rest-api-specs Merge eb15e3927f5265802c4e5152e893fd0860c015a2 into a1ac491c7a753e63710b5cf38d8c10211a5ae4e6

--- sdk/desktopvirtualization/azure-resourcemanager-desktopvirtualization/src/main/java/com/azure/resourcemanager/desktopvirtualization/fluent/ApplicationsClient.java
+++ sdk/desktopvirtualization/azure-resourcemanager-desktopvirtualization/src/main/java/com/azure/resourcemanager/desktopvirtualization/fluent/ApplicationsClient.java
@@ -6,59 +6,14 @@ package com.azure.resourcemanager.desktopvirtualization.fluent;
 
 import com.azure.core.annotation.ReturnType;
 import com.azure.core.annotation.ServiceMethod;
-import com.azure.core.http.rest.PagedFlux;
 import com.azure.core.http.rest.PagedIterable;
 import com.azure.core.http.rest.Response;
 import com.azure.core.util.Context;
 import com.azure.resourcemanager.desktopvirtualization.fluent.models.ApplicationInner;
 import com.azure.resourcemanager.desktopvirtualization.models.ApplicationPatch;
-import reactor.core.publisher.Mono;
 
 /** An instance of this class provides access to all the operations defined in ApplicationsClient. */
 public interface ApplicationsClient {
-    /**
-     * Get an application.
-     *
-     * @param resourceGroupName The name of the resource group. The name is case insensitive.
-     * @param applicationGroupName The name of the application group.
-     * @param applicationName The name of the application within the specified application group.
-     * @throws IllegalArgumentException thrown if parameters fail the validation.
-     * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
-     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return an application along with {@link Response} on successful completion of {@link Mono}.
-     */
-    @ServiceMethod(returns = ReturnType.SINGLE)
-    Mono<Response<ApplicationInner>> getWithResponseAsync(
-        String resourceGroupName, String applicationGroupName, String applicationName);
-
-    /**
-     * Get an application.
-     *
-     * @param resourceGroupName The name of the resource group. The name is case insensitive.
-     * @param applicationGroupName The name of the application group.
-     * @param applicationName The name of the application within the specified application group.
-     * @throws IllegalArgumentException thrown if parameters fail the validation.
-     * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
-     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return an application on successful completion of {@link Mono}.
-     */
-    @ServiceMethod(returns = ReturnType.SINGLE)
-    Mono<ApplicationInner> getAsync(String resourceGroupName, String applicationGroupName, String applicationName);
-
-    /**
-     * Get an application.
-     *
-     * @param resourceGroupName The name of the resource group. The name is case insensitive.
-     * @param applicationGroupName The name of the application group.
-     * @param applicationName The name of the application within the specified application group.
-     * @throws IllegalArgumentException thrown if parameters fail the validation.
-     * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
-     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return an application.
-     */
-    @ServiceMethod(returns = ReturnType.SINGLE)
-    ApplicationInner get(String resourceGroupName, String applicationGroupName, String applicationName);
-
     /**
      * Get an application.
      *
@@ -76,52 +31,18 @@ public interface ApplicationsClient {
         String resourceGroupName, String applicationGroupName, String applicationName, Context context);
 
     /**
-     * Create or update an application.
+     * Get an application.
      *
      * @param resourceGroupName The name of the resource group. The name is case insensitive.
      * @param applicationGroupName The name of the application group.
      * @param applicationName The name of the application within the specified application group.
-     * @param application Object containing Application definitions.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
      * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
      * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return schema for Application properties along with {@link Response} on successful completion of {@link Mono}.
+     * @return an application.
      */
     @ServiceMethod(returns = ReturnType.SINGLE)
-    Mono<Response<ApplicationInner>> createOrUpdateWithResponseAsync(
-        String resourceGroupName, String applicationGroupName, String applicationName, ApplicationInner application);
-
-    /**
-     * Create or update an application.
-     *
-     * @param resourceGroupName The name of the resource group. The name is case insensitive.
-     * @param applicationGroupName The name of the application group.
-     * @param applicationName The name of the application within the specified application group.
-     * @param application Object containing Application definitions.
-     * @throws IllegalArgumentException thrown if parameters fail the validation.
-     * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
-     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return schema for Application properties on successful completion of {@link Mono}.
-     */
-    @ServiceMethod(returns = ReturnType.SINGLE)
-    Mono<ApplicationInner> createOrUpdateAsync(
-        String resourceGroupName, String applicationGroupName, String applicationName, ApplicationInner application);
-
-    /**
-     * Create or update an application.
-     *
-     * @param resourceGroupName The name of the resource group. The name is case insensitive.
-     * @param applicationGroupName The name of the application group.
-     * @param applicationName The name of the application within the specified application group.
-     * @param application Object containing Application definitions.
-     * @throws IllegalArgumentException thrown if parameters fail the validation.
-     * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
-     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return schema for Application properties.
-     */
-    @ServiceMethod(returns = ReturnType.SINGLE)
-    ApplicationInner createOrUpdate(
-        String resourceGroupName, String applicationGroupName, String applicationName, ApplicationInner application);
+    ApplicationInner get(String resourceGroupName, String applicationGroupName, String applicationName);
 
     /**
      * Create or update an application.
@@ -145,46 +66,20 @@ public interface ApplicationsClient {
         Context context);
 
     /**
-     * Remove an application.
+     * Create or update an application.
      *
      * @param resourceGroupName The name of the resource group. The name is case insensitive.
      * @param applicationGroupName The name of the application group.
      * @param applicationName The name of the application within the specified application group.
+     * @param application Object containing Application definitions.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
      * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
      * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return the {@link Response} on successful completion of {@link Mono}.
+     * @return schema for Application properties.
      */
     @ServiceMethod(returns = ReturnType.SINGLE)
-    Mono<Response<Void>> deleteWithResponseAsync(
-        String resourceGroupName, String applicationGroupName, String applicationName);
-
-    /**
-     * Remove an application.
-     *
-     * @param resourceGroupName The name of the resource group. The name is case insensitive.
-     * @param applicationGroupName The name of the application group.
-     * @param applicationName The name of the application within the specified application group.
-     * @throws IllegalArgumentException thrown if parameters fail the validation.
-     * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
-     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return A {@link Mono} that completes when a successful response is received.
-     */
-    @ServiceMethod(returns = ReturnType.SINGLE)
-    Mono<Void> deleteAsync(String resourceGroupName, String applicationGroupName, String applicationName);
-
-    /**
-     * Remove an application.
-     *
-     * @param resourceGroupName The name of the resource group. The name is case insensitive.
-     * @param applicationGroupName The name of the application group.
-     * @param applicationName The name of the application within the specified application group.
-     * @throws IllegalArgumentException thrown if parameters fail the validation.
-     * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
-     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     */
-    @ServiceMethod(returns = ReturnType.SINGLE)
-    void delete(String resourceGroupName, String applicationGroupName, String applicationName);
+    ApplicationInner createOrUpdate(
+        String resourceGroupName, String applicationGroupName, String applicationName, ApplicationInner application);
 
     /**
      * Remove an application.
@@ -203,39 +98,7 @@ public interface ApplicationsClient {
         String resourceGroupName, String applicationGroupName, String applicationName, Context context);
 
     /**
-     * Update an application.
-     *
-     * @param resourceGroupName The name of the resource group. The name is case insensitive.
-     * @param applicationGroupName The name of the application group.
-     * @param applicationName The name of the application within the specified application group.
-     * @param application Object containing Application definitions.
-     * @throws IllegalArgumentException thrown if parameters fail the validation.
-     * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
-     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return schema for Application properties along with {@link Response} on successful completion of {@link Mono}.
-     */
-    @ServiceMethod(returns = ReturnType.SINGLE)
-    Mono<Response<ApplicationInner>> updateWithResponseAsync(
-        String resourceGroupName, String applicationGroupName, String applicationName, ApplicationPatch application);
-
-    /**
-     * Update an application.
-     *
-     * @param resourceGroupName The name of the resource group. The name is case insensitive.
-     * @param applicationGroupName The name of the application group.
-     * @param applicationName The name of the application within the specified application group.
-     * @param application Object containing Application definitions.
-     * @throws IllegalArgumentException thrown if parameters fail the validation.
-     * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
-     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return schema for Application properties on successful completion of {@link Mono}.
-     */
-    @ServiceMethod(returns = ReturnType.SINGLE)
-    Mono<ApplicationInner> updateAsync(
-        String resourceGroupName, String applicationGroupName, String applicationName, ApplicationPatch application);
-
-    /**
-     * Update an application.
+     * Remove an application.
      *
      * @param resourceGroupName The name of the resource group. The name is case insensitive.
      * @param applicationGroupName The name of the application group.
@@ -243,24 +106,9 @@ public interface ApplicationsClient {
      * @throws IllegalArgumentException thrown if parameters fail the validation.
      * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
      * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return schema for Application properties on successful completion of {@link Mono}.
      */
     @ServiceMethod(returns = ReturnType.SINGLE)
-    Mono<ApplicationInner> updateAsync(String resourceGroupName, String applicationGroupName, String applicationName);
-
-    /**
-     * Update an application.
-     *
-     * @param resourceGroupName The name of the resource group. The name is case insensitive.
-     * @param applicationGroupName The name of the application group.
-     * @param applicationName The name of the application within the specified application group.
-     * @throws IllegalArgumentException thrown if parameters fail the validation.
-     * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
-     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return schema for Application properties.
-     */
-    @ServiceMethod(returns = ReturnType.SINGLE)
-    ApplicationInner update(String resourceGroupName, String applicationGroupName, String applicationName);
+    void delete(String resourceGroupName, String applicationGroupName, String applicationName);
 
     /**
      * Update an application.
@@ -284,17 +132,18 @@ public interface ApplicationsClient {
         Context context);
 
     /**
-     * List applications.
+     * Update an application.
      *
      * @param resourceGroupName The name of the resource group. The name is case insensitive.
      * @param applicationGroupName The name of the application group.
+     * @param applicationName The name of the application within the specified application group.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
      * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
      * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return applicationList as paginated response with {@link PagedFlux}.
+     * @return schema for Application properties.
      */
-    @ServiceMethod(returns = ReturnType.COLLECTION)
-    PagedFlux<ApplicationInner> listAsync(String resourceGroupName, String applicationGroupName);
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    ApplicationInner update(String resourceGroupName, String applicationGroupName, String applicationName);
 
     /**
      * List applications.
@@ -314,6 +163,9 @@ public interface ApplicationsClient {
      *
      * @param resourceGroupName The name of the resource group. The name is case insensitive.
      * @param applicationGroupName The name of the application group.
+     * @param pageSize Number of items per page.
+     * @param isDescending Indicates whether the collection is descending.
+     * @param initialSkip Initial number of items to skip.
      * @param context The context to associate with this operation.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
      * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
@@ -321,5 +173,11 @@ public interface ApplicationsClient {
      * @return applicationList as paginated response with {@link PagedIterable}.
      */
     @ServiceMethod(returns = ReturnType.COLLECTION)
-    PagedIterable<ApplicationInner> list(String resourceGroupName, String applicationGroupName, Context context);
+    PagedIterable<ApplicationInner> list(
+        String resourceGroupName,
+        String applicationGroupName,
+        Integer pageSize,
+        Boolean isDescending,
+        Integer initialSkip,
+        Context context);
 }
