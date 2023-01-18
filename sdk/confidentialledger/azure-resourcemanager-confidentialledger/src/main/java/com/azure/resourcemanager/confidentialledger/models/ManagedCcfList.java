20a71307831d839533eadd44c7445e20ac8fd4b0
SDKAuto <sdkautomation@microsoft.com>
2023-01-18 23:32:53 +0000

    CodeGen from PR 22233 in Azure/azure-rest-api-specs Merge cd6ac41015edb561859fef0cdf862c98d8a0a376 into 3751704f5318f1175875c94b66af769db917f2d3

--- sdk/confidentialledger/azure-resourcemanager-confidentialledger/src/main/java/com/azure/resourcemanager/confidentialledger/models/ManagedCcfList.java
+++ sdk/confidentialledger/azure-resourcemanager-confidentialledger/src/main/java/com/azure/resourcemanager/confidentialledger/models/ManagedCcfList.java
@@ -5,18 +5,18 @@
 package com.azure.resourcemanager.confidentialledger.models;
 
 import com.azure.core.annotation.Fluent;
-import com.azure.resourcemanager.confidentialledger.fluent.models.ConfidentialLedgerInner;
+import com.azure.resourcemanager.confidentialledger.fluent.models.ManagedCcfInner;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
-/** Object that includes an array of Confidential Ledgers and a possible link for next set. */
+/** Object that includes an array of Managed CCF and a possible link for next set. */
 @Fluent
-public final class ConfidentialLedgerList {
+public final class ManagedCcfList {
     /*
-     * List of Confidential Ledgers
+     * List of Managed CCF
      */
     @JsonProperty(value = "value")
-    private List<ConfidentialLedgerInner> value;
+    private List<ManagedCcfInner> value;
 
     /*
      * The URL the client should use to fetch the next page (per server side paging).
@@ -24,26 +24,26 @@ public final class ConfidentialLedgerList {
     @JsonProperty(value = "nextLink")
     private String nextLink;
 
-    /** Creates an instance of ConfidentialLedgerList class. */
-    public ConfidentialLedgerList() {
+    /** Creates an instance of ManagedCcfList class. */
+    public ManagedCcfList() {
     }
 
     /**
-     * Get the value property: List of Confidential Ledgers.
+     * Get the value property: List of Managed CCF.
      *
      * @return the value value.
      */
-    public List<ConfidentialLedgerInner> value() {
+    public List<ManagedCcfInner> value() {
         return this.value;
     }
 
     /**
-     * Set the value property: List of Confidential Ledgers.
+     * Set the value property: List of Managed CCF.
      *
      * @param value the value value to set.
-     * @return the ConfidentialLedgerList object itself.
+     * @return the ManagedCcfList object itself.
      */
-    public ConfidentialLedgerList withValue(List<ConfidentialLedgerInner> value) {
+    public ManagedCcfList withValue(List<ManagedCcfInner> value) {
         this.value = value;
         return this;
     }
@@ -61,9 +61,9 @@ public final class ConfidentialLedgerList {
      * Set the nextLink property: The URL the client should use to fetch the next page (per server side paging).
      *
      * @param nextLink the nextLink value to set.
-     * @return the ConfidentialLedgerList object itself.
+     * @return the ManagedCcfList object itself.
      */
-    public ConfidentialLedgerList withNextLink(String nextLink) {
+    public ManagedCcfList withNextLink(String nextLink) {
         this.nextLink = nextLink;
         return this;
     }
