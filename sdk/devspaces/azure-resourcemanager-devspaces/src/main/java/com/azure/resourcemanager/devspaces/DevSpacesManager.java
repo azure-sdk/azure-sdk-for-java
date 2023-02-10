b2f95eaa261d9969328d54093ce06335d264ea54
SDKAuto <sdkautomation@microsoft.com>
2023-02-10 11:17:39 +0000

    CodeGen from PR 2882 in test-repo-billy/azure-rest-api-specs Merge eb2e63ccb9aa2a9448d7b49e91465f212d91a7f2 into 28a696cfa0329bcf2d45590868c57f37426df0a1

--- sdk/devspaces/azure-resourcemanager-devspaces/src/main/java/com/azure/resourcemanager/devspaces/DevSpacesManager.java
+++ sdk/devspaces/azure-resourcemanager-devspaces/src/main/java/com/azure/resourcemanager/devspaces/DevSpacesManager.java
@@ -211,7 +211,7 @@ public final class DevSpacesManager {
                 .append("-")
                 .append("com.azure.resourcemanager.devspaces")
                 .append("/")
-                .append("1.0.0-beta.2");
+                .append("1.0.0-beta.1");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder
                     .append(" (")
