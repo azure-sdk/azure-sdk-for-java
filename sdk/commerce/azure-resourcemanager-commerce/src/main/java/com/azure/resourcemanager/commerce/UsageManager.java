56188e4c07f67c365e3d8fad894e9bf956721e1f
SDKAuto <sdkautomation@microsoft.com>
2023-02-10 03:46:55 +0000

    CodeGen from PR 22541 in Azure/azure-rest-api-specs Merge 200066b18054fd6751cf6ea089ac17f32033f537 into 33a7dbc1743fdacb8e2259011a035154a98cad66

--- sdk/commerce/azure-resourcemanager-commerce/src/main/java/com/azure/resourcemanager/commerce/UsageManager.java
+++ sdk/commerce/azure-resourcemanager-commerce/src/main/java/com/azure/resourcemanager/commerce/UsageManager.java
@@ -207,7 +207,7 @@ public final class UsageManager {
                 .append("-")
                 .append("com.azure.resourcemanager.commerce")
                 .append("/")
-                .append("1.0.0-beta.2");
+                .append("1.0.0-beta.1");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder
                     .append(" (")
