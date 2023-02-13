88334aeeb8debc529a790c197e96fef33caa3f79
SDKAuto <sdkautomation@microsoft.com>
2023-02-13 07:18:29 +0000

    CodeGen from PR 22571 in Azure/azure-rest-api-specs Merge 6fccc71b059153103398dc04a2c7f533e6777315 into 47d1d82108a0db0395ed4eca106622becee7fbb4

--- sdk/databoxedge/azure-resourcemanager-databoxedge/src/samples/java/com/azure/resourcemanager/databoxedge/generated/TriggersDeleteSamples.java
+++ sdk/databoxedge/azure-resourcemanager-databoxedge/src/samples/java/com/azure/resourcemanager/databoxedge/generated/TriggersDeleteSamples.java
@@ -7,7 +7,7 @@ package com.azure.resourcemanager.databoxedge.generated;
 /** Samples for Triggers Delete. */
 public final class TriggersDeleteSamples {
     /*
-     * x-ms-original-file: specification/databoxedge/resource-manager/Microsoft.DataBoxEdge/stable/2019-08-01/examples/TriggerDelete.json
+     * x-ms-original-file: specification/databoxedge/resource-manager/Microsoft.DataBoxEdge/stable/2022-03-01/examples/TriggerDelete.json
      */
     /**
      * Sample code: TriggerDelete.
