45c8653ef1f2e42c0bd0eacb9fc693c18524595a
SDKAuto <sdkautomation@microsoft.com>
2023-02-13 17:52:03 +0000

    CodeGen from PR 2790 in test-repo-billy/azure-rest-api-specs Merge 4e86cb44966ae1b15962de76d337af81ffc522fe into 50e7b9b2c1527ee2342032595541b2ffd12ae10d

--- sdk/databoxedge/azure-resourcemanager-databoxedge/src/samples/java/com/azure/resourcemanager/databoxedge/generated/DevicesGetByResourceGroupSamples.java
+++ sdk/databoxedge/azure-resourcemanager-databoxedge/src/samples/java/com/azure/resourcemanager/databoxedge/generated/DevicesGetByResourceGroupSamples.java
@@ -7,7 +7,7 @@ package com.azure.resourcemanager.databoxedge.generated;
 /** Samples for Devices GetByResourceGroup. */
 public final class DevicesGetByResourceGroupSamples {
     /*
-     * x-ms-original-file: specification/databoxedge/resource-manager/Microsoft.DataBoxEdge/stable/2019-08-01/examples/DataBoxEdgeDeviceGetByName.json
+     * x-ms-original-file: specification/databoxedge/resource-manager/Microsoft.DataBoxEdge/stable/2022-03-01/examples/DataBoxEdgeDeviceGetByName.json
      */
     /**
      * Sample code: DataBoxEdgeDeviceGetByName.
@@ -15,6 +15,22 @@ public final class DevicesGetByResourceGroupSamples {
      * @param manager Entry point to DataBoxEdgeManager.
      */
     public static void dataBoxEdgeDeviceGetByName(com.azure.resourcemanager.databoxedge.DataBoxEdgeManager manager) {
+        manager
+            .devices()
+            .getByResourceGroupWithResponse(
+                "GroupForEdgeAutomation", "testedgedevice", com.azure.core.util.Context.NONE);
+    }
+
+    /*
+     * x-ms-original-file: specification/databoxedge/resource-manager/Microsoft.DataBoxEdge/stable/2022-03-01/examples/DataBoxEdgeDeviceGetByNameWithDataResidency.json
+     */
+    /**
+     * Sample code: DataBoxEdgeDeviceGetByNameWithDataResidency.
+     *
+     * @param manager Entry point to DataBoxEdgeManager.
+     */
+    public static void dataBoxEdgeDeviceGetByNameWithDataResidency(
+        com.azure.resourcemanager.databoxedge.DataBoxEdgeManager manager) {
         manager
             .devices()
             .getByResourceGroupWithResponse(
