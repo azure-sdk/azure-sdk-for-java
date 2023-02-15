ef2bc94b0d22e553bccfa947b8250e4557d2d73b
SDKAuto <sdkautomation@microsoft.com>
2023-02-15 06:16:35 +0000

    CodeGen from PR 22627 in Azure/azure-rest-api-specs Merge 85862c611d0b43b2d43cc603bb23a85b8ddb5963 into 402006d2796cdd3894d013d83e77b46a5c844005

--- sdk/dataprotection/azure-resourcemanager-dataprotection/src/main/java/com/azure/resourcemanager/dataprotection/DataProtectionManager.java
+++ sdk/dataprotection/azure-resourcemanager-dataprotection/src/main/java/com/azure/resourcemanager/dataprotection/DataProtectionManager.java
@@ -268,7 +268,7 @@ public final class DataProtectionManager {
                 .append("-")
                 .append("com.azure.resourcemanager.dataprotection")
                 .append("/")
-                .append("1.0.0-beta.3");
+                .append("1.0.0-beta.1");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder
                     .append(" (")
