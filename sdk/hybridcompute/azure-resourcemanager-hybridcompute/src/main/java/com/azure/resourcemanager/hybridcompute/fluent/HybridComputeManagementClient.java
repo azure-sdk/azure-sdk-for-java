f2d6615554926b1344aca12cc9edd0a1e616f2d9
SDKAuto <sdkautomation@microsoft.com>
2023-03-28 06:40:31 +0000

    CodeGen from PR 23320 in Azure/azure-rest-api-specs Merge 9dc6c8c6dc1f87a4be8e88397c87d05f55d4f23d into 1e9b59f586be35d6f0a71d1fd4dad44a01a9061e

--- sdk/hybridcompute/azure-resourcemanager-hybridcompute/src/main/java/com/azure/resourcemanager/hybridcompute/fluent/HybridComputeManagementClient.java
+++ sdk/hybridcompute/azure-resourcemanager-hybridcompute/src/main/java/com/azure/resourcemanager/hybridcompute/fluent/HybridComputeManagementClient.java
@@ -59,11 +59,32 @@ public interface HybridComputeManagementClient {
     MachineExtensionsClient getMachineExtensions();
 
     /**
+     * Gets the ResourceProvidersClient object to access its operations.
+     *
+     * @return the ResourceProvidersClient object.
+     */
+    ResourceProvidersClient getResourceProviders();
+
+    /**
+     * Gets the ExtensionMetadatasClient object to access its operations.
+     *
+     * @return the ExtensionMetadatasClient object.
+     */
+    ExtensionMetadatasClient getExtensionMetadatas();
+
+    /**
      * Gets the OperationsClient object to access its operations.
      *
      * @return the OperationsClient object.
      */
     OperationsClient getOperations();
+
+    /**
+     * Gets the NetworkProfilesClient object to access its operations.
+     *
+     * @return the NetworkProfilesClient object.
+     */
+    NetworkProfilesClient getNetworkProfiles();
 
     /**
      * Gets the PrivateLinkScopesClient object to access its operations.
