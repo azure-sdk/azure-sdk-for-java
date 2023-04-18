55e50b50e9cb6fb899574483aefa0ecbf4ee407d
SDKAuto <sdkautomation@microsoft.com>
2023-04-18 20:52:05 +0000

    CodeGen from PR 23611 in Azure/azure-rest-api-specs Merge 713973710b1c5e88c5733df287d4ffd74c42ceca into ee8866750618fc07f5c8be4044442d3728b90647

--- sdk/securityinsights/azure-resourcemanager-securityinsights/src/main/java/com/azure/resourcemanager/securityinsights/SecurityInsightsManager.java
+++ sdk/securityinsights/azure-resourcemanager-securityinsights/src/main/java/com/azure/resourcemanager/securityinsights/SecurityInsightsManager.java
@@ -25,9 +25,11 @@ import com.azure.core.util.Configuration;
 import com.azure.core.util.logging.ClientLogger;
 import com.azure.resourcemanager.securityinsights.fluent.SecurityInsights;
 import com.azure.resourcemanager.securityinsights.implementation.ActionsImpl;
+import com.azure.resourcemanager.securityinsights.implementation.AlertRuleOperationsImpl;
 import com.azure.resourcemanager.securityinsights.implementation.AlertRuleTemplatesImpl;
 import com.azure.resourcemanager.securityinsights.implementation.AlertRulesImpl;
 import com.azure.resourcemanager.securityinsights.implementation.AutomationRulesImpl;
+import com.azure.resourcemanager.securityinsights.implementation.BillingStatisticsImpl;
 import com.azure.resourcemanager.securityinsights.implementation.BookmarkOperationsImpl;
 import com.azure.resourcemanager.securityinsights.implementation.BookmarkRelationsImpl;
 import com.azure.resourcemanager.securityinsights.implementation.BookmarksImpl;
@@ -41,8 +43,12 @@ import com.azure.resourcemanager.securityinsights.implementation.EntityQueriesIm
 import com.azure.resourcemanager.securityinsights.implementation.EntityQueryTemplatesImpl;
 import com.azure.resourcemanager.securityinsights.implementation.EntityRelationsImpl;
 import com.azure.resourcemanager.securityinsights.implementation.FileImportsImpl;
+import com.azure.resourcemanager.securityinsights.implementation.GetRecommendationsImpl;
+import com.azure.resourcemanager.securityinsights.implementation.GetTriggeredAnalyticsRuleRunsImpl;
+import com.azure.resourcemanager.securityinsights.implementation.GetsImpl;
 import com.azure.resourcemanager.securityinsights.implementation.IncidentCommentsImpl;
 import com.azure.resourcemanager.securityinsights.implementation.IncidentRelationsImpl;
+import com.azure.resourcemanager.securityinsights.implementation.IncidentTasksImpl;
 import com.azure.resourcemanager.securityinsights.implementation.IncidentsImpl;
 import com.azure.resourcemanager.securityinsights.implementation.IpGeodatasImpl;
 import com.azure.resourcemanager.securityinsights.implementation.MetadatasImpl;
@@ -57,12 +63,16 @@ import com.azure.resourcemanager.securityinsights.implementation.SourceControlsO
 import com.azure.resourcemanager.securityinsights.implementation.ThreatIntelligenceIndicatorMetricsImpl;
 import com.azure.resourcemanager.securityinsights.implementation.ThreatIntelligenceIndicatorsImpl;
 import com.azure.resourcemanager.securityinsights.implementation.ThreatIntelligenceIndicatorsOperationsImpl;
+import com.azure.resourcemanager.securityinsights.implementation.TriggeredAnalyticsRuleRunOperationsImpl;
+import com.azure.resourcemanager.securityinsights.implementation.UpdatesImpl;
 import com.azure.resourcemanager.securityinsights.implementation.WatchlistItemsImpl;
 import com.azure.resourcemanager.securityinsights.implementation.WatchlistsImpl;
 import com.azure.resourcemanager.securityinsights.models.Actions;
+import com.azure.resourcemanager.securityinsights.models.AlertRuleOperations;
 import com.azure.resourcemanager.securityinsights.models.AlertRuleTemplates;
 import com.azure.resourcemanager.securityinsights.models.AlertRules;
 import com.azure.resourcemanager.securityinsights.models.AutomationRules;
+import com.azure.resourcemanager.securityinsights.models.BillingStatistics;
 import com.azure.resourcemanager.securityinsights.models.BookmarkOperations;
 import com.azure.resourcemanager.securityinsights.models.BookmarkRelations;
 import com.azure.resourcemanager.securityinsights.models.Bookmarks;
@@ -76,8 +86,12 @@ import com.azure.resourcemanager.securityinsights.models.EntityQueries;
 import com.azure.resourcemanager.securityinsights.models.EntityQueryTemplates;
 import com.azure.resourcemanager.securityinsights.models.EntityRelations;
 import com.azure.resourcemanager.securityinsights.models.FileImports;
+import com.azure.resourcemanager.securityinsights.models.GetRecommendations;
+import com.azure.resourcemanager.securityinsights.models.GetTriggeredAnalyticsRuleRuns;
+import com.azure.resourcemanager.securityinsights.models.Gets;
 import com.azure.resourcemanager.securityinsights.models.IncidentComments;
 import com.azure.resourcemanager.securityinsights.models.IncidentRelations;
+import com.azure.resourcemanager.securityinsights.models.IncidentTasks;
 import com.azure.resourcemanager.securityinsights.models.Incidents;
 import com.azure.resourcemanager.securityinsights.models.IpGeodatas;
 import com.azure.resourcemanager.securityinsights.models.Metadatas;
@@ -91,6 +105,8 @@ import com.azure.resourcemanager.securityinsights.models.SourceControlsOperation
 import com.azure.resourcemanager.securityinsights.models.ThreatIntelligenceIndicatorMetrics;
 import com.azure.resourcemanager.securityinsights.models.ThreatIntelligenceIndicators;
 import com.azure.resourcemanager.securityinsights.models.ThreatIntelligenceIndicatorsOperations;
+import com.azure.resourcemanager.securityinsights.models.TriggeredAnalyticsRuleRunOperations;
+import com.azure.resourcemanager.securityinsights.models.Updates;
 import com.azure.resourcemanager.securityinsights.models.WatchlistItems;
 import com.azure.resourcemanager.securityinsights.models.Watchlists;
 import java.time.Duration;
@@ -114,6 +130,8 @@ public final class SecurityInsightsManager {
     private AutomationRules automationRules;
 
     private Incidents incidents;
+
+    private BillingStatistics billingStatistics;
 
     private Bookmarks bookmarks;
 
@@ -143,11 +161,19 @@ public final class SecurityInsightsManager {
 
     private IncidentRelations incidentRelations;
 
+    private IncidentTasks incidentTasks;
+
     private Metadatas metadatas;
 
     private OfficeConsents officeConsents;
 
     private SentinelOnboardingStates sentinelOnboardingStates;
+
+    private GetRecommendations getRecommendations;
+
+    private Gets gets;
+
+    private Updates updates;
 
     private SecurityMLAnalyticsSettings securityMLAnalyticsSettings;
 
@@ -162,6 +188,12 @@ public final class SecurityInsightsManager {
     private ThreatIntelligenceIndicatorsOperations threatIntelligenceIndicatorsOperations;
 
     private ThreatIntelligenceIndicatorMetrics threatIntelligenceIndicatorMetrics;
+
+    private TriggeredAnalyticsRuleRunOperations triggeredAnalyticsRuleRunOperations;
+
+    private GetTriggeredAnalyticsRuleRuns getTriggeredAnalyticsRuleRuns;
+
+    private AlertRuleOperations alertRuleOperations;
 
     private Watchlists watchlists;
 
@@ -338,7 +370,7 @@ public final class SecurityInsightsManager {
                 .append("-")
                 .append("com.azure.resourcemanager.securityinsights")
                 .append("/")
-                .append("1.0.0-beta.4");
+                .append("1.0.0-beta.1");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder
                     .append(" (")
@@ -453,6 +485,18 @@ public final class SecurityInsightsManager {
             this.incidents = new IncidentsImpl(clientObject.getIncidents(), this);
         }
         return incidents;
+    }
+
+    /**
+     * Gets the resource collection API of BillingStatistics.
+     *
+     * @return Resource collection API of BillingStatistics.
+     */
+    public BillingStatistics billingStatistics() {
+        if (this.billingStatistics == null) {
+            this.billingStatistics = new BillingStatisticsImpl(clientObject.getBillingStatistics(), this);
+        }
+        return billingStatistics;
     }
 
     /**
@@ -624,6 +668,18 @@ public final class SecurityInsightsManager {
     }
 
     /**
+     * Gets the resource collection API of IncidentTasks. It manages IncidentTask.
+     *
+     * @return Resource collection API of IncidentTasks.
+     */
+    public IncidentTasks incidentTasks() {
+        if (this.incidentTasks == null) {
+            this.incidentTasks = new IncidentTasksImpl(clientObject.getIncidentTasks(), this);
+        }
+        return incidentTasks;
+    }
+
+    /**
      * Gets the resource collection API of Metadatas. It manages MetadataModel.
      *
      * @return Resource collection API of Metadatas.
@@ -658,6 +714,42 @@ public final class SecurityInsightsManager {
                 new SentinelOnboardingStatesImpl(clientObject.getSentinelOnboardingStates(), this);
         }
         return sentinelOnboardingStates;
+    }
+
+    /**
+     * Gets the resource collection API of GetRecommendations.
+     *
+     * @return Resource collection API of GetRecommendations.
+     */
+    public GetRecommendations getRecommendations() {
+        if (this.getRecommendations == null) {
+            this.getRecommendations = new GetRecommendationsImpl(clientObject.getGetRecommendations(), this);
+        }
+        return getRecommendations;
+    }
+
+    /**
+     * Gets the resource collection API of Gets.
+     *
+     * @return Resource collection API of Gets.
+     */
+    public Gets gets() {
+        if (this.gets == null) {
+            this.gets = new GetsImpl(clientObject.getGets(), this);
+        }
+        return gets;
+    }
+
+    /**
+     * Gets the resource collection API of Updates.
+     *
+     * @return Resource collection API of Updates.
+     */
+    public Updates updates() {
+        if (this.updates == null) {
+            this.updates = new UpdatesImpl(clientObject.getUpdates(), this);
+        }
+        return updates;
     }
 
     /**
@@ -748,6 +840,45 @@ public final class SecurityInsightsManager {
                 new ThreatIntelligenceIndicatorMetricsImpl(clientObject.getThreatIntelligenceIndicatorMetrics(), this);
         }
         return threatIntelligenceIndicatorMetrics;
+    }
+
+    /**
+     * Gets the resource collection API of TriggeredAnalyticsRuleRunOperations.
+     *
+     * @return Resource collection API of TriggeredAnalyticsRuleRunOperations.
+     */
+    public TriggeredAnalyticsRuleRunOperations triggeredAnalyticsRuleRunOperations() {
+        if (this.triggeredAnalyticsRuleRunOperations == null) {
+            this.triggeredAnalyticsRuleRunOperations =
+                new TriggeredAnalyticsRuleRunOperationsImpl(
+                    clientObject.getTriggeredAnalyticsRuleRunOperations(), this);
+        }
+        return triggeredAnalyticsRuleRunOperations;
+    }
+
+    /**
+     * Gets the resource collection API of GetTriggeredAnalyticsRuleRuns.
+     *
+     * @return Resource collection API of GetTriggeredAnalyticsRuleRuns.
+     */
+    public GetTriggeredAnalyticsRuleRuns getTriggeredAnalyticsRuleRuns() {
+        if (this.getTriggeredAnalyticsRuleRuns == null) {
+            this.getTriggeredAnalyticsRuleRuns =
+                new GetTriggeredAnalyticsRuleRunsImpl(clientObject.getGetTriggeredAnalyticsRuleRuns(), this);
+        }
+        return getTriggeredAnalyticsRuleRuns;
+    }
+
+    /**
+     * Gets the resource collection API of AlertRuleOperations.
+     *
+     * @return Resource collection API of AlertRuleOperations.
+     */
+    public AlertRuleOperations alertRuleOperations() {
+        if (this.alertRuleOperations == null) {
+            this.alertRuleOperations = new AlertRuleOperationsImpl(clientObject.getAlertRuleOperations(), this);
+        }
+        return alertRuleOperations;
     }
 
     /**
