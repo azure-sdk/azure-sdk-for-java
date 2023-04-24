5c9d59c6f4bf73335a108bb150e022bb7e92d4ad
SDKAuto <sdkautomation@microsoft.com>
2023-04-24 08:08:32 +0000

    CodeGen from PR 2594 in test-repo-billy/azure-rest-api-specs Merge fda03acb3594cdd152e50146045adcf588b8c6cf into 9a46bdbfea7730a3521ac11f5ea1120007d67f79

--- sdk/machinelearning/azure-resourcemanager-machinelearning/src/samples/java/com/azure/resourcemanager/machinelearning/generated/WorkspacesCreateOrUpdateSamples.java
+++ sdk/machinelearning/azure-resourcemanager-machinelearning/src/samples/java/com/azure/resourcemanager/machinelearning/generated/WorkspacesCreateOrUpdateSamples.java
@@ -20,7 +20,7 @@ import java.util.Map;
 /** Samples for Workspaces CreateOrUpdate. */
 public final class WorkspacesCreateOrUpdateSamples {
     /*
-     * x-ms-original-file: specification/machinelearningservices/resource-manager/Microsoft.MachineLearningServices/stable/2022-10-01/examples/Workspace/create.json
+     * x-ms-original-file: specification/machinelearningservices/resource-manager/Microsoft.MachineLearningServices/stable/2023-04-01/examples/Workspace/create.json
      */
     /**
      * Sample code: Create Workspace.
