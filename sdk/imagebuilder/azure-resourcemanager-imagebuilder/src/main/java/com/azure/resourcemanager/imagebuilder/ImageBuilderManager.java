aec2b33afc88d5116e5b528474e1eef409f22b34
SDKAuto <sdkautomation@microsoft.com>
2023-05-12 13:52:41 +0000

    CodeGen from PR 23953 in Azure/azure-rest-api-specs Merge 147f2c7fa7bb262a7cd40975bdfa931315d273f7 into af3e299279d41ea879cab44db46663c2fac692ad

--- sdk/imagebuilder/azure-resourcemanager-imagebuilder/src/main/java/com/azure/resourcemanager/imagebuilder/ImageBuilderManager.java
+++ sdk/imagebuilder/azure-resourcemanager-imagebuilder/src/main/java/com/azure/resourcemanager/imagebuilder/ImageBuilderManager.java
@@ -211,7 +211,7 @@ public final class ImageBuilderManager {
                 .append("-")
                 .append("com.azure.resourcemanager.imagebuilder")
                 .append("/")
-                .append("1.0.0-beta.4");
+                .append("1.0.0-beta.1");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder
                     .append(" (")
