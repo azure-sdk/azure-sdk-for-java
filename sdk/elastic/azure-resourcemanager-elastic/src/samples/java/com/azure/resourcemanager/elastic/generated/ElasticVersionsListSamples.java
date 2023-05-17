91ffcd539cbc0650f6317efceb49313ae7338ece
SDKAuto <sdkautomation@microsoft.com>
2023-05-17 18:39:56 +0000

    CodeGen from PR 24031 in Azure/azure-rest-api-specs Merge f4fbc536495e73af7f749d6af226a277c672fb12 into 173bb3b6fd5b1809fdbf347f67fccfa0440ac126

--- sdk/elastic/azure-resourcemanager-elastic/src/samples/java/com/azure/resourcemanager/elastic/generated/ElasticVersionsListSamples.java
+++ sdk/elastic/azure-resourcemanager-elastic/src/samples/java/com/azure/resourcemanager/elastic/generated/ElasticVersionsListSamples.java
@@ -7,7 +7,7 @@ package com.azure.resourcemanager.elastic.generated;
 /** Samples for ElasticVersions List. */
 public final class ElasticVersionsListSamples {
     /*
-     * x-ms-original-file: specification/elastic/resource-manager/Microsoft.Elastic/preview/2023-02-01-preview/examples/ElasticVersions_List.json
+     * x-ms-original-file: specification/elastic/resource-manager/Microsoft.Elastic/preview/2023-05-01-preview/examples/ElasticVersions_List.json
      */
     /**
      * Sample code: ElasticVersions_List.
@@ -15,6 +15,6 @@ public final class ElasticVersionsListSamples {
      * @param manager Entry point to ElasticManager.
      */
     public static void elasticVersionsList(com.azure.resourcemanager.elastic.ElasticManager manager) {
-        manager.elasticVersions().listWithResponse("myregion", com.azure.core.util.Context.NONE);
+        manager.elasticVersions().list("myregion", com.azure.core.util.Context.NONE);
     }
 }
