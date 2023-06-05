b686775492b289319fe6dcd85b4e1a1fe2f67d8e
SDKAuto <sdkautomation@microsoft.com>
2023-06-05 14:32:28 +0000

    CodeGen from PR 24275 in Azure/azure-rest-api-specs Merge 953cc2e311eb38387d5e167920ca5c8d267a7bde into 3858211e84dfd25440432d910eed244e8b4334ac

--- sdk/appcontainers/azure-resourcemanager-appcontainers/src/samples/java/com/azure/resourcemanager/appcontainers/generated/ContainerAppsRevisionsActivateRevisionSamples.java
+++ sdk/appcontainers/azure-resourcemanager-appcontainers/src/samples/java/com/azure/resourcemanager/appcontainers/generated/ContainerAppsRevisionsActivateRevisionSamples.java
@@ -7,7 +7,7 @@ package com.azure.resourcemanager.appcontainers.generated;
 /** Samples for ContainerAppsRevisions ActivateRevision. */
 public final class ContainerAppsRevisionsActivateRevisionSamples {
     /*
-     * x-ms-original-file: specification/app/resource-manager/Microsoft.App/preview/2022-11-01-preview/examples/Revisions_Activate.json
+     * x-ms-original-file: specification/app/resource-manager/Microsoft.App/preview/2023-05-02-preview/examples/Revisions_Activate.json
      */
     /**
      * Sample code: Activate Container App's revision.
