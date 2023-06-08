bbfef4c72809645761147b6a3e1ff3ee42e5c59e
SDKAuto <sdkautomation@microsoft.com>
2023-06-08 23:22:50 +0000

    CodeGen from PR 24374 in Azure/azure-rest-api-specs Merge d8aaec54dcedaed00123ccbe57e7aa24b43a7ea9 into 1290a9dfec16dcf4e62191774b9cc208e92f678e

--- sdk/anomalydetector/azure-ai-anomalydetector/src/main/java/com/azure/ai/anomalydetector/AnomalyDetectorClientBuilder.java
+++ sdk/anomalydetector/azure-ai-anomalydetector/src/main/java/com/azure/ai/anomalydetector/AnomalyDetectorClientBuilder.java
@@ -33,6 +33,7 @@ import com.azure.core.util.ClientOptions;
 import com.azure.core.util.Configuration;
 import com.azure.core.util.CoreUtils;
 import com.azure.core.util.builder.ClientBuilderUtil;
+import com.azure.core.util.logging.ClientLogger;
 import com.azure.core.util.serializer.JacksonAdapter;
 import java.util.ArrayList;
 import java.util.List;
@@ -72,6 +73,9 @@ public final class AnomalyDetectorClientBuilder
     @Generated
     @Override
     public AnomalyDetectorClientBuilder pipeline(HttpPipeline pipeline) {
+        if (this.pipeline != null && pipeline == null) {
+            LOGGER.info("HttpPipeline is being set to 'null' when it was previously configured.");
+        }
         this.pipeline = pipeline;
         return this;
     }
@@ -287,4 +291,6 @@ public final class AnomalyDetectorClientBuilder
     public AnomalyDetectorClient buildClient() {
         return new AnomalyDetectorClient(new AnomalyDetectorAsyncClient(buildInnerClient()));
     }
+
+    private static final ClientLogger LOGGER = new ClientLogger(AnomalyDetectorClientBuilder.class);
 }
