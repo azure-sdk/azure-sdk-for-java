6c573bf6f4843f111d6040cf12c347e775583aa8
SDKAuto <sdkautomation@microsoft.com>
2023-06-29 23:26:41 +0000

    CodeGen from PR 23677 in Azure/azure-rest-api-specs Merge 8eba276aa860a808a6eee2db6a9c22ab12a43a1d into f0866b0733a2f4bed9e85b2f7e4e51bb230029a5

--- sdk/billing/azure-resourcemanager-billing/src/main/java/com/azure/resourcemanager/billing/BillingManager.java
+++ sdk/billing/azure-resourcemanager-billing/src/main/java/com/azure/resourcemanager/billing/BillingManager.java
@@ -283,7 +283,7 @@ public final class BillingManager {
                 .append("-")
                 .append("com.azure.resourcemanager.billing")
                 .append("/")
-                .append("1.0.0-beta.3");
+                .append("1.0.0-beta.1");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder
                     .append(" (")
