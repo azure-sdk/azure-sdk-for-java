2acb9bb9161e0c876c34c4c2cc41221256167e45
SDKAuto <sdkautomation@microsoft.com>
2023-07-11 00:34:03 +0000

    CodeGen from PR 2939 in test-repo-billy/azure-rest-api-specs Merge 9285405b366e20eef68ab4da40fb47508acf378d into e5d8bf380ef60a6a283ba81fd0ce4bc0e49e6680

--- sdk/agrifood/azure-resourcemanager-agrifood/src/samples/java/com/azure/resourcemanager/agrifood/generated/PrivateEndpointConnectionsGetSamples.java
+++ sdk/agrifood/azure-resourcemanager-agrifood/src/samples/java/com/azure/resourcemanager/agrifood/generated/PrivateEndpointConnectionsGetSamples.java
@@ -4,8 +4,6 @@
 
 package com.azure.resourcemanager.agrifood.generated;
 
-import com.azure.core.util.Context;
-
 /** Samples for PrivateEndpointConnections Get. */
 public final class PrivateEndpointConnectionsGetSamples {
     /*
@@ -20,6 +18,9 @@ public final class PrivateEndpointConnectionsGetSamples {
         manager
             .privateEndpointConnections()
             .getWithResponse(
-                "examples-rg", "examples-farmbeatsResourceName", "privateEndpointConnectionName", Context.NONE);
+                "examples-rg",
+                "examples-farmbeatsResourceName",
+                "privateEndpointConnectionName",
+                com.azure.core.util.Context.NONE);
     }
 }
