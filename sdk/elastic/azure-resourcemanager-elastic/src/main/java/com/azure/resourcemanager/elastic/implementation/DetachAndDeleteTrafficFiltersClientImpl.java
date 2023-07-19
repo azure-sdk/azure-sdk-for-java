657a2e449756d29a5440d4d1bfc6d1fef6337d9f
SDKAuto <sdkautomation@microsoft.com>
2023-07-19 18:30:15 +0000

    CodeGen from PR 24587 in Azure/azure-rest-api-specs Merge c8d69ffb8fba7fbac410be9e8130a4e49e4e2b6d into e680f9bf4f154ec427ba7feac432ed8efd9665d0

--- sdk/elastic/azure-resourcemanager-elastic/src/main/java/com/azure/resourcemanager/elastic/implementation/DetachAndDeleteTrafficFiltersClientImpl.java
+++ sdk/elastic/azure-resourcemanager-elastic/src/main/java/com/azure/resourcemanager/elastic/implementation/DetachAndDeleteTrafficFiltersClientImpl.java
@@ -73,7 +73,7 @@ public final class DetachAndDeleteTrafficFiltersClientImpl implements DetachAndD
     /**
      * Detach and Delete traffic filter from the given deployment.
      *
-     * @param resourceGroupName The name of the resource group to which the Elastic resource belongs.
+     * @param resourceGroupName The name of the resource group. The name is case insensitive.
      * @param monitorName Monitor resource name.
      * @param rulesetId Ruleset Id of the filter.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
@@ -123,7 +123,7 @@ public final class DetachAndDeleteTrafficFiltersClientImpl implements DetachAndD
     /**
      * Detach and Delete traffic filter from the given deployment.
      *
-     * @param resourceGroupName The name of the resource group to which the Elastic resource belongs.
+     * @param resourceGroupName The name of the resource group. The name is case insensitive.
      * @param monitorName Monitor resource name.
      * @param rulesetId Ruleset Id of the filter.
      * @param context The context to associate with this operation.
@@ -171,7 +171,7 @@ public final class DetachAndDeleteTrafficFiltersClientImpl implements DetachAndD
     /**
      * Detach and Delete traffic filter from the given deployment.
      *
-     * @param resourceGroupName The name of the resource group to which the Elastic resource belongs.
+     * @param resourceGroupName The name of the resource group. The name is case insensitive.
      * @param monitorName Monitor resource name.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
      * @throws ManagementException thrown if the request is rejected by server.
@@ -187,7 +187,7 @@ public final class DetachAndDeleteTrafficFiltersClientImpl implements DetachAndD
     /**
      * Detach and Delete traffic filter from the given deployment.
      *
-     * @param resourceGroupName The name of the resource group to which the Elastic resource belongs.
+     * @param resourceGroupName The name of the resource group. The name is case insensitive.
      * @param monitorName Monitor resource name.
      * @param rulesetId Ruleset Id of the filter.
      * @param context The context to associate with this operation.
@@ -205,7 +205,7 @@ public final class DetachAndDeleteTrafficFiltersClientImpl implements DetachAndD
     /**
      * Detach and Delete traffic filter from the given deployment.
      *
-     * @param resourceGroupName The name of the resource group to which the Elastic resource belongs.
+     * @param resourceGroupName The name of the resource group. The name is case insensitive.
      * @param monitorName Monitor resource name.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
      * @throws ManagementException thrown if the request is rejected by server.
--- sdk/elastic/azure-resourcemanager-elastic/src/samples/java/com/azure/resourcemanager/elastic/generated/MonitorsListByResourceGroupSamples.java
+++ sdk/elastic/azure-resourcemanager-elastic/src/samples/java/com/azure/resourcemanager/elastic/generated/MonitorsListByResourceGroupSamples.java
@@ -7,7 +7,7 @@ package com.azure.resourcemanager.elastic.generated;
 /** Samples for Monitors ListByResourceGroup. */
 public final class MonitorsListByResourceGroupSamples {
     /*
-     * x-ms-original-file: specification/elastic/resource-manager/Microsoft.Elastic/preview/2023-02-01-preview/examples/Monitors_ListByResourceGroup.json
+     * x-ms-original-file: specification/elastic/resource-manager/Microsoft.Elastic/preview/2023-07-01-preview/examples/Monitors_ListByResourceGroup.json
      */
     /**
      * Sample code: Monitors_ListByResourceGroup.
