d81222b0ba8d53a55d3e4ec530af4c78328d147d
SDKAuto <sdkautomation@microsoft.com>
2023-08-12 00:54:54 +0000

    CodeGen from PR 25290 in Azure/azure-rest-api-specs Merge 4d2c8cf911b96035e5fcc8f68dbb493af685fb47 into 2265cda5e0021ea1ab9cadd1fa3dc68bb4f5e6b4

--- sdk/consumption/azure-resourcemanager-consumption/src/main/java/com/azure/resourcemanager/consumption/models/Status.java
+++ sdk/consumption/azure-resourcemanager-consumption/src/main/java/com/azure/resourcemanager/consumption/models/Status.java
@@ -8,7 +8,7 @@ import com.azure.core.util.ExpandableStringEnum;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import java.util.Collection;
 
-/** Defines values for Status. */
+/** The status of the lot. */
 public final class Status extends ExpandableStringEnum<Status> {
     /** Static value None for Status. */
     public static final Status NONE = fromString("None");
@@ -29,6 +29,15 @@ public final class Status extends ExpandableStringEnum<Status> {
     public static final Status CANCELED = fromString("Canceled");
 
     /**
+     * Creates a new instance of Status value.
+     *
+     * @deprecated Use the {@link #fromString(String)} factory method.
+     */
+    @Deprecated
+    public Status() {
+    }
+
+    /**
      * Creates or finds a Status from its string representation.
      *
      * @param name a name to look for.
@@ -39,7 +48,11 @@ public final class Status extends ExpandableStringEnum<Status> {
         return fromString(name, Status.class);
     }
 
-    /** @return known Status values. */
+    /**
+     * Gets known Status values.
+     *
+     * @return known Status values.
+     */
     public static Collection<Status> values() {
         return values(Status.class);
     }
