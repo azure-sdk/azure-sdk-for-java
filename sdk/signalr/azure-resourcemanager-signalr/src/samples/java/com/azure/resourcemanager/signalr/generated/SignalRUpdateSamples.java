3ec67d2a1b6de11266890a5e117a12f9e0cbef82
SDKAuto <sdkautomation@microsoft.com>
2023-08-18 05:59:00 +0000

    CodeGen from PR 25378 in Azure/azure-rest-api-specs Merge 658fb5c422a8caaffe413286e71c6a1a7722187d into 624dbc769880e5676ae8bb20d3c82ebd1783c64a

--- sdk/signalr/azure-resourcemanager-signalr/src/samples/java/com/azure/resourcemanager/signalr/generated/SignalRUpdateSamples.java
+++ sdk/signalr/azure-resourcemanager-signalr/src/samples/java/com/azure/resourcemanager/signalr/generated/SignalRUpdateSamples.java
@@ -33,7 +33,7 @@ import java.util.Map;
 /** Samples for SignalR Update. */
 public final class SignalRUpdateSamples {
     /*
-     * x-ms-original-file: specification/signalr/resource-manager/Microsoft.SignalRService/stable/2023-02-01/examples/SignalR_Update.json
+     * x-ms-original-file: specification/signalr/resource-manager/Microsoft.SignalRService/preview/2023-08-01-preview/examples/SignalR_Update.json
      */
     /**
      * Sample code: SignalR_Update.
@@ -48,7 +48,7 @@ public final class SignalRUpdateSamples {
                 .getValue();
         resource
             .update()
-            .withTags(mapOf("key1", "value1"))
+            .withTags(mapOf("key1", "fakeTokenPlaceholder"))
             .withSku(new ResourceSku().withName("Premium_P1").withTier(SignalRSkuTier.PREMIUM).withCapacity(1))
             .withIdentity(new ManagedIdentity().withType(ManagedIdentityType.SYSTEM_ASSIGNED))
             .withTls(new SignalRTlsSettings().withClientCertEnabled(false))
