1dc7b9caa9cd94667c079126206b418761673d0e
SDKAuto <sdkautomation@microsoft.com>
2023-08-23 09:18:50 +0000

    CodeGen from PR 25154 in Azure/azure-rest-api-specs Merge 078a5e20b268e90b8544de2699946a1dc019d8f0 into 5372f410f6af3de9559b63defbd556a7b10c4e65

--- sdk/dataprotection/azure-resourcemanager-dataprotection/src/samples/java/com/azure/resourcemanager/dataprotection/generated/ResourceGuardsPutSamples.java
+++ sdk/dataprotection/azure-resourcemanager-dataprotection/src/samples/java/com/azure/resourcemanager/dataprotection/generated/ResourceGuardsPutSamples.java
@@ -27,6 +27,7 @@ public final class ResourceGuardsPutSamples {
             .create();
     }
 
+    // Use "Map.of" if available
     @SuppressWarnings("unchecked")
     private static <T> Map<String, T> mapOf(Object... inputs) {
         Map<String, T> map = new HashMap<>();
