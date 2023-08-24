3248ccc1a36b60a17fa4302990850a30dca870b2
SDKAuto <sdkautomation@microsoft.com>
2023-08-24 09:57:07 +0000

    CodeGen from PR 25369 in Azure/azure-rest-api-specs Merge ca0330004cac361b10d2ef805c3b48df9b2c61ee into db852c3c595d58ed3e13d3031290198ed19876b3

--- sdk/dataprotection/azure-resourcemanager-dataprotection/src/samples/java/com/azure/resourcemanager/dataprotection/generated/BackupVaultsUpdateSamples.java
+++ sdk/dataprotection/azure-resourcemanager-dataprotection/src/samples/java/com/azure/resourcemanager/dataprotection/generated/BackupVaultsUpdateSamples.java
@@ -41,6 +41,7 @@ public final class BackupVaultsUpdateSamples {
             .apply();
     }
 
+    // Use "Map.of" if available
     @SuppressWarnings("unchecked")
     private static <T> Map<String, T> mapOf(Object... inputs) {
         Map<String, T> map = new HashMap<>();
