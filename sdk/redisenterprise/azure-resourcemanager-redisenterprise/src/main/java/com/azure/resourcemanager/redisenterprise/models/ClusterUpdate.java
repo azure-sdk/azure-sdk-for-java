6341c9573428a344541c298d570a49131cb054b5
SDKAuto <sdkautomation@microsoft.com>
2023-08-24 10:29:08 +0000

    CodeGen from PR 25369 in Azure/azure-rest-api-specs Merge ca0330004cac361b10d2ef805c3b48df9b2c61ee into db852c3c595d58ed3e13d3031290198ed19876b3

--- sdk/redisenterprise/azure-resourcemanager-redisenterprise/src/main/java/com/azure/resourcemanager/redisenterprise/models/ClusterUpdate.java
+++ sdk/redisenterprise/azure-resourcemanager-redisenterprise/src/main/java/com/azure/resourcemanager/redisenterprise/models/ClusterUpdate.java
@@ -30,12 +30,6 @@ public final class ClusterUpdate {
     private ClusterProperties innerProperties;
 
     /*
-     * The identity of the resource.
-     */
-    @JsonProperty(value = "identity")
-    private ManagedServiceIdentity identity;
-
-    /*
      * Resource tags.
      */
     @JsonProperty(value = "tags")
@@ -78,26 +72,6 @@ public final class ClusterUpdate {
     }
 
     /**
-     * Get the identity property: The identity of the resource.
-     *
-     * @return the identity value.
-     */
-    public ManagedServiceIdentity identity() {
-        return this.identity;
-    }
-
-    /**
-     * Set the identity property: The identity of the resource.
-     *
-     * @param identity the identity value to set.
-     * @return the ClusterUpdate object itself.
-     */
-    public ClusterUpdate withIdentity(ManagedServiceIdentity identity) {
-        this.identity = identity;
-        return this;
-    }
-
-    /**
      * Get the tags property: Resource tags.
      *
      * @return the tags value.
@@ -137,29 +111,6 @@ public final class ClusterUpdate {
             this.innerProperties = new ClusterProperties();
         }
         this.innerProperties().withMinimumTlsVersion(minimumTlsVersion);
-        return this;
-    }
-
-    /**
-     * Get the encryption property: Encryption-at-rest configuration for the cluster.
-     *
-     * @return the encryption value.
-     */
-    public ClusterPropertiesEncryption encryption() {
-        return this.innerProperties() == null ? null : this.innerProperties().encryption();
-    }
-
-    /**
-     * Set the encryption property: Encryption-at-rest configuration for the cluster.
-     *
-     * @param encryption the encryption value to set.
-     * @return the ClusterUpdate object itself.
-     */
-    public ClusterUpdate withEncryption(ClusterPropertiesEncryption encryption) {
-        if (this.innerProperties() == null) {
-            this.innerProperties = new ClusterProperties();
-        }
-        this.innerProperties().withEncryption(encryption);
         return this;
     }
 
@@ -220,9 +171,6 @@ public final class ClusterUpdate {
         }
         if (innerProperties() != null) {
             innerProperties().validate();
-        }
-        if (identity() != null) {
-            identity().validate();
         }
     }
 }
