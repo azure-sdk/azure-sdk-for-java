cd24787c4ca1384003a0e37f037973306d75399c
SDKAuto <sdkautomation@microsoft.com>
2023-09-15 06:27:14 +0000

    CodeGen from PR 25783 in Azure/azure-rest-api-specs Merge 3ce7a5c3d58c28fa9aa186b1be2e584e411686e5 into d97207a38747c5174455721b93cd2b2c67200618

--- sdk/delegatednetwork/azure-resourcemanager-delegatednetwork/src/samples/java/com/azure/resourcemanager/delegatednetwork/generated/ControllerGetByResourceGroupSamples.java
+++ sdk/delegatednetwork/azure-resourcemanager-delegatednetwork/src/samples/java/com/azure/resourcemanager/delegatednetwork/generated/ControllerGetByResourceGroupSamples.java
@@ -7,7 +7,7 @@ package com.azure.resourcemanager.delegatednetwork.generated;
 /** Samples for Controller GetByResourceGroup. */
 public final class ControllerGetByResourceGroupSamples {
     /*
-     * x-ms-original-file: specification/dnc/resource-manager/Microsoft.DelegatedNetwork/stable/2021-03-15/examples/getController.json
+     * x-ms-original-file: specification/dnc/resource-manager/Microsoft.DelegatedNetwork/preview/2023-06-27-preview/examples/getController.json
      */
     /**
      * Sample code: Get details of a controller.
