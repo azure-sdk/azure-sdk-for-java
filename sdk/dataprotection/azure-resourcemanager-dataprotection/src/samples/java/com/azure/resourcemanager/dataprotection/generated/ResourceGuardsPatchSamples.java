1be7094165357a410865f4718f0b28b757e0eebe
SDKAuto <sdkautomation@microsoft.com>
2023-09-19 23:26:08 +0000

    CodeGen from PR 24773 in Azure/azure-rest-api-specs Merge 2a01bf8eb4f7487a33d81be2bc575e50d5f321b8 into db852c3c595d58ed3e13d3031290198ed19876b3

--- sdk/dataprotection/azure-resourcemanager-dataprotection/src/samples/java/com/azure/resourcemanager/dataprotection/generated/ResourceGuardsPatchSamples.java
+++ sdk/dataprotection/azure-resourcemanager-dataprotection/src/samples/java/com/azure/resourcemanager/dataprotection/generated/ResourceGuardsPatchSamples.java
@@ -28,6 +28,7 @@ public final class ResourceGuardsPatchSamples {
         resource.update().withTags(mapOf("newKey", "fakeTokenPlaceholder")).apply();
     }
 
+    // Use "Map.of" if available
     @SuppressWarnings("unchecked")
     private static <T> Map<String, T> mapOf(Object... inputs) {
         Map<String, T> map = new HashMap<>();
