ad9563f7b214af532370043885e05163a2e95a11
SDKAuto <sdkautomation@microsoft.com>
2023-09-20 00:42:14 +0000

    CodeGen from PR 24773 in Azure/azure-rest-api-specs Merge 2a01bf8eb4f7487a33d81be2bc575e50d5f321b8 into db852c3c595d58ed3e13d3031290198ed19876b3

--- sdk/avs/azure-resourcemanager-avs/src/main/java/com/azure/resourcemanager/avs/AvsManager.java
+++ sdk/avs/azure-resourcemanager-avs/src/main/java/com/azure/resourcemanager/avs/AvsManager.java
@@ -263,7 +263,7 @@ public final class AvsManager {
                 .append("-")
                 .append("com.azure.resourcemanager.avs")
                 .append("/")
-                .append("1.1.0");
+                .append("1.0.0-beta.1");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder
                     .append(" (")
