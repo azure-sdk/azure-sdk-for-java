bfbe383101864eb554d5c1581b8b009e9d2f52b6
SDKAuto <sdkautomation@microsoft.com>
2023-09-27 07:14:32 +0000

    CodeGen from PR 25726 in Azure/azure-rest-api-specs Merge 0c65966ac4c09bba521ccb81da1d20c376b5dc83 into daba4466fa6dbb7c5fce01640138409ba2213e95

--- sdk/paloaltonetworks/azure-resourcemanager-paloaltonetworks-ngfw/src/samples/java/com/azure/resourcemanager/paloaltonetworks/ngfw/generated/FirewallsCreateOrUpdateSamples.java
+++ sdk/paloaltonetworks/azure-resourcemanager-paloaltonetworks-ngfw/src/samples/java/com/azure/resourcemanager/paloaltonetworks/ngfw/generated/FirewallsCreateOrUpdateSamples.java
@@ -206,6 +206,7 @@ public final class FirewallsCreateOrUpdateSamples {
             .create();
     }
 
+    // Use "Map.of" if available
     @SuppressWarnings("unchecked")
     private static <T> Map<String, T> mapOf(Object... inputs) {
         Map<String, T> map = new HashMap<>();
