dba9b3b4964f1eb06476bdb8fa748377e4d293fe
SDKAuto <sdkautomation@microsoft.com>
2023-09-29 07:36:33 +0000

    CodeGen from PR 3041 in test-repo-billy/azure-rest-api-specs Merge da04998b145cbc39c152731d9c1c4ddce5331907 into 80ebd30ba2a47b41ec210e954bd25e07fc83014b

--- sdk/agrifood/azure-resourcemanager-agrifood/src/main/java/com/azure/resourcemanager/agrifood/implementation/AgriFoodManagementClientBuilder.java
+++ sdk/agrifood/azure-resourcemanager-agrifood/src/main/java/com/azure/resourcemanager/agrifood/implementation/AgriFoodManagementClientBuilder.java
@@ -18,12 +18,12 @@ import java.time.Duration;
 @ServiceClientBuilder(serviceClients = {AgriFoodManagementClientImpl.class})
 public final class AgriFoodManagementClientBuilder {
     /*
-     * The ID of the target subscription.
+     * The ID of the target subscription. The value must be an UUID.
      */
     private String subscriptionId;
 
     /**
-     * Sets The ID of the target subscription.
+     * Sets The ID of the target subscription. The value must be an UUID.
      *
      * @param subscriptionId the subscriptionId value.
      * @return the AgriFoodManagementClientBuilder.
@@ -137,7 +137,7 @@ public final class AgriFoodManagementClientBuilder {
                 localSerializerAdapter,
                 localDefaultPollInterval,
                 localEnvironment,
-                subscriptionId,
+                this.subscriptionId,
                 localEndpoint);
         return client;
     }
