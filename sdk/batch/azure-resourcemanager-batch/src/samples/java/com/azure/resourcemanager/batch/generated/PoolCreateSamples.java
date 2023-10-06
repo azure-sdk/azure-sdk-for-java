eacdfa1f130e004a418b1ab2f6d6e3b36fbaa23d
SDKAuto <sdkautomation@microsoft.com>
2023-10-06 13:18:23 +0000

    CodeGen from PR 25627 in Azure/azure-rest-api-specs Merge 9cd30ce94423b971c0cafe9a5e73df8f4c77085c into 2b6c2c43ef43bf1d4c55c5f2a20010607de81b1e

--- sdk/batch/azure-resourcemanager-batch/src/samples/java/com/azure/resourcemanager/batch/generated/PoolCreateSamples.java
+++ sdk/batch/azure-resourcemanager-batch/src/samples/java/com/azure/resourcemanager/batch/generated/PoolCreateSamples.java
@@ -532,6 +532,7 @@ public final class PoolCreateSamples {
             .create();
     }
 
+    // Use "Map.of" if available
     @SuppressWarnings("unchecked")
     private static <T> Map<String, T> mapOf(Object... inputs) {
         Map<String, T> map = new HashMap<>();
