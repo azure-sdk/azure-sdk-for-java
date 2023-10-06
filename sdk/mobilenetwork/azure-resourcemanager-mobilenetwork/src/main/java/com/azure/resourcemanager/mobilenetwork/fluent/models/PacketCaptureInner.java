f7c45a66d93063b68667205fe73a96a69e38f19c
SDKAuto <sdkautomation@microsoft.com>
2023-10-06 14:07:16 +0000

    CodeGen from PR 25627 in Azure/azure-rest-api-specs Merge 9cd30ce94423b971c0cafe9a5e73df8f4c77085c into 2b6c2c43ef43bf1d4c55c5f2a20010607de81b1e

--- sdk/mobilenetwork/azure-resourcemanager-mobilenetwork/src/main/java/com/azure/resourcemanager/mobilenetwork/fluent/models/PacketCaptureInner.java
+++ sdk/mobilenetwork/azure-resourcemanager-mobilenetwork/src/main/java/com/azure/resourcemanager/mobilenetwork/fluent/models/PacketCaptureInner.java
@@ -6,6 +6,7 @@ package com.azure.resourcemanager.mobilenetwork.fluent.models;
 
 import com.azure.core.annotation.Fluent;
 import com.azure.core.management.ProxyResource;
+import com.azure.core.management.SystemData;
 import com.azure.core.util.logging.ClientLogger;
 import com.azure.resourcemanager.mobilenetwork.models.PacketCaptureStatus;
 import com.azure.resourcemanager.mobilenetwork.models.ProvisioningState;
@@ -23,6 +24,12 @@ public final class PacketCaptureInner extends ProxyResource {
     @JsonProperty(value = "properties", required = true)
     private PacketCapturePropertiesFormat innerProperties = new PacketCapturePropertiesFormat();
 
+    /*
+     * Azure Resource Manager metadata containing createdBy and modifiedBy information.
+     */
+    @JsonProperty(value = "systemData", access = JsonProperty.Access.WRITE_ONLY)
+    private SystemData systemData;
+
     /** Creates an instance of PacketCaptureInner class. */
     public PacketCaptureInner() {
     }
@@ -36,6 +43,15 @@ public final class PacketCaptureInner extends ProxyResource {
      */
     private PacketCapturePropertiesFormat innerProperties() {
         return this.innerProperties;
+    }
+
+    /**
+     * Get the systemData property: Azure Resource Manager metadata containing createdBy and modifiedBy information.
+     *
+     * @return the systemData value.
+     */
+    public SystemData systemData() {
+        return this.systemData;
     }
 
     /**
