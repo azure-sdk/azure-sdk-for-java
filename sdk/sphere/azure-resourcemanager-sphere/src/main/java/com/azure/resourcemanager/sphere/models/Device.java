b75a86c73ebfce70c5742aafd4e5862d5611cb4c
SDKAuto <sdkautomation@microsoft.com>
2023-10-06 14:53:13 +0000

    CodeGen from PR 25627 in Azure/azure-rest-api-specs Merge 9cd30ce94423b971c0cafe9a5e73df8f4c77085c into 2b6c2c43ef43bf1d4c55c5f2a20010607de81b1e

--- sdk/sphere/azure-resourcemanager-sphere/src/main/java/com/azure/resourcemanager/sphere/models/Device.java
+++ sdk/sphere/azure-resourcemanager-sphere/src/main/java/com/azure/resourcemanager/sphere/models/Device.java
@@ -4,6 +4,7 @@
 
 package com.azure.resourcemanager.sphere.models;
 
+import com.azure.core.management.SystemData;
 import com.azure.core.util.Context;
 import com.azure.resourcemanager.sphere.fluent.models.DeviceInner;
 import java.time.OffsetDateTime;
@@ -30,6 +31,13 @@ public interface Device {
      * @return the type value.
      */
     String type();
+
+    /**
+     * Gets the systemData property: Azure Resource Manager metadata containing createdBy and modifiedBy information.
+     *
+     * @return the systemData value.
+     */
+    SystemData systemData();
 
     /**
      * Gets the deviceId property: Device ID.
