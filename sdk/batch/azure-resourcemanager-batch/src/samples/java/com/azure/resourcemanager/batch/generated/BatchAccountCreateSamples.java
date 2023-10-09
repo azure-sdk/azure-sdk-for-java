47474b15c36d239369f7f18abdf184809b1194e0
SDKAuto <sdkautomation@microsoft.com>
2023-10-09 23:59:05 +0000

    CodeGen from PR 26147 in Azure/azure-rest-api-specs Merge b60b9b9cc35af08612b93e3262169f3a89b57a70 into 950065151c9171ed96a2fcefba99185ad8b664d7

--- sdk/batch/azure-resourcemanager-batch/src/samples/java/com/azure/resourcemanager/batch/generated/BatchAccountCreateSamples.java
+++ sdk/batch/azure-resourcemanager-batch/src/samples/java/com/azure/resourcemanager/batch/generated/BatchAccountCreateSamples.java
@@ -141,6 +141,7 @@ public final class BatchAccountCreateSamples {
             .create();
     }
 
+    // Use "Map.of" if available
     @SuppressWarnings("unchecked")
     private static <T> Map<String, T> mapOf(Object... inputs) {
         Map<String, T> map = new HashMap<>();
