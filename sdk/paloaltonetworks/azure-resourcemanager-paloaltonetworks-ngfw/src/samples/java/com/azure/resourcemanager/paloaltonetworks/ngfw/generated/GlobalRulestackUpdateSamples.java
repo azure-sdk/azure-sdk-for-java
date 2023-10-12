68e620523fda0824a868d801136ed3519ed5eb8b
SDKAuto <sdkautomation@microsoft.com>
2023-10-12 00:57:48 +0000

    CodeGen from PR 25962 in Azure/azure-rest-api-specs Merge 54c8291c1e68e1fbdcb3f537392f54ed7c4b1b69 into 950065151c9171ed96a2fcefba99185ad8b664d7

--- sdk/paloaltonetworks/azure-resourcemanager-paloaltonetworks-ngfw/src/samples/java/com/azure/resourcemanager/paloaltonetworks/ngfw/generated/GlobalRulestackUpdateSamples.java
+++ sdk/paloaltonetworks/azure-resourcemanager-paloaltonetworks-ngfw/src/samples/java/com/azure/resourcemanager/paloaltonetworks/ngfw/generated/GlobalRulestackUpdateSamples.java
@@ -80,6 +80,7 @@ public final class GlobalRulestackUpdateSamples {
             .updateWithResponse("praval", new GlobalRulestackResourceUpdate(), com.azure.core.util.Context.NONE);
     }
 
+    // Use "Map.of" if available
     @SuppressWarnings("unchecked")
     private static <T> Map<String, T> mapOf(Object... inputs) {
         Map<String, T> map = new HashMap<>();
