70927552bbd68644e6c9e3b63076eca577b96d5f
SDKAuto <sdkautomation@microsoft.com>
2023-10-24 23:07:06 +0000

    CodeGen from PR 25958 in Azure/azure-rest-api-specs Merge 02cbb5abc20cf16ea73b3a952e4ca9de17f6227e into fa0011c95ee2903e69b5de9acb9888d82eae30c7

--- sdk/support/azure-resourcemanager-support/src/main/java/com/azure/resourcemanager/support/SupportManager.java
+++ sdk/support/azure-resourcemanager-support/src/main/java/com/azure/resourcemanager/support/SupportManager.java
@@ -259,7 +259,7 @@ public final class SupportManager {
                 .append("-")
                 .append("com.azure.resourcemanager.support")
                 .append("/")
-                .append("1.0.0-beta.3");
+                .append("1.0.0-beta.1");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder
                     .append(" (")
