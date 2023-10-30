10fdefb56b790998134b669a33a7844413b0a3a5
SDKAuto <sdkautomation@microsoft.com>
2023-10-30 05:23:50 +0000

    CodeGen from PR 26417 in Azure/azure-rest-api-specs Merge 49fa2e5f4cb2d970a12e18a8470958df17f4dfe2 into 5b821e97ef0de4cafe9a96353482106af2454f1d

--- sdk/managementgroups/azure-resourcemanager-managementgroups/src/samples/java/com/azure/resourcemanager/managementgroups/generated/ManagementGroupsListSamples.java
+++ sdk/managementgroups/azure-resourcemanager-managementgroups/src/samples/java/com/azure/resourcemanager/managementgroups/generated/ManagementGroupsListSamples.java
@@ -7,7 +7,7 @@ package com.azure.resourcemanager.managementgroups.generated;
 /** Samples for ManagementGroups List. */
 public final class ManagementGroupsListSamples {
     /*
-     * x-ms-original-file: specification/managementgroups/resource-manager/Microsoft.Management/stable/2021-04-01/examples/ListManagementGroups.json
+     * x-ms-original-file: specification/managementgroups/resource-manager/Microsoft.Management/stable/2023-04-01/examples/ListManagementGroups.json
      */
     /**
      * Sample code: ListManagementGroups.
