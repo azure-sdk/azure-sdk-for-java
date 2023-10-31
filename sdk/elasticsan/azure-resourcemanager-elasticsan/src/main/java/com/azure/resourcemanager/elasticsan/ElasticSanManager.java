f3d144fe8dc4bd3787da374110d7efa16d66ed9c
SDKAuto <sdkautomation@microsoft.com>
2023-10-31 23:47:54 +0000

    CodeGen from PR 26462 in Azure/azure-rest-api-specs Merge 50175f111e9c899249e79eb082a75fb8a7aba0e2 into c9a13dd38a8b73fe6c0eb5b83f09a611f4fec82e

--- sdk/elasticsan/azure-resourcemanager-elasticsan/src/main/java/com/azure/resourcemanager/elasticsan/ElasticSanManager.java
+++ sdk/elasticsan/azure-resourcemanager-elasticsan/src/main/java/com/azure/resourcemanager/elasticsan/ElasticSanManager.java
@@ -231,7 +231,7 @@ public final class ElasticSanManager {
                 .append("-")
                 .append("com.azure.resourcemanager.elasticsan")
                 .append("/")
-                .append("1.0.0-beta.3");
+                .append("1.0.0-beta.1");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder
                     .append(" (")
