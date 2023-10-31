57071e89fedbcf1ee27f60cf4606fa768eca8df0
SDKAuto <sdkautomation@microsoft.com>
2023-10-31 04:27:59 +0000

    CodeGen from PR 26427 in Azure/azure-rest-api-specs Merge ca53b46a98fe227af975a016ddb76898b2040482 into ceffc2870986ab280d7ee7e056297ca5b449abed

--- sdk/connectedvmware/azure-resourcemanager-connectedvmware/src/main/java/com/azure/resourcemanager/connectedvmware/ConnectedVMwareManager.java
+++ sdk/connectedvmware/azure-resourcemanager-connectedvmware/src/main/java/com/azure/resourcemanager/connectedvmware/ConnectedVMwareManager.java
@@ -247,7 +247,7 @@ public final class ConnectedVMwareManager {
                 .append("-")
                 .append("com.azure.resourcemanager.connectedvmware")
                 .append("/")
-                .append("1.0.0");
+                .append("1.0.0-beta.1");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder
                     .append(" (")
