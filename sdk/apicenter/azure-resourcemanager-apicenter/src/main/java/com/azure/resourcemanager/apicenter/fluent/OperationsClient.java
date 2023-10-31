957dc4ccfd6616be9125eb3e898f016443d8fbc8
SDKAuto <sdkautomation@microsoft.com>
2023-10-31 23:09:29 +0000

    CodeGen from PR 26462 in Azure/azure-rest-api-specs Merge fa446654d6f2bb46e0e230a8c69561480496929a into c9a13dd38a8b73fe6c0eb5b83f09a611f4fec82e

--- sdk/apicenter/azure-resourcemanager-apicenter/src/main/java/com/azure/resourcemanager/apicenter/fluent/OperationsClient.java
+++ sdk/apicenter/azure-resourcemanager-apicenter/src/main/java/com/azure/resourcemanager/apicenter/fluent/OperationsClient.java
@@ -13,7 +13,9 @@ import com.azure.resourcemanager.apicenter.fluent.models.OperationInner;
 /** An instance of this class provides access to all the operations defined in OperationsClient. */
 public interface OperationsClient {
     /**
-     * List the operations for the provider.
+     * List resource provider operations
+     *
+     * <p>Returns a collection of resource provider operations.
      *
      * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
      * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
@@ -24,7 +26,9 @@ public interface OperationsClient {
     PagedIterable<OperationInner> list();
 
     /**
-     * List the operations for the provider.
+     * List resource provider operations
+     *
+     * <p>Returns a collection of resource provider operations.
      *
      * @param context The context to associate with this operation.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
