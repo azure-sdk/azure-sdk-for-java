f35a58132798bf2b96503b6e56c3c3759ef7fdd9
SDKAuto <sdkautomation@microsoft.com>
2023-10-31 23:12:32 +0000

    CodeGen from PR 26462 in Azure/azure-rest-api-specs Merge fa446654d6f2bb46e0e230a8c69561480496929a into c9a13dd38a8b73fe6c0eb5b83f09a611f4fec82e

--- sdk/kusto/azure-resourcemanager-kusto/src/main/java/com/azure/resourcemanager/kusto/KustoManager.java
+++ sdk/kusto/azure-resourcemanager-kusto/src/main/java/com/azure/resourcemanager/kusto/KustoManager.java
@@ -267,7 +267,7 @@ public final class KustoManager {
                 .append("-")
                 .append("com.azure.resourcemanager.kusto")
                 .append("/")
-                .append("1.0.0");
+                .append("1.0.0-beta.1");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder
                     .append(" (")
