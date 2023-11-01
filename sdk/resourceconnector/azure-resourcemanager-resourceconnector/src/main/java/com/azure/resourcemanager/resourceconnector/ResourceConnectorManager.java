97b11e0e1fe7a2b4bdc6c0795ba744781d613b6b
SDKAuto <sdkautomation@microsoft.com>
2023-11-01 00:44:47 +0000

    CodeGen from PR 26499 in Azure/azure-rest-api-specs Merge 326fa9bc024ab4751a4abfe545cf59de05c55d72 into c3d50e902abe789604a79cd5fd38c058c62b086e

--- sdk/resourceconnector/azure-resourcemanager-resourceconnector/src/main/java/com/azure/resourcemanager/resourceconnector/ResourceConnectorManager.java
+++ sdk/resourceconnector/azure-resourcemanager-resourceconnector/src/main/java/com/azure/resourcemanager/resourceconnector/ResourceConnectorManager.java
@@ -203,7 +203,7 @@ public final class ResourceConnectorManager {
                 .append("-")
                 .append("com.azure.resourcemanager.resourceconnector")
                 .append("/")
-                .append("1.0.0");
+                .append("1.0.0-beta.1");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder
                     .append(" (")
