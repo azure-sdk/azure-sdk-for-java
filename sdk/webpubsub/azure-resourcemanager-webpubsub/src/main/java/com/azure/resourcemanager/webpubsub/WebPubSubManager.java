f69f65ec8b309356ffdb870eeb574b1280e9143d
SDKAuto <sdkautomation@microsoft.com>
2023-11-01 01:09:11 +0000

    CodeGen from PR 26499 in Azure/azure-rest-api-specs Merge 326fa9bc024ab4751a4abfe545cf59de05c55d72 into c3d50e902abe789604a79cd5fd38c058c62b086e

--- sdk/webpubsub/azure-resourcemanager-webpubsub/src/main/java/com/azure/resourcemanager/webpubsub/WebPubSubManager.java
+++ sdk/webpubsub/azure-resourcemanager-webpubsub/src/main/java/com/azure/resourcemanager/webpubsub/WebPubSubManager.java
@@ -239,7 +239,7 @@ public final class WebPubSubManager {
                 .append("-")
                 .append("com.azure.resourcemanager.webpubsub")
                 .append("/")
-                .append("1.1.0-beta.2");
+                .append("1.0.0-beta.1");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder
                     .append(" (")
