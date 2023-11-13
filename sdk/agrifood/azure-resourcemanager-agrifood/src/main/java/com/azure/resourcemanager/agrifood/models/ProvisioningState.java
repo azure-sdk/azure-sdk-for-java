301a2eeb538702210528531fa85defbcbc0d6ef1
SDKAuto <sdkautomation@microsoft.com>
2023-11-13 20:04:21 +0000

    CodeGen from PR 3088 in test-repo-billy/azure-rest-api-specs Merge 3d77ed54e3febcc8d176fb706e7ef0d7221f33f3 into 4b4e0b08aca18fa159396e290e3ef559416a4cb3

--- sdk/agrifood/azure-resourcemanager-agrifood/src/main/java/com/azure/resourcemanager/agrifood/models/ProvisioningState.java
+++ sdk/agrifood/azure-resourcemanager-agrifood/src/main/java/com/azure/resourcemanager/agrifood/models/ProvisioningState.java
@@ -8,7 +8,7 @@ import com.azure.core.util.ExpandableStringEnum;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import java.util.Collection;
 
-/** Sensor integration instance provisioning state. */
+/** Data Manager For Agriculture instance provisioning state. */
 public final class ProvisioningState extends ExpandableStringEnum<ProvisioningState> {
     /** Static value Creating for ProvisioningState. */
     public static final ProvisioningState CREATING = fromString("Creating");
@@ -24,6 +24,18 @@ public final class ProvisioningState extends ExpandableStringEnum<ProvisioningSt
 
     /** Static value Failed for ProvisioningState. */
     public static final ProvisioningState FAILED = fromString("Failed");
+
+    /** Static value Running for ProvisioningState. */
+    public static final ProvisioningState RUNNING = fromString("Running");
+
+    /**
+     * Creates a new instance of ProvisioningState value.
+     *
+     * @deprecated Use the {@link #fromString(String)} factory method.
+     */
+    @Deprecated
+    public ProvisioningState() {
+    }
 
     /**
      * Creates or finds a ProvisioningState from its string representation.
