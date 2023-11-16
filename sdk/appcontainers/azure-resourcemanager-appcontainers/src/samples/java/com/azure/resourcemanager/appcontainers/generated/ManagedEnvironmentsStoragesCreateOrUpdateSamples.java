2008880212f7014b066d713381036e7a4f80bada
SDKAuto <sdkautomation@microsoft.com>
2023-11-16 14:22:11 +0000

    CodeGen from PR 26733 in Azure/azure-rest-api-specs Merge 094f3b71081a9d7759570b0b002c23d203ce7b45 into a4b7802e963b3a77b885d8c7c1616cd1870877fb

--- sdk/appcontainers/azure-resourcemanager-appcontainers/src/samples/java/com/azure/resourcemanager/appcontainers/generated/ManagedEnvironmentsStoragesCreateOrUpdateSamples.java
+++ sdk/appcontainers/azure-resourcemanager-appcontainers/src/samples/java/com/azure/resourcemanager/appcontainers/generated/ManagedEnvironmentsStoragesCreateOrUpdateSamples.java
@@ -8,30 +8,26 @@ import com.azure.resourcemanager.appcontainers.models.AccessMode;
 import com.azure.resourcemanager.appcontainers.models.AzureFileProperties;
 import com.azure.resourcemanager.appcontainers.models.ManagedEnvironmentStorageProperties;
 
-/** Samples for ManagedEnvironmentsStorages CreateOrUpdate. */
+/**
+ * Samples for ManagedEnvironmentsStorages CreateOrUpdate.
+ */
 public final class ManagedEnvironmentsStoragesCreateOrUpdateSamples {
     /*
-     * x-ms-original-file: specification/app/resource-manager/Microsoft.App/stable/2023-05-01/examples/ManagedEnvironmentsStorages_CreateOrUpdate.json
+     * x-ms-original-file: specification/app/resource-manager/Microsoft.App/preview/2023-08-01-preview/examples/
+     * ManagedEnvironmentsStorages_CreateOrUpdate.json
      */
     /**
      * Sample code: Create or update environments storage.
-     *
+     * 
      * @param manager Entry point to ContainerAppsApiManager.
      */
-    public static void createOrUpdateEnvironmentsStorage(
-        com.azure.resourcemanager.appcontainers.ContainerAppsApiManager manager) {
-        manager
-            .managedEnvironmentsStorages()
-            .define("jlaw-demo1")
+    public static void
+        createOrUpdateEnvironmentsStorage(com.azure.resourcemanager.appcontainers.ContainerAppsApiManager manager) {
+        manager.managedEnvironmentsStorages().define("jlaw-demo1")
             .withExistingManagedEnvironment("examplerg", "managedEnv")
-            .withProperties(
-                new ManagedEnvironmentStorageProperties()
-                    .withAzureFile(
-                        new AzureFileProperties()
-                            .withAccountName("account1")
-                            .withAccountKey("fakeTokenPlaceholder")
-                            .withAccessMode(AccessMode.READ_ONLY)
-                            .withShareName("share1")))
+            .withProperties(new ManagedEnvironmentStorageProperties().withAzureFile(
+                new AzureFileProperties().withAccountName("account1").withAccountKey("fakeTokenPlaceholder")
+                    .withAccessMode(AccessMode.READ_ONLY).withShareName("share1")))
             .create();
     }
 }
