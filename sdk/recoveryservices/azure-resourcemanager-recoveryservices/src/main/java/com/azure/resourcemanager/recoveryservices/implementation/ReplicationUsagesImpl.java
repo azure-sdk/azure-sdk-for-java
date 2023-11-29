635cc2e6eaa92ea20154105bbc5a018141e8b760
SDKAuto <sdkautomation@microsoft.com>
2023-11-29 09:18:29 +0000

    CodeGen from PR 26238 in Azure/azure-rest-api-specs Merge 792bc7986be891cfa57544899edb1271f5f0a4ca into 91ada2b6fbdb06456e440807e5b4706110930c49

--- sdk/recoveryservices/azure-resourcemanager-recoveryservices/src/main/java/com/azure/resourcemanager/recoveryservices/implementation/ReplicationUsagesImpl.java
+++ sdk/recoveryservices/azure-resourcemanager-recoveryservices/src/main/java/com/azure/resourcemanager/recoveryservices/implementation/ReplicationUsagesImpl.java
@@ -19,8 +19,7 @@ public final class ReplicationUsagesImpl implements ReplicationUsages {
 
     private final com.azure.resourcemanager.recoveryservices.RecoveryServicesManager serviceManager;
 
-    public ReplicationUsagesImpl(
-        ReplicationUsagesClient innerClient,
+    public ReplicationUsagesImpl(ReplicationUsagesClient innerClient,
         com.azure.resourcemanager.recoveryservices.RecoveryServicesManager serviceManager) {
         this.innerClient = innerClient;
         this.serviceManager = serviceManager;
