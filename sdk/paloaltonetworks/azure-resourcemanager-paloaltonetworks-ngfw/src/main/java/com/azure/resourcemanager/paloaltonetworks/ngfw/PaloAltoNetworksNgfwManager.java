135a3a527281f01b7bebdbc80c5a3fa7c6fd5394
SDKAuto <sdkautomation@microsoft.com>
2024-01-19 11:01:17 +0000

    CodeGen from PR 27458 in Azure/azure-rest-api-specs Merge 55c814b4dbf0cfb2dd5ed0806525da2186075e0c into 3c237ab0065adc1a69205b6de58a2d578884f60b

--- sdk/paloaltonetworks/azure-resourcemanager-paloaltonetworks-ngfw/src/main/java/com/azure/resourcemanager/paloaltonetworks/ngfw/PaloAltoNetworksNgfwManager.java
+++ sdk/paloaltonetworks/azure-resourcemanager-paloaltonetworks-ngfw/src/main/java/com/azure/resourcemanager/paloaltonetworks/ngfw/PaloAltoNetworksNgfwManager.java
@@ -251,7 +251,7 @@ public final class PaloAltoNetworksNgfwManager {
 
             StringBuilder userAgentBuilder = new StringBuilder();
             userAgentBuilder.append("azsdk-java").append("-").append("com.azure.resourcemanager.paloaltonetworks.ngfw")
-                .append("/").append("1.1.0");
+                .append("/").append("1.0.0-beta.1");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder.append(" (").append(Configuration.getGlobalConfiguration().get("java.version"))
                     .append("; ").append(Configuration.getGlobalConfiguration().get("os.name")).append("; ")
