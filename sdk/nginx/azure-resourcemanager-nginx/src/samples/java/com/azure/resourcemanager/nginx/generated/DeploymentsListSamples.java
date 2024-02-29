e1370b3f4085f6792b964811ee869fa14fcacb7c
SDKAuto <sdkautomation@microsoft.com>
2024-02-29 07:11:52 +0000

    CodeGen from PR 27988 in Azure/azure-rest-api-specs Merge ad93caf75d5ff2b6da4d60595926b152ee3779c1 into 45fe26fe8f0ed4885e9b3573fabf712d64d236a1

--- sdk/nginx/azure-resourcemanager-nginx/src/samples/java/com/azure/resourcemanager/nginx/generated/DeploymentsListSamples.java
+++ sdk/nginx/azure-resourcemanager-nginx/src/samples/java/com/azure/resourcemanager/nginx/generated/DeploymentsListSamples.java
@@ -10,7 +10,7 @@ package com.azure.resourcemanager.nginx.generated;
 public final class DeploymentsListSamples {
     /*
      * x-ms-original-file:
-     * specification/nginx/resource-manager/NGINX.NGINXPLUS/stable/2023-04-01/examples/Deployments_List.json
+     * specification/nginx/resource-manager/NGINX.NGINXPLUS/stable/2023-09-01/examples/Deployments_List.json
      */
     /**
      * Sample code: Deployments_List.
