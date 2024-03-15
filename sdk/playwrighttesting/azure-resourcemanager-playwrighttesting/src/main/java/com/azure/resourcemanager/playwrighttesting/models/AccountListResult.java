f4a58c83d66f6545b20dfd0b1d7a1b9044e177da
SDKAuto <sdkautomation@microsoft.com>
2024-03-15 21:20:07 +0000

    CodeGen from PR 28287 in Azure/azure-rest-api-specs Merge f4f163b13aa1a27a205e82eb53e92ae6c417bbb6 into a7f8fb628797436ba442c2b59afb082a4080a9cd

--- sdk/playwrighttesting/azure-resourcemanager-playwrighttesting/src/main/java/com/azure/resourcemanager/playwrighttesting/models/AccountListResult.java
+++ sdk/playwrighttesting/azure-resourcemanager-playwrighttesting/src/main/java/com/azure/resourcemanager/playwrighttesting/models/AccountListResult.java
@@ -24,7 +24,7 @@ public final class AccountListResult {
     /*
      * The link to the next page of items
      */
-    @JsonProperty(value = "nextLink")
+    @JsonProperty(value = "nextLink", access = JsonProperty.Access.WRITE_ONLY)
     private String nextLink;
 
     /**
@@ -60,17 +60,6 @@ public final class AccountListResult {
      */
     public String nextLink() {
         return this.nextLink;
-    }
-
-    /**
-     * Set the nextLink property: The link to the next page of items.
-     * 
-     * @param nextLink the nextLink value to set.
-     * @return the AccountListResult object itself.
-     */
-    public AccountListResult withNextLink(String nextLink) {
-        this.nextLink = nextLink;
-        return this;
     }
 
     /**
