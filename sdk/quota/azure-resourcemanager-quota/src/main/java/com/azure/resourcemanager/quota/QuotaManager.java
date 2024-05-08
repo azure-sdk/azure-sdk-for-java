acbee0c975b74f3bbcb717cb416a0fd47b62209e
SDKAuto <sdkautomation@microsoft.com>
2024-05-08 06:16:38 +0000

    CodeGen from PR 29027 in Azure/azure-rest-api-specs Merge c2a261d157f6495867b08c1e757399c1139d1cae into 0f21c214f0f5b4180ec71655b36b8fbd40fd1ceb

--- sdk/quota/azure-resourcemanager-quota/src/main/java/com/azure/resourcemanager/quota/QuotaManager.java
+++ sdk/quota/azure-resourcemanager-quota/src/main/java/com/azure/resourcemanager/quota/QuotaManager.java
@@ -253,7 +253,7 @@ public final class QuotaManager {
                 .append("-")
                 .append("com.azure.resourcemanager.quota")
                 .append("/")
-                .append("1.1.0-beta.1");
+                .append("1.0.0-beta.1");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder.append(" (")
                     .append(Configuration.getGlobalConfiguration().get("java.version"))
