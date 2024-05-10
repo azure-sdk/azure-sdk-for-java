51ca03024222932333f53e85276139b69a9a5f2f
SDKAuto <sdkautomation@microsoft.com>
2024-05-10 19:23:19 +0000

    CodeGen from PR 29051 in Azure/azure-rest-api-specs Merge 5212b175a7966d3b76f940a69cbcb44a4a2854b0 into f0cc3a15708d4e26a6ce3b5c70ffb6b39ff50196

--- sdk/migrationdiscoverysap/azure-resourcemanager-migrationdiscoverysap/src/main/java/com/azure/resourcemanager/migrationdiscoverysap/models/SapInstanceListResult.java
+++ sdk/migrationdiscoverysap/azure-resourcemanager-migrationdiscoverysap/src/main/java/com/azure/resourcemanager/migrationdiscoverysap/models/SapInstanceListResult.java
@@ -80,8 +80,8 @@ public final class SapInstanceListResult {
      */
     public void validate() {
         if (value() == null) {
-            throw LOGGER.logExceptionAsError(
-                new IllegalArgumentException("Missing required property value in model SapInstanceListResult"));
+            throw LOGGER.atError()
+                .log(new IllegalArgumentException("Missing required property value in model SapInstanceListResult"));
         } else {
             value().forEach(e -> e.validate());
         }
