3993ec41d9d352982bb5dad676007917c0489ca3
SDKAuto <sdkautomation@microsoft.com>
2024-05-14 04:52:37 +0000

    CodeGen from PR 3349 in test-repo-billy/azure-rest-api-specs Merge 935e8d5c4e3382f90879a6b914dabfdeb9b5a448 into 9789b00f225d650cdf2c6623967095ea21614520

--- sdk/astro/azure-resourcemanager-astro/src/main/java/com/azure/resourcemanager/astro/models/LiftrBaseUserDetails.java
+++ sdk/astro/azure-resourcemanager-astro/src/main/java/com/azure/resourcemanager/astro/models/LiftrBaseUserDetails.java
@@ -156,16 +156,17 @@ public final class LiftrBaseUserDetails {
      */
     public void validate() {
         if (firstName() == null) {
-            throw LOGGER.logExceptionAsError(
-                new IllegalArgumentException("Missing required property firstName in model LiftrBaseUserDetails"));
+            throw LOGGER.atError()
+                .log(new IllegalArgumentException("Missing required property firstName in model LiftrBaseUserDetails"));
         }
         if (lastName() == null) {
-            throw LOGGER.logExceptionAsError(
-                new IllegalArgumentException("Missing required property lastName in model LiftrBaseUserDetails"));
+            throw LOGGER.atError()
+                .log(new IllegalArgumentException("Missing required property lastName in model LiftrBaseUserDetails"));
         }
         if (emailAddress() == null) {
-            throw LOGGER.logExceptionAsError(
-                new IllegalArgumentException("Missing required property emailAddress in model LiftrBaseUserDetails"));
+            throw LOGGER.atError()
+                .log(new IllegalArgumentException(
+                    "Missing required property emailAddress in model LiftrBaseUserDetails"));
         }
     }
 
