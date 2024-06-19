bcc4a335d29298888326891da0dc398e7edf80a2
SDKAuto <sdkautomation@microsoft.com>
2024-06-19 08:27:02 +0000

    CodeGen from PR 3365 in test-repo-billy/azure-rest-api-specs Merge 99f83ef19548503a06e240d23e4e1f25b5927069 into a8c212bc18177ac4891ec93aa7b4c5c834c156e4

--- sdk/kusto/azure-resourcemanager-kusto/src/main/java/com/azure/resourcemanager/kusto/models/OperationsResultsLocationsGetHeaders.java
+++ sdk/kusto/azure-resourcemanager-kusto/src/main/java/com/azure/resourcemanager/kusto/models/OperationsResultsLocationsGetHeaders.java
@@ -7,15 +7,15 @@ package com.azure.resourcemanager.kusto.models;
 import com.azure.core.annotation.Fluent;
 import com.azure.core.http.HttpHeaderName;
 import com.azure.core.http.HttpHeaders;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
-/** The OperationsResultsLocationsGetHeaders model. */
+/**
+ * The OperationsResultsLocationsGetHeaders model.
+ */
 @Fluent
 public final class OperationsResultsLocationsGetHeaders {
     /*
      * The Azure-AsyncOperation property.
      */
-    @JsonProperty(value = "Azure-AsyncOperation")
     private String azureAsyncOperation;
 
     private static final HttpHeaderName AZURE_ASYNC_OPERATION = HttpHeaderName.fromString("Azure-AsyncOperation");
@@ -23,7 +23,7 @@ public final class OperationsResultsLocationsGetHeaders {
     // HttpHeaders containing the raw property values.
     /**
      * Creates an instance of OperationsResultsLocationsGetHeaders class.
-     *
+     * 
      * @param rawHeaders The raw HttpHeaders that will be used to create the property values.
      */
     public OperationsResultsLocationsGetHeaders(HttpHeaders rawHeaders) {
@@ -32,7 +32,7 @@ public final class OperationsResultsLocationsGetHeaders {
 
     /**
      * Get the azureAsyncOperation property: The Azure-AsyncOperation property.
-     *
+     * 
      * @return the azureAsyncOperation value.
      */
     public String azureAsyncOperation() {
@@ -41,7 +41,7 @@ public final class OperationsResultsLocationsGetHeaders {
 
     /**
      * Set the azureAsyncOperation property: The Azure-AsyncOperation property.
-     *
+     * 
      * @param azureAsyncOperation the azureAsyncOperation value to set.
      * @return the OperationsResultsLocationsGetHeaders object itself.
      */
@@ -52,7 +52,7 @@ public final class OperationsResultsLocationsGetHeaders {
 
     /**
      * Validates the instance.
-     *
+     * 
      * @throws IllegalArgumentException thrown if the instance is not valid.
      */
     public void validate() {
