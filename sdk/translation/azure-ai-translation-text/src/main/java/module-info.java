027072d586625a10a34cd0c909416f3f60f877f5
SDKAuto <sdkautomation@microsoft.com>
2024-06-19 19:29:57 +0000

    CodeGen from PR 29509 in Azure/azure-rest-api-specs Merge 2634a16db1f1116ab520d1d5ba61fbb55b8b2fb8 into b48d5d72073a296514d3d4db77887d8711526ccc

--- sdk/translation/azure-ai-translation-text/src/main/java/module-info.java
+++ sdk/translation/azure-ai-translation-text/src/main/java/module-info.java
@@ -6,5 +6,6 @@ module com.azure.ai.translation.text {
     requires transitive com.azure.core;
     exports com.azure.ai.translation.text;
     exports com.azure.ai.translation.text.models;
+    opens com.azure.ai.translation.text.models to com.azure.core;
     opens com.azure.ai.translation.text.models to com.azure.core, com.fasterxml.jackson.databind;
 }
