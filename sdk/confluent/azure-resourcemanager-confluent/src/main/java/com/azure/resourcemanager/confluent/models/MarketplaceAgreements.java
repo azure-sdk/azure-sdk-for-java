5840d7884309722caca495b7ce690c82dbeb17b2
SDKAuto <sdkautomation@microsoft.com>
2024-06-21 07:41:24 +0000

    CodeGen from PR 29540 in Azure/azure-rest-api-specs Merge d55db2dba501613cd6e66bf423d26d0fc255e108 into 7753cb8917f0968713c013a1f25875e8bd8dc492

--- sdk/confluent/azure-resourcemanager-confluent/src/main/java/com/azure/resourcemanager/confluent/models/MarketplaceAgreements.java
+++ sdk/confluent/azure-resourcemanager-confluent/src/main/java/com/azure/resourcemanager/confluent/models/MarketplaceAgreements.java
@@ -4,7 +4,6 @@
 
 package com.azure.resourcemanager.confluent.models;
 
-import com.azure.core.http.rest.PagedIterable;
 import com.azure.core.http.rest.Response;
 import com.azure.core.util.Context;
 import com.azure.resourcemanager.confluent.fluent.models.ConfluentAgreementResourceInner;
@@ -13,26 +12,6 @@ import com.azure.resourcemanager.confluent.fluent.models.ConfluentAgreementResou
  * Resource collection API of MarketplaceAgreements.
  */
 public interface MarketplaceAgreements {
-    /**
-     * List Confluent marketplace agreements in the subscription.
-     * 
-     * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
-     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return response of a list operation as paginated response with {@link PagedIterable}.
-     */
-    PagedIterable<ConfluentAgreementResource> list();
-
-    /**
-     * List Confluent marketplace agreements in the subscription.
-     * 
-     * @param context The context to associate with this operation.
-     * @throws IllegalArgumentException thrown if parameters fail the validation.
-     * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
-     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return response of a list operation as paginated response with {@link PagedIterable}.
-     */
-    PagedIterable<ConfluentAgreementResource> list(Context context);
-
     /**
      * Create Confluent Marketplace agreement in the subscription.
      * 
