b39abd9309b3d985d9b45b394233737c6c90fd5a
SDKAuto <sdkautomation@microsoft.com>
2024-06-21 00:29:14 +0000

    CodeGen from PR 29228 in Azure/azure-rest-api-specs Merge 96171396bb586e4d56bb8c88a8717fd7e4f10c59 into d1296700aa6cd650970e9891dd58eef5698327fd

--- sdk/cognitiveservices/azure-resourcemanager-cognitiveservices/src/main/java/module-info.java
+++ sdk/cognitiveservices/azure-resourcemanager-cognitiveservices/src/main/java/module-info.java
@@ -4,16 +4,10 @@
 
 module com.azure.resourcemanager.cognitiveservices {
     requires transitive com.azure.core.management;
-
     exports com.azure.resourcemanager.cognitiveservices;
     exports com.azure.resourcemanager.cognitiveservices.fluent;
     exports com.azure.resourcemanager.cognitiveservices.fluent.models;
     exports com.azure.resourcemanager.cognitiveservices.models;
-
-    opens com.azure.resourcemanager.cognitiveservices.fluent.models to
-        com.azure.core,
-        com.fasterxml.jackson.databind;
-    opens com.azure.resourcemanager.cognitiveservices.models to
-        com.azure.core,
-        com.fasterxml.jackson.databind;
+    opens com.azure.resourcemanager.cognitiveservices.fluent.models to com.azure.core, com.fasterxml.jackson.databind;
+    opens com.azure.resourcemanager.cognitiveservices.models to com.azure.core, com.fasterxml.jackson.databind;
 }
