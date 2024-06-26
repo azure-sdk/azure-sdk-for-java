c3d793cb620205e9901c93a00d901cddc2ae65b2
SDKAuto <sdkautomation@microsoft.com>
2024-06-26 17:18:50 +0000

    CodeGen from PR 29510 in Azure/azure-rest-api-specs Merge 658b7864e30fa36c698ff4691a701c3664bbf1e8 into 2b6c728b10f0d8eca8bad4e7f009e38334ea7f1c

--- sdk/hybridcompute/azure-resourcemanager-hybridcompute/src/samples/java/com/azure/resourcemanager/hybridcompute/generated/NetworkSecurityPerimeterConfigurationsReconcileForPrivateLinkScopeSamples.java
+++ sdk/hybridcompute/azure-resourcemanager-hybridcompute/src/samples/java/com/azure/resourcemanager/hybridcompute/generated/NetworkSecurityPerimeterConfigurationsReconcileForPrivateLinkScopeSamples.java
@@ -5,23 +5,23 @@
 package com.azure.resourcemanager.hybridcompute.generated;
 
 /**
- * Samples for NetworkSecurityPerimeterConfigurations GetByPrivateLinkScope.
+ * Samples for NetworkSecurityPerimeterConfigurations ReconcileForPrivateLinkScope.
  */
-public final class NetworkSecurityPerimeterConfigurationsGetByPrivateLinkScopeSamples {
+public final class NetworkSecurityPerimeterConfigurationsReconcileForPrivateLinkScopeSamples {
     /*
      * x-ms-original-file:
      * specification/hybridcompute/resource-manager/Microsoft.HybridCompute/preview/2024-05-20-preview/examples/
-     * networkSecurityPerimeterConfiguration/NetworkSecurityPerimeterConfigurationGet.json
+     * networkSecurityPerimeterConfiguration/NetworkSecurityPerimeterConfigurationReconcile.json
      */
     /**
-     * Sample code: Gets the network security perimeter configuration of the private link scope.
+     * Sample code: Reconciles the network security perimeter configuration of the private link scope.
      * 
      * @param manager Entry point to HybridComputeManager.
      */
-    public static void getsTheNetworkSecurityPerimeterConfigurationOfThePrivateLinkScope(
+    public static void reconcilesTheNetworkSecurityPerimeterConfigurationOfThePrivateLinkScope(
         com.azure.resourcemanager.hybridcompute.HybridComputeManager manager) {
         manager.networkSecurityPerimeterConfigurations()
-            .getByPrivateLinkScopeWithResponse("my-resource-group", "my-privatelinkscope",
+            .reconcileForPrivateLinkScope("my-resource-group", "my-privatelinkscope",
                 "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee.myAssociation", com.azure.core.util.Context.NONE);
     }
 }
