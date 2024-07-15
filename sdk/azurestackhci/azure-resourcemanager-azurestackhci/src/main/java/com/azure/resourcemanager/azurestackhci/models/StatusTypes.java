9701be7d7232df7eceef1d059036a0a3ec58718d
SDKAuto <sdkautomation@microsoft.com>
2024-07-15 06:49:35 +0000

    CodeGen from PR 29814 in Azure/azure-rest-api-specs Merge 3c730ee0a936c8f9809cd56ad6791b2588be76b8 into 1aa912658531534e4e57ea613591075f7b97897c

--- sdk/azurestackhci/azure-resourcemanager-azurestackhci/src/main/java/com/azure/resourcemanager/azurestackhci/models/StatusTypes.java
+++ sdk/azurestackhci/azure-resourcemanager-azurestackhci/src/main/java/com/azure/resourcemanager/azurestackhci/models/StatusTypes.java
@@ -5,23 +5,30 @@
 package com.azure.resourcemanager.azurestackhci.models;
 
 import com.azure.core.util.ExpandableStringEnum;
-import com.fasterxml.jackson.annotation.JsonCreator;
 import java.util.Collection;
 
-/** The installation status of the hybrid machine agent installation. */
+/**
+ * The installation status of the hybrid machine agent installation.
+ */
 public final class StatusTypes extends ExpandableStringEnum<StatusTypes> {
-    /** Static value Succeeded for StatusTypes. */
+    /**
+     * Static value Succeeded for StatusTypes.
+     */
     public static final StatusTypes SUCCEEDED = fromString("Succeeded");
 
-    /** Static value InProgress for StatusTypes. */
+    /**
+     * Static value InProgress for StatusTypes.
+     */
     public static final StatusTypes IN_PROGRESS = fromString("InProgress");
 
-    /** Static value Failed for StatusTypes. */
+    /**
+     * Static value Failed for StatusTypes.
+     */
     public static final StatusTypes FAILED = fromString("Failed");
 
     /**
      * Creates a new instance of StatusTypes value.
-     *
+     * 
      * @deprecated Use the {@link #fromString(String)} factory method.
      */
     @Deprecated
@@ -30,18 +37,17 @@ public final class StatusTypes extends ExpandableStringEnum<StatusTypes> {
 
     /**
      * Creates or finds a StatusTypes from its string representation.
-     *
+     * 
      * @param name a name to look for.
      * @return the corresponding StatusTypes.
      */
-    @JsonCreator
     public static StatusTypes fromString(String name) {
         return fromString(name, StatusTypes.class);
     }
 
     /**
      * Gets known StatusTypes values.
-     *
+     * 
      * @return known StatusTypes values.
      */
     public static Collection<StatusTypes> values() {
