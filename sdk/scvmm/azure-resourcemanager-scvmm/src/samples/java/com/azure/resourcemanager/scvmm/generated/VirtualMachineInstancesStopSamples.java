9a7c1d1304cd693c36d255fe7f4d8f15e52f86fb
SDKAuto <sdkautomation@microsoft.com>
2024-07-19 06:58:25 +0000

    CodeGen from PR 29568 in Azure/azure-rest-api-specs Merge 90f963877b94a2472ee4c73bdabc9a46baae592f into 50ed4464b0e2a90e5d313b83bcb00a28762dd80f

--- sdk/scvmm/azure-resourcemanager-scvmm/src/samples/java/com/azure/resourcemanager/scvmm/generated/VirtualMachineInstancesStopSamples.java
+++ sdk/scvmm/azure-resourcemanager-scvmm/src/samples/java/com/azure/resourcemanager/scvmm/generated/VirtualMachineInstancesStopSamples.java
@@ -12,7 +12,7 @@ import com.azure.resourcemanager.scvmm.models.StopVirtualMachineOptions;
  */
 public final class VirtualMachineInstancesStopSamples {
     /*
-     * x-ms-original-file: specification/scvmm/resource-manager/Microsoft.ScVmm/stable/2023-10-07/examples/
+     * x-ms-original-file: specification/scvmm/resource-manager/Microsoft.ScVmm/stable/2024-06-01/examples/
      * VirtualMachineInstances_Stop_MaximumSet_Gen.json
      */
     /**
@@ -27,7 +27,7 @@ public final class VirtualMachineInstancesStopSamples {
     }
 
     /*
-     * x-ms-original-file: specification/scvmm/resource-manager/Microsoft.ScVmm/stable/2023-10-07/examples/
+     * x-ms-original-file: specification/scvmm/resource-manager/Microsoft.ScVmm/stable/2024-06-01/examples/
      * VirtualMachineInstances_Stop_MinimumSet_Gen.json
      */
     /**
