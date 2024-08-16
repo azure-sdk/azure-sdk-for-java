9e0ac0328c7f470903ed3aa1f061c6613f4bdb42
SDKAuto <sdkautomation@microsoft.com>
2024-08-16 12:36:28 +0000

    CodeGen from PR 3461 in test-repo-billy/azure-rest-api-specs Merge ae371fa064dfef97d1ae6ed7d1d0ca24e23d11c1 into 2ad400fcd7d7055b38957f542ffbe1a72290023e

--- sdk/oracledatabase/azure-resourcemanager-oracledatabase/src/main/java/com/azure/resourcemanager/oracledatabase/implementation/CloudVmClustersClientImpl.java
+++ sdk/oracledatabase/azure-resourcemanager-oracledatabase/src/main/java/com/azure/resourcemanager/oracledatabase/implementation/CloudVmClustersClientImpl.java
@@ -57,22 +57,22 @@ public final class CloudVmClustersClientImpl implements CloudVmClustersClient {
     /**
      * The service client containing this operation class.
      */
-    private final OracleDatabaseResourceManagerImpl client;
+    private final OracleDatabaseResourceManagerForTestingImpl client;
 
     /**
      * Initializes an instance of CloudVmClustersClientImpl.
      * 
      * @param client the instance of the service client containing this operation class.
      */
-    CloudVmClustersClientImpl(OracleDatabaseResourceManagerImpl client) {
+    CloudVmClustersClientImpl(OracleDatabaseResourceManagerForTestingImpl client) {
         this.service
             = RestProxy.create(CloudVmClustersService.class, client.getHttpPipeline(), client.getSerializerAdapter());
         this.client = client;
     }
 
     /**
-     * The interface defining all the services for OracleDatabaseResourceManagerCloudVmClusters to be used by the proxy
-     * service to perform REST calls.
+     * The interface defining all the services for OracleDatabaseResourceManagerForTestingCloudVmClusters to be used by
+     * the proxy service to perform REST calls.
      */
     @Host("{$host}")
     @ServiceInterface(name = "OracleDatabaseResour")
