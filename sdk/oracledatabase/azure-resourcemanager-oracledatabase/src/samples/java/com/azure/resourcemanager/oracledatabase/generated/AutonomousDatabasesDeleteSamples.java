1166671e025960668d0daa8083eaa0eb5258a2ab
SDKAuto <sdkautomation@microsoft.com>
2024-08-27 03:31:22 +0000

    CodeGen from PR 3461 in test-repo-billy/azure-rest-api-specs Merge c800dad7162baede2debc40d848ad348a8c7df56 into 2b91022c223ed3618c268c750f831b76fe0f4fb5

--- sdk/oracledatabase/azure-resourcemanager-oracledatabase/src/samples/java/com/azure/resourcemanager/oracledatabase/generated/AutonomousDatabasesDeleteSamples.java
+++ sdk/oracledatabase/azure-resourcemanager-oracledatabase/src/samples/java/com/azure/resourcemanager/oracledatabase/generated/AutonomousDatabasesDeleteSamples.java
@@ -10,7 +10,7 @@ package com.azure.resourcemanager.oracledatabase.generated;
 public final class AutonomousDatabasesDeleteSamples {
     /*
      * x-ms-original-file:
-     * specification/oracle/resource-manager/Oracle.Database/stable/2023-09-01/examples/autonomousDatabase_delete.json
+     * specification/oracle/resource-manager/Oracle.Database/stable/2024-06-01/examples/autonomousDatabase_delete.json
      */
     /**
      * Sample code: Delete Autonomous Database.
