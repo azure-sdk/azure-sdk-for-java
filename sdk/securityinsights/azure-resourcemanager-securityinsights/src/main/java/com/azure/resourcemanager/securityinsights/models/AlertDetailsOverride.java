bbf7c938d67df938955e603fbc588111ca7a16e5
SDKAuto <sdkautomation@microsoft.com>
2024-08-29 22:18:52 +0000

    CodeGen from PR 30375 in Azure/azure-rest-api-specs Merge 8431906f0c3b8bb5413de94aa4e1be2bd2062e55 into 06dcd0cafb47c39d5545b2c0b7bbdc223860b26b

--- sdk/securityinsights/azure-resourcemanager-securityinsights/src/main/java/com/azure/resourcemanager/securityinsights/models/AlertDetailsOverride.java
+++ sdk/securityinsights/azure-resourcemanager-securityinsights/src/main/java/com/azure/resourcemanager/securityinsights/models/AlertDetailsOverride.java
@@ -5,38 +5,52 @@
 package com.azure.resourcemanager.securityinsights.models;
 
 import com.azure.core.annotation.Fluent;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.azure.json.JsonReader;
+import com.azure.json.JsonSerializable;
+import com.azure.json.JsonToken;
+import com.azure.json.JsonWriter;
+import java.io.IOException;
+import java.util.List;
 
-/** Settings for how to dynamically override alert static details. */
+/**
+ * Settings for how to dynamically override alert static details.
+ */
 @Fluent
-public final class AlertDetailsOverride {
+public final class AlertDetailsOverride implements JsonSerializable<AlertDetailsOverride> {
     /*
      * the format containing columns name(s) to override the alert name
      */
-    @JsonProperty(value = "alertDisplayNameFormat")
     private String alertDisplayNameFormat;
 
     /*
      * the format containing columns name(s) to override the alert description
      */
-    @JsonProperty(value = "alertDescriptionFormat")
     private String alertDescriptionFormat;
 
     /*
      * the column name to take the alert tactics from
      */
-    @JsonProperty(value = "alertTacticsColumnName")
     private String alertTacticsColumnName;
 
     /*
      * the column name to take the alert severity from
      */
-    @JsonProperty(value = "alertSeverityColumnName")
     private String alertSeverityColumnName;
+
+    /*
+     * List of additional dynamic properties to override
+     */
+    private List<AlertPropertyMapping> alertDynamicProperties;
+
+    /**
+     * Creates an instance of AlertDetailsOverride class.
+     */
+    public AlertDetailsOverride() {
+    }
 
     /**
      * Get the alertDisplayNameFormat property: the format containing columns name(s) to override the alert name.
-     *
+     * 
      * @return the alertDisplayNameFormat value.
      */
     public String alertDisplayNameFormat() {
@@ -45,7 +59,7 @@ public final class AlertDetailsOverride {
 
     /**
      * Set the alertDisplayNameFormat property: the format containing columns name(s) to override the alert name.
-     *
+     * 
      * @param alertDisplayNameFormat the alertDisplayNameFormat value to set.
      * @return the AlertDetailsOverride object itself.
      */
@@ -56,7 +70,7 @@ public final class AlertDetailsOverride {
 
     /**
      * Get the alertDescriptionFormat property: the format containing columns name(s) to override the alert description.
-     *
+     * 
      * @return the alertDescriptionFormat value.
      */
     public String alertDescriptionFormat() {
@@ -65,7 +79,7 @@ public final class AlertDetailsOverride {
 
     /**
      * Set the alertDescriptionFormat property: the format containing columns name(s) to override the alert description.
-     *
+     * 
      * @param alertDescriptionFormat the alertDescriptionFormat value to set.
      * @return the AlertDetailsOverride object itself.
      */
@@ -76,7 +90,7 @@ public final class AlertDetailsOverride {
 
     /**
      * Get the alertTacticsColumnName property: the column name to take the alert tactics from.
-     *
+     * 
      * @return the alertTacticsColumnName value.
      */
     public String alertTacticsColumnName() {
@@ -85,7 +99,7 @@ public final class AlertDetailsOverride {
 
     /**
      * Set the alertTacticsColumnName property: the column name to take the alert tactics from.
-     *
+     * 
      * @param alertTacticsColumnName the alertTacticsColumnName value to set.
      * @return the AlertDetailsOverride object itself.
      */
@@ -96,7 +110,7 @@ public final class AlertDetailsOverride {
 
     /**
      * Get the alertSeverityColumnName property: the column name to take the alert severity from.
-     *
+     * 
      * @return the alertSeverityColumnName value.
      */
     public String alertSeverityColumnName() {
@@ -105,7 +119,7 @@ public final class AlertDetailsOverride {
 
     /**
      * Set the alertSeverityColumnName property: the column name to take the alert severity from.
-     *
+     * 
      * @param alertSeverityColumnName the alertSeverityColumnName value to set.
      * @return the AlertDetailsOverride object itself.
      */
@@ -115,10 +129,84 @@ public final class AlertDetailsOverride {
     }
 
     /**
+     * Get the alertDynamicProperties property: List of additional dynamic properties to override.
+     * 
+     * @return the alertDynamicProperties value.
+     */
+    public List<AlertPropertyMapping> alertDynamicProperties() {
+        return this.alertDynamicProperties;
+    }
+
+    /**
+     * Set the alertDynamicProperties property: List of additional dynamic properties to override.
+     * 
+     * @param alertDynamicProperties the alertDynamicProperties value to set.
+     * @return the AlertDetailsOverride object itself.
+     */
+    public AlertDetailsOverride withAlertDynamicProperties(List<AlertPropertyMapping> alertDynamicProperties) {
+        this.alertDynamicProperties = alertDynamicProperties;
+        return this;
+    }
+
+    /**
      * Validates the instance.
-     *
+     * 
      * @throws IllegalArgumentException thrown if the instance is not valid.
      */
     public void validate() {
+        if (alertDynamicProperties() != null) {
+            alertDynamicProperties().forEach(e -> e.validate());
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public JsonWriter toJson(JsonWriter jsonWriter) throws IOException {
+        jsonWriter.writeStartObject();
+        jsonWriter.writeStringField("alertDisplayNameFormat", this.alertDisplayNameFormat);
+        jsonWriter.writeStringField("alertDescriptionFormat", this.alertDescriptionFormat);
+        jsonWriter.writeStringField("alertTacticsColumnName", this.alertTacticsColumnName);
+        jsonWriter.writeStringField("alertSeverityColumnName", this.alertSeverityColumnName);
+        jsonWriter.writeArrayField("alertDynamicProperties", this.alertDynamicProperties,
+            (writer, element) -> writer.writeJson(element));
+        return jsonWriter.writeEndObject();
+    }
+
+    /**
+     * Reads an instance of AlertDetailsOverride from the JsonReader.
+     * 
+     * @param jsonReader The JsonReader being read.
+     * @return An instance of AlertDetailsOverride if the JsonReader was pointing to an instance of it, or null if it
+     * was pointing to JSON null.
+     * @throws IOException If an error occurs while reading the AlertDetailsOverride.
+     */
+    public static AlertDetailsOverride fromJson(JsonReader jsonReader) throws IOException {
+        return jsonReader.readObject(reader -> {
+            AlertDetailsOverride deserializedAlertDetailsOverride = new AlertDetailsOverride();
+            while (reader.nextToken() != JsonToken.END_OBJECT) {
+                String fieldName = reader.getFieldName();
+                reader.nextToken();
+
+                if ("alertDisplayNameFormat".equals(fieldName)) {
+                    deserializedAlertDetailsOverride.alertDisplayNameFormat = reader.getString();
+                } else if ("alertDescriptionFormat".equals(fieldName)) {
+                    deserializedAlertDetailsOverride.alertDescriptionFormat = reader.getString();
+                } else if ("alertTacticsColumnName".equals(fieldName)) {
+                    deserializedAlertDetailsOverride.alertTacticsColumnName = reader.getString();
+                } else if ("alertSeverityColumnName".equals(fieldName)) {
+                    deserializedAlertDetailsOverride.alertSeverityColumnName = reader.getString();
+                } else if ("alertDynamicProperties".equals(fieldName)) {
+                    List<AlertPropertyMapping> alertDynamicProperties
+                        = reader.readArray(reader1 -> AlertPropertyMapping.fromJson(reader1));
+                    deserializedAlertDetailsOverride.alertDynamicProperties = alertDynamicProperties;
+                } else {
+                    reader.skipChildren();
+                }
+            }
+
+            return deserializedAlertDetailsOverride;
+        });
     }
 }
