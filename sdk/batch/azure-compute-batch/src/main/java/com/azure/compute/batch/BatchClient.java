9d6a3c5f6f08f1bc53a4310a908fe90ebc2d3871
SDKAuto <sdkautomation@microsoft.com>
2024-09-13 00:41:34 +0000

    CodeGen from PR 30497 in Azure/azure-rest-api-specs Merge d92c15fbe64813b2e2ef070ec74b7ba5782717cb into 2d614982e89de434fd8a7d96e7080c1d87271037

--- sdk/batch/azure-compute-batch/src/main/java/com/azure/compute/batch/BatchClient.java
+++ sdk/batch/azure-compute-batch/src/main/java/com/azure/compute/batch/BatchClient.java
@@ -21510,7 +21510,7 @@ public final class BatchClient {
      *     properties (Optional): {
      *         creationTime: OffsetDateTime (Optional)
      *         lastModified: OffsetDateTime (Required)
-     *         contentLength: long (Required)
+     *         contentLength: String (Required)
      *         contentType: String (Optional)
      *         fileMode: String (Optional)
      *     }
@@ -23621,7 +23621,7 @@ public final class BatchClient {
      *     properties (Optional): {
      *         creationTime: OffsetDateTime (Optional)
      *         lastModified: OffsetDateTime (Required)
-     *         contentLength: long (Required)
+     *         contentLength: String (Required)
      *         contentType: String (Optional)
      *         fileMode: String (Optional)
      *     }
@@ -27070,7 +27070,7 @@ public final class BatchClient {
      *
      * @param timeOutInSeconds The maximum time that the server can spend processing the request, in seconds. The
      * default is 30 seconds. If the value is larger than 30, the default will be used instead.".
-     * @param starttime The earliest time from which to include metrics. This must be at least two and
+     * @param startTime The earliest time from which to include metrics. This must be at least two and
      * a half hours before the current time. If not specified this defaults to the
      * start time of the last aggregation interval currently available.
      * @param endtime The latest time from which to include metrics. This must be at least two hours
@@ -27090,14 +27090,14 @@ public final class BatchClient {
     @Generated
     @ServiceMethod(returns = ReturnType.COLLECTION)
     PagedIterable<BatchPoolUsageMetrics> listPoolUsageMetricsInternal(Integer timeOutInSeconds,
-        OffsetDateTime starttime, OffsetDateTime endtime, String filter) {
+        OffsetDateTime startTime, OffsetDateTime endtime, String filter) {
         // Generated convenience method for listPoolUsageMetricsInternal
         RequestOptions requestOptions = new RequestOptions();
         if (timeOutInSeconds != null) {
             requestOptions.addQueryParam("timeOut", String.valueOf(timeOutInSeconds), false);
         }
-        if (starttime != null) {
-            requestOptions.addQueryParam("startTime", String.valueOf(starttime), false);
+        if (startTime != null) {
+            requestOptions.addQueryParam("startTime", String.valueOf(startTime), false);
         }
         if (endtime != null) {
             requestOptions.addQueryParam("endtime", String.valueOf(endtime), false);
