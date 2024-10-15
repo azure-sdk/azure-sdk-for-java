7bb6857b37cc1b81737ae0236b5031c3bb1e7653
SDKAuto <sdkautomation@microsoft.com>
2024-10-15 16:58:19 +0000

    CodeGen from PR 30957 in Azure/azure-rest-api-specs Merge 48cad6945210190710ca7ecbbc5671afa25aa403 into d54bbb5ba9b27c228fe43063c5c2f0eab67bc361

--- sdk/machinelearning/azure-resourcemanager-machinelearning/src/main/java/com/azure/resourcemanager/machinelearning/implementation/RegistryCodeContainersClientImpl.java
+++ sdk/machinelearning/azure-resourcemanager-machinelearning/src/main/java/com/azure/resourcemanager/machinelearning/implementation/RegistryCodeContainersClientImpl.java
@@ -50,22 +50,22 @@ public final class RegistryCodeContainersClientImpl implements RegistryCodeConta
     /**
      * The service client containing this operation class.
      */
-    private final AzureMachineLearningWorkspacesImpl client;
+    private final AzureMachineLearningServicesImpl client;
 
     /**
      * Initializes an instance of RegistryCodeContainersClientImpl.
      * 
      * @param client the instance of the service client containing this operation class.
      */
-    RegistryCodeContainersClientImpl(AzureMachineLearningWorkspacesImpl client) {
+    RegistryCodeContainersClientImpl(AzureMachineLearningServicesImpl client) {
         this.service = RestProxy.create(RegistryCodeContainersService.class, client.getHttpPipeline(),
             client.getSerializerAdapter());
         this.client = client;
     }
 
     /**
-     * The interface defining all the services for AzureMachineLearningWorkspacesRegistryCodeContainers to be used by
-     * the proxy service to perform REST calls.
+     * The interface defining all the services for AzureMachineLearningServicesRegistryCodeContainers to be used by the
+     * proxy service to perform REST calls.
      */
     @Host("{$host}")
     @ServiceInterface(name = "AzureMachineLearning")
