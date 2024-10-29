e92eca1ff3e570f90628056a0d46bff4ac0d7004
SDKAuto <sdkautomation@microsoft.com>
2024-10-29 07:56:02 +0000

    CodeGen from PR 31303 in Azure/azure-rest-api-specs Merge 7037b9349a8b7018399ba12d1ec2c1d6d375c7ad into 4e3820497caa29ed0efada28a38cb4292cea9d55

--- sdk/extendedlocation/azure-resourcemanager-extendedlocation/src/main/java/com/azure/resourcemanager/extendedlocation/CustomLocationsManager.java
+++ sdk/extendedlocation/azure-resourcemanager-extendedlocation/src/main/java/com/azure/resourcemanager/extendedlocation/CustomLocationsManager.java
@@ -209,7 +209,7 @@ public final class CustomLocationsManager {
                 .append("-")
                 .append("com.azure.resourcemanager.extendedlocation")
                 .append("/")
-                .append("1.0.0-beta.3");
+                .append("1.0.0-beta.1");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder.append(" (")
                     .append(Configuration.getGlobalConfiguration().get("java.version"))
