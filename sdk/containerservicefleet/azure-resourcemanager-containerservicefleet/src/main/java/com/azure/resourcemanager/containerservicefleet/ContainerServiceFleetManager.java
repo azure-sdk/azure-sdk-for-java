bdb8a084d3c2ec54956678fc9698298951ed7a9f
SDKAuto <sdkautomation@microsoft.com>
2024-10-31 03:15:11 +0000

    CodeGen from PR 31355 in Azure/azure-rest-api-specs Merge 337462d51278e61dc3a73c27cd89c3cf833d41d2 into 400510ae981419169f35012c3a217b268e779b2b

--- sdk/containerservicefleet/azure-resourcemanager-containerservicefleet/src/main/java/com/azure/resourcemanager/containerservicefleet/ContainerServiceFleetManager.java
+++ sdk/containerservicefleet/azure-resourcemanager-containerservicefleet/src/main/java/com/azure/resourcemanager/containerservicefleet/ContainerServiceFleetManager.java
@@ -226,7 +226,7 @@ public final class ContainerServiceFleetManager {
                 .append("-")
                 .append("com.azure.resourcemanager.containerservicefleet")
                 .append("/")
-                .append("1.2.0-beta.1");
+                .append("1.0.0-beta.1");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder.append(" (")
                     .append(Configuration.getGlobalConfiguration().get("java.version"))
