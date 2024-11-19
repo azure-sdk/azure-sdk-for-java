0b02f3f37291ea668a31f116542c264a3298e7da
SDKAuto <sdkautomation@microsoft.com>
2024-11-19 02:02:45 +0000

    CodeGen from PR 31583 in Azure/azure-rest-api-specs Merge 460120789f46307a312a2fd8a7bfdcd84d1e58af into 14f4f6b25fe74f90f4ee74fdac5cd7e05d419245

--- sdk/appcontainers/azure-resourcemanager-appcontainers/src/main/java/com/azure/resourcemanager/appcontainers/fluent/models/ManagedEnvironmentInner.java
+++ sdk/appcontainers/azure-resourcemanager-appcontainers/src/main/java/com/azure/resourcemanager/appcontainers/fluent/models/ManagedEnvironmentInner.java
@@ -283,6 +283,15 @@ public final class ManagedEnvironmentInner extends Resource {
     }
 
     /**
+     * Get the privateLinkDefaultDomain property: Private Link Default Domain Name for the environment.
+     * 
+     * @return the privateLinkDefaultDomain value.
+     */
+    public String privateLinkDefaultDomain() {
+        return this.innerProperties() == null ? null : this.innerProperties().privateLinkDefaultDomain();
+    }
+
+    /**
      * Get the staticIp property: Static IP of the Environment.
      * 
      * @return the staticIp value.
@@ -292,9 +301,8 @@ public final class ManagedEnvironmentInner extends Resource {
     }
 
     /**
-     * Get the appLogsConfiguration property: Cluster configuration which enables the log daemon to export
-     * app logs to a destination. Currently only "log-analytics" is
-     * supported.
+     * Get the appLogsConfiguration property: Cluster configuration which enables the log daemon to export app logs to
+     * configured destination.
      * 
      * @return the appLogsConfiguration value.
      */
@@ -303,9 +311,8 @@ public final class ManagedEnvironmentInner extends Resource {
     }
 
     /**
-     * Set the appLogsConfiguration property: Cluster configuration which enables the log daemon to export
-     * app logs to a destination. Currently only "log-analytics" is
-     * supported.
+     * Set the appLogsConfiguration property: Cluster configuration which enables the log daemon to export app logs to
+     * configured destination.
      * 
      * @param appLogsConfiguration the appLogsConfiguration value to set.
      * @return the ManagedEnvironmentInner object itself.
@@ -385,6 +392,29 @@ public final class ManagedEnvironmentInner extends Resource {
             this.innerProperties = new ManagedEnvironmentProperties();
         }
         this.innerProperties().withZoneRedundant(zoneRedundant);
+        return this;
+    }
+
+    /**
+     * Get the availabilityZones property: The list of availability zones to use for managed environment.
+     * 
+     * @return the availabilityZones value.
+     */
+    public List<String> availabilityZones() {
+        return this.innerProperties() == null ? null : this.innerProperties().availabilityZones();
+    }
+
+    /**
+     * Set the availabilityZones property: The list of availability zones to use for managed environment.
+     * 
+     * @param availabilityZones the availabilityZones value to set.
+     * @return the ManagedEnvironmentInner object itself.
+     */
+    public ManagedEnvironmentInner withAvailabilityZones(List<String> availabilityZones) {
+        if (this.innerProperties() == null) {
+            this.innerProperties = new ManagedEnvironmentProperties();
+        }
+        this.innerProperties().withAvailabilityZones(availabilityZones);
         return this;
     }
 
