ddf62fd32b120f5aba952b428a11c41b543d0b54
SDKAuto <sdkautomation@microsoft.com>
2024-12-04 07:32:08 +0000

    CodeGen from PR 3564 in test-repo-billy/azure-rest-api-specs Merge 867328d523719e230928efab0fc24e54a8b8bc72 into 53ed59041775b47e32970efc8eea6dc7c3b5de05

--- sdk/datafactory/azure-resourcemanager-datafactory/src/main/java/com/azure/resourcemanager/datafactory/DataFactoryManager.java
+++ sdk/datafactory/azure-resourcemanager-datafactory/src/main/java/com/azure/resourcemanager/datafactory/DataFactoryManager.java
@@ -11,15 +11,15 @@ import com.azure.core.http.HttpPipelineBuilder;
 import com.azure.core.http.HttpPipelinePosition;
 import com.azure.core.http.policy.AddDatePolicy;
 import com.azure.core.http.policy.AddHeadersFromContextPolicy;
-import com.azure.core.http.policy.HttpLoggingPolicy;
+import com.azure.core.http.policy.BearerTokenAuthenticationPolicy;
 import com.azure.core.http.policy.HttpLogOptions;
+import com.azure.core.http.policy.HttpLoggingPolicy;
 import com.azure.core.http.policy.HttpPipelinePolicy;
 import com.azure.core.http.policy.HttpPolicyProviders;
 import com.azure.core.http.policy.RequestIdPolicy;
 import com.azure.core.http.policy.RetryOptions;
 import com.azure.core.http.policy.RetryPolicy;
 import com.azure.core.http.policy.UserAgentPolicy;
-import com.azure.core.management.http.policy.ArmChallengeAuthenticationPolicy;
 import com.azure.core.management.profile.AzureProfile;
 import com.azure.core.util.Configuration;
 import com.azure.core.util.logging.ClientLogger;
@@ -40,11 +40,10 @@ import com.azure.resourcemanager.datafactory.implementation.IntegrationRuntimesI
 import com.azure.resourcemanager.datafactory.implementation.LinkedServicesImpl;
 import com.azure.resourcemanager.datafactory.implementation.ManagedPrivateEndpointsImpl;
 import com.azure.resourcemanager.datafactory.implementation.ManagedVirtualNetworksImpl;
-import com.azure.resourcemanager.datafactory.implementation.OperationsImpl;
 import com.azure.resourcemanager.datafactory.implementation.PipelineRunsImpl;
 import com.azure.resourcemanager.datafactory.implementation.PipelinesImpl;
-import com.azure.resourcemanager.datafactory.implementation.PrivateEndpointConnectionOperationsImpl;
 import com.azure.resourcemanager.datafactory.implementation.PrivateEndPointConnectionsImpl;
+import com.azure.resourcemanager.datafactory.implementation.PrivateEndpointConnectionOperationsImpl;
 import com.azure.resourcemanager.datafactory.implementation.PrivateLinkResourcesImpl;
 import com.azure.resourcemanager.datafactory.implementation.TriggerRunsImpl;
 import com.azure.resourcemanager.datafactory.implementation.TriggersImpl;
@@ -63,11 +62,10 @@ import com.azure.resourcemanager.datafactory.models.IntegrationRuntimes;
 import com.azure.resourcemanager.datafactory.models.LinkedServices;
 import com.azure.resourcemanager.datafactory.models.ManagedPrivateEndpoints;
 import com.azure.resourcemanager.datafactory.models.ManagedVirtualNetworks;
-import com.azure.resourcemanager.datafactory.models.Operations;
 import com.azure.resourcemanager.datafactory.models.PipelineRuns;
 import com.azure.resourcemanager.datafactory.models.Pipelines;
-import com.azure.resourcemanager.datafactory.models.PrivateEndpointConnectionOperations;
 import com.azure.resourcemanager.datafactory.models.PrivateEndPointConnections;
+import com.azure.resourcemanager.datafactory.models.PrivateEndpointConnectionOperations;
 import com.azure.resourcemanager.datafactory.models.PrivateLinkResources;
 import com.azure.resourcemanager.datafactory.models.TriggerRuns;
 import com.azure.resourcemanager.datafactory.models.Triggers;
@@ -84,11 +82,9 @@ import java.util.stream.Collectors;
  * V2 services.
  */
 public final class DataFactoryManager {
-    private Operations operations;
+    private ExposureControls exposureControls;
 
     private Factories factories;
-
-    private ExposureControls exposureControls;
 
     private IntegrationRuntimes integrationRuntimes;
 
@@ -294,7 +290,7 @@ public final class DataFactoryManager {
                 .append("-")
                 .append("com.azure.resourcemanager.datafactory")
                 .append("/")
-                .append("1.0.0-beta.30");
+                .append("1.0.0-beta.1");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder.append(" (")
                     .append(Configuration.getGlobalConfiguration().get("java.version"))
@@ -327,7 +323,7 @@ public final class DataFactoryManager {
             HttpPolicyProviders.addBeforeRetryPolicies(policies);
             policies.add(retryPolicy);
             policies.add(new AddDatePolicy());
-            policies.add(new ArmChallengeAuthenticationPolicy(credential, scopes.toArray(new String[0])));
+            policies.add(new BearerTokenAuthenticationPolicy(credential, scopes.toArray(new String[0])));
             policies.addAll(this.policies.stream()
                 .filter(p -> p.getPipelinePosition() == HttpPipelinePosition.PER_RETRY)
                 .collect(Collectors.toList()));
@@ -341,15 +337,15 @@ public final class DataFactoryManager {
     }
 
     /**
-     * Gets the resource collection API of Operations.
+     * Gets the resource collection API of ExposureControls.
      * 
-     * @return Resource collection API of Operations.
+     * @return Resource collection API of ExposureControls.
      */
-    public Operations operations() {
-        if (this.operations == null) {
-            this.operations = new OperationsImpl(clientObject.getOperations(), this);
+    public ExposureControls exposureControls() {
+        if (this.exposureControls == null) {
+            this.exposureControls = new ExposureControlsImpl(clientObject.getExposureControls(), this);
         }
-        return operations;
+        return exposureControls;
     }
 
     /**
@@ -362,18 +358,6 @@ public final class DataFactoryManager {
             this.factories = new FactoriesImpl(clientObject.getFactories(), this);
         }
         return factories;
-    }
-
-    /**
-     * Gets the resource collection API of ExposureControls.
-     * 
-     * @return Resource collection API of ExposureControls.
-     */
-    public ExposureControls exposureControls() {
-        if (this.exposureControls == null) {
-            this.exposureControls = new ExposureControlsImpl(clientObject.getExposureControls(), this);
-        }
-        return exposureControls;
     }
 
     /**
