3ac0e22c2a9a495d7e448a702141cf78de62a935
SDKAuto <sdkautomation@microsoft.com>
2024-12-18 04:32:21 +0000

    CodeGen from PR 3383 in test-repo-billy/azure-rest-api-specs Merge 24e8fc9a3de89c41812f7390e4fb266159ab2929 into 38f1c3fea49fef7f917ab06516df896f11b4741a

--- sdk/contentsafety/azure-ai-contentsafety/src/main/java/com/azure/ai/contentsafety/ContentSafetyAsyncClient.java
+++ sdk/contentsafety/azure-ai-contentsafety/src/main/java/com/azure/ai/contentsafety/ContentSafetyAsyncClient.java
@@ -52,7 +52,7 @@ public final class ContentSafetyAsyncClient {
      * <pre>
      * {@code
      * {
-     *     text: String (Required)
+     *     text2: String (Required)
      *     categories (Optional): [
      *         String(Hate/SelfHarm/Sexual/Violence) (Optional)
      *     ]
