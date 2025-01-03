f20ed391b8f9d938f142ee7f3b636e5042e51e2a
SDKAuto <sdkautomation@microsoft.com>
2025-01-03 06:21:44 +0000

    CodeGen from PR 31983 in Azure/azure-rest-api-specs Merge baedf1fcc56187649923066f77923cb525252f0a into c37cd44037491c96dccd02454195bd6f3411f026

--- sdk/largeinstance/azure-resourcemanager-largeinstance/src/main/java/com/azure/resourcemanager/largeinstance/LargeInstanceManager.java
+++ sdk/largeinstance/azure-resourcemanager-largeinstance/src/main/java/com/azure/resourcemanager/largeinstance/LargeInstanceManager.java
@@ -213,7 +213,7 @@ public final class LargeInstanceManager {
                 .append("-")
                 .append("com.azure.resourcemanager.largeinstance")
                 .append("/")
-                .append("1.0.0-beta.2");
+                .append("1.0.0-beta.1");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder.append(" (")
                     .append(Configuration.getGlobalConfiguration().get("java.version"))
