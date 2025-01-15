14e5152221a062a2d580b65e2f69207a9978ca27
SDKAuto <sdkautomation@microsoft.com>
2025-01-15 03:14:47 +0000

    CodeGen from PR 32146 in Azure/azure-rest-api-specs Merge 136344102858c6c2dfdc0b0f01101274b2f31443 into 3feb9e396d38c4c0479126d3f697ff86a6ce8693

--- sdk/webpubsub/azure-resourcemanager-webpubsub/CHANGELOG.md
+++ sdk/webpubsub/azure-resourcemanager-webpubsub/CHANGELOG.md
@@ -1,14 +1,8 @@
 # Release History
 
-## 1.2.0-beta.1 (Unreleased)
+## 1.0.0-beta.1 (2025-01-15)
 
-### Features Added
-
-### Breaking Changes
-
-### Bugs Fixed
-
-### Other Changes
+- Azure Resource Manager WebPubSub client library for Java. This package contains Microsoft Azure SDK for WebPubSub Management SDK. REST API for Azure WebPubSub Service. Package tag package-2024-03-01. For documentation on how to use this package, please see [Azure Management Libraries for Java](https://aka.ms/azsdk/java/mgmt).
 
 ## 1.1.0 (2024-09-26)
 
--- sdk/webpubsub/azure-resourcemanager-webpubsub/src/main/java/com/azure/resourcemanager/webpubsub/WebPubSubManager.java
+++ sdk/webpubsub/azure-resourcemanager-webpubsub/src/main/java/com/azure/resourcemanager/webpubsub/WebPubSubManager.java
@@ -11,15 +11,15 @@ import com.azure.core.http.HttpPipelineBuilder;
 import com.azure.core.http.HttpPipelinePosition;
 import com.azure.core.http.policy.AddDatePolicy;
 import com.azure.core.http.policy.AddHeadersFromContextPolicy;
-import com.azure.core.http.policy.HttpLoggingPolicy;
+import com.azure.core.http.policy.BearerTokenAuthenticationPolicy;
 import com.azure.core.http.policy.HttpLogOptions;
+import com.azure.core.http.policy.HttpLoggingPolicy;
 import com.azure.core.http.policy.HttpPipelinePolicy;
 import com.azure.core.http.policy.HttpPolicyProviders;
 import com.azure.core.http.policy.RequestIdPolicy;
 import com.azure.core.http.policy.RetryOptions;
 import com.azure.core.http.policy.RetryPolicy;
 import com.azure.core.http.policy.UserAgentPolicy;
-import com.azure.core.management.http.policy.ArmChallengeAuthenticationPolicy;
 import com.azure.core.management.profile.AzureProfile;
 import com.azure.core.util.Configuration;
 import com.azure.core.util.logging.ClientLogger;
@@ -43,10 +43,10 @@ import com.azure.resourcemanager.webpubsub.models.WebPubSubCustomDomains;
 import com.azure.resourcemanager.webpubsub.models.WebPubSubHubs;
 import com.azure.resourcemanager.webpubsub.models.WebPubSubPrivateEndpointConnections;
 import com.azure.resourcemanager.webpubsub.models.WebPubSubPrivateLinkResources;
-import com.azure.resourcemanager.webpubsub.models.WebPubSubReplicas;
 import com.azure.resourcemanager.webpubsub.models.WebPubSubReplicaSharedPrivateLinkResources;
-import com.azure.resourcemanager.webpubsub.models.WebPubSubs;
+import com.azure.resourcemanager.webpubsub.models.WebPubSubReplicas;
 import com.azure.resourcemanager.webpubsub.models.WebPubSubSharedPrivateLinkResources;
+import com.azure.resourcemanager.webpubsub.models.WebPubSubs;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
@@ -245,7 +245,7 @@ public final class WebPubSubManager {
                 .append("-")
                 .append("com.azure.resourcemanager.webpubsub")
                 .append("/")
-                .append("1.1.0");
+                .append("1.0.0-beta.1");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder.append(" (")
                     .append(Configuration.getGlobalConfiguration().get("java.version"))
@@ -278,7 +278,7 @@ public final class WebPubSubManager {
             HttpPolicyProviders.addBeforeRetryPolicies(policies);
             policies.add(retryPolicy);
             policies.add(new AddDatePolicy());
-            policies.add(new ArmChallengeAuthenticationPolicy(credential, scopes.toArray(new String[0])));
+            policies.add(new BearerTokenAuthenticationPolicy(credential, scopes.toArray(new String[0])));
             policies.addAll(this.policies.stream()
                 .filter(p -> p.getPipelinePosition() == HttpPipelinePosition.PER_RETRY)
                 .collect(Collectors.toList()));
