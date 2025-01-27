caa8f602dca3112e3427b27e9b25572103724c7b
SDKAuto <sdkautomation@microsoft.com>
2025-01-27 19:02:14 +0000

    CodeGen from PR 32346 in Azure/azure-rest-api-specs Merge 0b079e88e3e6486968e4ce0b5c643c9bb61f2b33 into bb9db37f6542f55284f3189ffbf4717005159ffe

--- sdk/storageactions/azure-resourcemanager-storageactions/src/main/java/com/azure/resourcemanager/storageactions/StorageActionsManager.java
+++ sdk/storageactions/azure-resourcemanager-storageactions/src/main/java/com/azure/resourcemanager/storageactions/StorageActionsManager.java
@@ -217,7 +217,7 @@ public final class StorageActionsManager {
                 .append("-")
                 .append("com.azure.resourcemanager.storageactions")
                 .append("/")
-                .append("1.0.0-beta.2");
+                .append("1.0.0-beta.1");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder.append(" (")
                     .append(Configuration.getGlobalConfiguration().get("java.version"))
