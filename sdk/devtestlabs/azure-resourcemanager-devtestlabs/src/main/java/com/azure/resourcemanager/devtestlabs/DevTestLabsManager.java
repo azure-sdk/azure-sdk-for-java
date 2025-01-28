56751d538db7ba78ae5353800e3d27d9b7a4c6db
SDKAuto <sdkautomation@microsoft.com>
2025-01-28 21:40:19 +0000

    CodeGen from PR 32366 in Azure/azure-rest-api-specs Merge 84b2e541bf19d043dd4a38db63e8958ac20c92bf into 175d71800b942faaa4113416d5e395b8ba19ce31

--- sdk/devtestlabs/azure-resourcemanager-devtestlabs/src/main/java/com/azure/resourcemanager/devtestlabs/DevTestLabsManager.java
+++ sdk/devtestlabs/azure-resourcemanager-devtestlabs/src/main/java/com/azure/resourcemanager/devtestlabs/DevTestLabsManager.java
@@ -301,7 +301,7 @@ public final class DevTestLabsManager {
                 .append("-")
                 .append("com.azure.resourcemanager.devtestlabs")
                 .append("/")
-                .append("1.1.0");
+                .append("1.0.0-beta.1");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder.append(" (")
                     .append(Configuration.getGlobalConfiguration().get("java.version"))
