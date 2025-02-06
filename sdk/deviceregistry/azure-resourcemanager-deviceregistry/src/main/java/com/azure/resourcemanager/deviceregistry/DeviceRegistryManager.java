e692c56f2cb0ad1d2d15ab9baf661a04dce5c411
SDKAuto <sdkautomation@microsoft.com>
2025-02-06 22:37:44 +0000

    CodeGen from PR 32479 in Azure/azure-rest-api-specs Merge e78d8c8fee1c5f56c41928114c15025c2cee0859 into cdd0874bc93c651e711e2f06a866e5f9358a443e

--- sdk/deviceregistry/azure-resourcemanager-deviceregistry/src/main/java/com/azure/resourcemanager/deviceregistry/DeviceRegistryManager.java
+++ sdk/deviceregistry/azure-resourcemanager-deviceregistry/src/main/java/com/azure/resourcemanager/deviceregistry/DeviceRegistryManager.java
@@ -241,7 +241,7 @@ public final class DeviceRegistryManager {
                 .append("-")
                 .append("com.azure.resourcemanager.deviceregistry")
                 .append("/")
-                .append("1.0.0-beta.1");
+                .append("1.0.0-beta.3");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder.append(" (")
                     .append(Configuration.getGlobalConfiguration().get("java.version"))
