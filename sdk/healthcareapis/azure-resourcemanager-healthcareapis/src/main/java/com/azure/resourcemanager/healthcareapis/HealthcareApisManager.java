1483dca3c1078c4d71642b87e9830984f6c6627e
SDKAuto <sdkautomation@microsoft.com>
2025-02-14 00:33:31 +0000

    CodeGen from PR 32617 in Azure/azure-rest-api-specs Merge 5381f88c7ab72a7de51d861c2081afd5f7dcd600 into bb0cd5fdf4d0427e8b41ffc95da404448dad22de

--- sdk/healthcareapis/azure-resourcemanager-healthcareapis/src/main/java/com/azure/resourcemanager/healthcareapis/HealthcareApisManager.java
+++ sdk/healthcareapis/azure-resourcemanager-healthcareapis/src/main/java/com/azure/resourcemanager/healthcareapis/HealthcareApisManager.java
@@ -253,7 +253,7 @@ public final class HealthcareApisManager {
                 .append("-")
                 .append("com.azure.resourcemanager.healthcareapis")
                 .append("/")
-                .append("1.2.0");
+                .append("1.0.0-beta.1");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder.append(" (")
                     .append(Configuration.getGlobalConfiguration().get("java.version"))
