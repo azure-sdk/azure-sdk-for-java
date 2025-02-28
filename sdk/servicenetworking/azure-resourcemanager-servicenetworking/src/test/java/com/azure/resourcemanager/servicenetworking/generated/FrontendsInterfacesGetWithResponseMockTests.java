f938efa4788daf3b66a2573f16a378292f89f5f0
SDKAuto <sdkautomation@microsoft.com>
2025-02-28 20:49:14 +0000

    CodeGen from PR 32919 in Azure/azure-rest-api-specs Merge 987e160e669bd8bd6ad3b479a44ec8f543a22cf8 into 8e015e1d91c33639940fb36fa7f2c357665ff6dc

--- sdk/servicenetworking/azure-resourcemanager-servicenetworking/src/test/java/com/azure/resourcemanager/servicenetworking/generated/FrontendsInterfacesGetWithResponseMockTests.java
+++ sdk/servicenetworking/azure-resourcemanager-servicenetworking/src/test/java/com/azure/resourcemanager/servicenetworking/generated/FrontendsInterfacesGetWithResponseMockTests.java
@@ -6,8 +6,8 @@ package com.azure.resourcemanager.servicenetworking.generated;
 
 import com.azure.core.credential.AccessToken;
 import com.azure.core.http.HttpClient;
-import com.azure.core.management.AzureEnvironment;
 import com.azure.core.management.profile.AzureProfile;
+import com.azure.core.models.AzureCloud;
 import com.azure.core.test.http.MockHttpResponse;
 import com.azure.resourcemanager.servicenetworking.TrafficControllerManager;
 import com.azure.resourcemanager.servicenetworking.models.Frontend;
@@ -28,7 +28,7 @@ public final class FrontendsInterfacesGetWithResponseMockTests {
         TrafficControllerManager manager = TrafficControllerManager.configure()
             .withHttpClient(httpClient)
             .authenticate(tokenRequestContext -> Mono.just(new AccessToken("this_is_a_token", OffsetDateTime.MAX)),
-                new AzureProfile("", "", AzureEnvironment.AZURE));
+                new AzureProfile("", "", AzureCloud.AZURE_PUBLIC_CLOUD));
 
         Frontend response = manager.frontendsInterfaces()
             .getWithResponse("ebrjcxe", "fuwutttxf", "jrbirphxepcyv", com.azure.core.util.Context.NONE)
