36725cd6b765b3fc601a7695b2c6a4e8dcdd7240
SDKAuto <sdkautomation@microsoft.com>
2025-03-05 23:36:08 +0000

    CodeGen from PR 32161 in Azure/azure-rest-api-specs Merge 2d524740d4c2215ac21e51fbdf8f275d680c7d22 into 7d27c9d8f5ae6a97280e93e412fa76d9c6ddcb68

--- sdk/hybridcompute/azure-resourcemanager-hybridcompute/src/samples/java/com/azure/resourcemanager/hybridcompute/generated/NetworkProfileGetSamples.java
+++ sdk/hybridcompute/azure-resourcemanager-hybridcompute/src/samples/java/com/azure/resourcemanager/hybridcompute/generated/NetworkProfileGetSamples.java
@@ -10,7 +10,7 @@ package com.azure.resourcemanager.hybridcompute.generated;
 public final class NetworkProfileGetSamples {
     /*
      * x-ms-original-file:
-     * specification/hybridcompute/resource-manager/Microsoft.HybridCompute/preview/2024-07-31-preview/examples/
+     * specification/hybridcompute/resource-manager/Microsoft.HybridCompute/stable/2025-01-13/examples/
      * NetworkProfile_Get.json
      */
     /**
