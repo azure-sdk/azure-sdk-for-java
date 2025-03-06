719d36d2e3306efa593ff8d275594d859a2f7997
SDKAuto <sdkautomation@microsoft.com>
2025-03-06 07:29:37 +0000

    CodeGen from PR 32490 in Azure/azure-rest-api-specs Merge 283c669793a723793b6c0a4459030104df952107 into 51ef3f866e57b1a516b6b32cb9921a3589283472

--- sdk/servicenetworking/azure-resourcemanager-servicenetworking/src/main/java/com/azure/resourcemanager/servicenetworking/models/FrontendUpdateProperties.java
+++ sdk/servicenetworking/azure-resourcemanager-servicenetworking/src/main/java/com/azure/resourcemanager/servicenetworking/models/FrontendUpdateProperties.java
@@ -12,39 +12,19 @@ import com.azure.json.JsonWriter;
 import java.io.IOException;
 
 /**
- * Frontend Properties.
+ * The updatable properties of the Frontend.
  */
 @Fluent
-public final class FrontendProperties implements JsonSerializable<FrontendProperties> {
-    /*
-     * The Fully Qualified Domain Name of the DNS record associated to a Traffic Controller frontend.
-     */
-    private String fqdn;
-
+public final class FrontendUpdateProperties implements JsonSerializable<FrontendUpdateProperties> {
     /*
      * Frontend Security Policy Configuration
      */
     private SecurityPolicyConfigurations securityPolicyConfigurations;
 
-    /*
-     * Provisioning State of Traffic Controller Frontend Resource
-     */
-    private ProvisioningState provisioningState;
-
     /**
-     * Creates an instance of FrontendProperties class.
+     * Creates an instance of FrontendUpdateProperties class.
      */
-    public FrontendProperties() {
-    }
-
-    /**
-     * Get the fqdn property: The Fully Qualified Domain Name of the DNS record associated to a Traffic Controller
-     * frontend.
-     * 
-     * @return the fqdn value.
-     */
-    public String fqdn() {
-        return this.fqdn;
+    public FrontendUpdateProperties() {
     }
 
     /**
@@ -60,21 +40,12 @@ public final class FrontendProperties implements JsonSerializable<FrontendProper
      * Set the securityPolicyConfigurations property: Frontend Security Policy Configuration.
      * 
      * @param securityPolicyConfigurations the securityPolicyConfigurations value to set.
-     * @return the FrontendProperties object itself.
+     * @return the FrontendUpdateProperties object itself.
      */
-    public FrontendProperties
+    public FrontendUpdateProperties
         withSecurityPolicyConfigurations(SecurityPolicyConfigurations securityPolicyConfigurations) {
         this.securityPolicyConfigurations = securityPolicyConfigurations;
         return this;
-    }
-
-    /**
-     * Get the provisioningState property: Provisioning State of Traffic Controller Frontend Resource.
-     * 
-     * @return the provisioningState value.
-     */
-    public ProvisioningState provisioningState() {
-        return this.provisioningState;
     }
 
     /**
@@ -99,33 +70,29 @@ public final class FrontendProperties implements JsonSerializable<FrontendProper
     }
 
     /**
-     * Reads an instance of FrontendProperties from the JsonReader.
+     * Reads an instance of FrontendUpdateProperties from the JsonReader.
      * 
      * @param jsonReader The JsonReader being read.
-     * @return An instance of FrontendProperties if the JsonReader was pointing to an instance of it, or null if it was
-     * pointing to JSON null.
-     * @throws IOException If an error occurs while reading the FrontendProperties.
+     * @return An instance of FrontendUpdateProperties if the JsonReader was pointing to an instance of it, or null if
+     * it was pointing to JSON null.
+     * @throws IOException If an error occurs while reading the FrontendUpdateProperties.
      */
-    public static FrontendProperties fromJson(JsonReader jsonReader) throws IOException {
+    public static FrontendUpdateProperties fromJson(JsonReader jsonReader) throws IOException {
         return jsonReader.readObject(reader -> {
-            FrontendProperties deserializedFrontendProperties = new FrontendProperties();
+            FrontendUpdateProperties deserializedFrontendUpdateProperties = new FrontendUpdateProperties();
             while (reader.nextToken() != JsonToken.END_OBJECT) {
                 String fieldName = reader.getFieldName();
                 reader.nextToken();
 
-                if ("fqdn".equals(fieldName)) {
-                    deserializedFrontendProperties.fqdn = reader.getString();
-                } else if ("securityPolicyConfigurations".equals(fieldName)) {
-                    deserializedFrontendProperties.securityPolicyConfigurations
+                if ("securityPolicyConfigurations".equals(fieldName)) {
+                    deserializedFrontendUpdateProperties.securityPolicyConfigurations
                         = SecurityPolicyConfigurations.fromJson(reader);
-                } else if ("provisioningState".equals(fieldName)) {
-                    deserializedFrontendProperties.provisioningState = ProvisioningState.fromString(reader.getString());
                 } else {
                     reader.skipChildren();
                 }
             }
 
-            return deserializedFrontendProperties;
+            return deserializedFrontendUpdateProperties;
         });
     }
 }
