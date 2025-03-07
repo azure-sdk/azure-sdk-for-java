e1fa86836114bf27c1103285df51cca51f6aa253
SDKAuto <sdkautomation@microsoft.com>
2025-03-07 08:06:41 +0000

    CodeGen from PR 30296 in Azure/azure-rest-api-specs Merge 9a2fcc55d0a41af941acea20ec7933c4c70bb379 into af57daeaf232c2a59bf4ba6a522479c265e2002e

--- sdk/elasticsan/azure-resourcemanager-elasticsan/src/main/java/com/azure/resourcemanager/elasticsan/models/VolumeGroups.java
+++ sdk/elasticsan/azure-resourcemanager-elasticsan/src/main/java/com/azure/resourcemanager/elasticsan/models/VolumeGroups.java
@@ -29,13 +29,16 @@ public interface VolumeGroups {
      * 
      * @param resourceGroupName The name of the resource group. The name is case insensitive.
      * @param elasticSanName The name of the ElasticSan.
+     * @param xMsAccessSoftDeletedResources Optional, returns only soft deleted volume groups if set to true. If set to
+     * false or if not specified, returns only active volume groups.
      * @param context The context to associate with this operation.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
      * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
      * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
      * @return list of Volume Groups as paginated response with {@link PagedIterable}.
      */
-    PagedIterable<VolumeGroup> listByElasticSan(String resourceGroupName, String elasticSanName, Context context);
+    PagedIterable<VolumeGroup> listByElasticSan(String resourceGroupName, String elasticSanName,
+        XMsAccessSoftDeletedResources xMsAccessSoftDeletedResources, Context context);
 
     /**
      * Delete an VolumeGroup.
@@ -55,12 +58,15 @@ public interface VolumeGroups {
      * @param resourceGroupName The name of the resource group. The name is case insensitive.
      * @param elasticSanName The name of the ElasticSan.
      * @param volumeGroupName The name of the VolumeGroup.
+     * @param deleteType Optional. Specifies that the delete operation should be a permanent delete for the soft deleted
+     * volume group. The value of deleteType can only be 'permanent'.
      * @param context The context to associate with this operation.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
      * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
      * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
      */
-    void delete(String resourceGroupName, String elasticSanName, String volumeGroupName, Context context);
+    void delete(String resourceGroupName, String elasticSanName, String volumeGroupName, DeleteType deleteType,
+        Context context);
 
     /**
      * Get an VolumeGroups.
@@ -127,12 +133,14 @@ public interface VolumeGroups {
      * Delete an VolumeGroup.
      * 
      * @param id the resource ID.
+     * @param deleteType Optional. Specifies that the delete operation should be a permanent delete for the soft deleted
+     * volume group. The value of deleteType can only be 'permanent'.
      * @param context The context to associate with this operation.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
      * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
      * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
      */
-    void deleteByIdWithResponse(String id, Context context);
+    void deleteByIdWithResponse(String id, DeleteType deleteType, Context context);
 
     /**
      * Begins definition for a new VolumeGroup resource.
