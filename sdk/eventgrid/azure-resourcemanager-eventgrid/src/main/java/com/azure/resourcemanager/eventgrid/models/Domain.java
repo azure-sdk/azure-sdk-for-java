a2e5fa0f944acb1046ae6335d5e58cd3126101fb
SDKAuto <sdkautomation@microsoft.com>
2025-04-11 15:27:59 +0000

    CodeGen from PR 33889 in Azure/azure-rest-api-specs Merge a77c569c1a47e42f85fc7bcaca9cca4b17d86610 into ee80f38a67edcad70631d702a23fbad1bc0abef0

--- sdk/eventgrid/azure-resourcemanager-eventgrid/src/main/java/com/azure/resourcemanager/eventgrid/models/Domain.java
+++ sdk/eventgrid/azure-resourcemanager-eventgrid/src/main/java/com/azure/resourcemanager/eventgrid/models/Domain.java
@@ -52,6 +52,13 @@ public interface Domain {
     Map<String, String> tags();
 
     /**
+     * Gets the sku property: The Sku pricing tier for the Event Grid Domain resource.
+     * 
+     * @return the sku value.
+     */
+    ResourceSku sku();
+
+    /**
      * Gets the identity property: Identity information for the Event Grid Domain resource.
      * 
      * @return the identity value.
@@ -281,7 +288,7 @@ public interface Domain {
          * The stage of the Domain definition which contains all the minimum required properties for the resource to be
          * created, but also allows for any other optional properties to be specified.
          */
-        interface WithCreate extends DefinitionStages.WithTags, DefinitionStages.WithIdentity,
+        interface WithCreate extends DefinitionStages.WithTags, DefinitionStages.WithSku, DefinitionStages.WithIdentity,
             DefinitionStages.WithMinimumTlsVersionAllowed, DefinitionStages.WithInputSchema,
             DefinitionStages.WithEventTypeInfo, DefinitionStages.WithInputSchemaMapping,
             DefinitionStages.WithPublicNetworkAccess, DefinitionStages.WithInboundIpRules,
@@ -314,6 +321,19 @@ public interface Domain {
              * @return the next definition stage.
              */
             WithCreate withTags(Map<String, String> tags);
+        }
+
+        /**
+         * The stage of the Domain definition allowing to specify sku.
+         */
+        interface WithSku {
+            /**
+             * Specifies the sku property: The Sku pricing tier for the Event Grid Domain resource..
+             * 
+             * @param sku The Sku pricing tier for the Event Grid Domain resource.
+             * @return the next definition stage.
+             */
+            WithCreate withSku(ResourceSku sku);
         }
 
         /**
@@ -539,8 +559,9 @@ public interface Domain {
     /**
      * The template for Domain update.
      */
-    interface Update extends UpdateStages.WithTags, UpdateStages.WithIdentity, UpdateStages.WithPublicNetworkAccess,
-        UpdateStages.WithInboundIpRules, UpdateStages.WithMinimumTlsVersionAllowed, UpdateStages.WithDisableLocalAuth,
+    interface Update extends UpdateStages.WithTags, UpdateStages.WithIdentity, UpdateStages.WithSku,
+        UpdateStages.WithPublicNetworkAccess, UpdateStages.WithInboundIpRules,
+        UpdateStages.WithMinimumTlsVersionAllowed, UpdateStages.WithDisableLocalAuth,
         UpdateStages.WithAutoCreateTopicWithFirstSubscription, UpdateStages.WithAutoDeleteTopicWithLastSubscription,
         UpdateStages.WithDataResidencyBoundary, UpdateStages.WithEventTypeInfo {
         /**
@@ -587,6 +608,19 @@ public interface Domain {
              * @return the next definition stage.
              */
             Update withIdentity(IdentityInfo identity);
+        }
+
+        /**
+         * The stage of the Domain update allowing to specify sku.
+         */
+        interface WithSku {
+            /**
+             * Specifies the sku property: The Sku pricing tier for the domain..
+             * 
+             * @param sku The Sku pricing tier for the domain.
+             * @return the next definition stage.
+             */
+            Update withSku(ResourceSku sku);
         }
 
         /**
