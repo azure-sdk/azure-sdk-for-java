5d959c0d1968208e9de6ac7ccd25dff7fe30b647
SDKAuto <sdkautomation@microsoft.com>
2025-04-16 07:38:47 +0000

    CodeGen from PR 33986 in Azure/azure-rest-api-specs Merge c0d9c680f240997b7a7d0de1eaebd9986988bc1a into 4f6ba43a83ed4f80863e1fe698ac10b89e8c48a0

--- sdk/changeanalysis/azure-resourcemanager-changeanalysis/src/main/java/com/azure/resourcemanager/changeanalysis/AzureChangeAnalysisManager.java
+++ sdk/changeanalysis/azure-resourcemanager-changeanalysis/src/main/java/com/azure/resourcemanager/changeanalysis/AzureChangeAnalysisManager.java
@@ -22,6 +22,7 @@ import com.azure.core.http.policy.RetryPolicy;
 import com.azure.core.http.policy.UserAgentPolicy;
 import com.azure.core.management.profile.AzureProfile;
 import com.azure.core.util.Configuration;
+import com.azure.core.util.CoreUtils;
 import com.azure.core.util.logging.ClientLogger;
 import com.azure.resourcemanager.changeanalysis.fluent.AzureChangeAnalysisManagementClient;
 import com.azure.resourcemanager.changeanalysis.implementation.AzureChangeAnalysisManagementClientBuilder;
@@ -35,6 +36,7 @@ import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
@@ -100,6 +102,9 @@ public final class AzureChangeAnalysisManager {
      */
     public static final class Configurable {
         private static final ClientLogger LOGGER = new ClientLogger(Configurable.class);
+        private static final String SDK_VERSION = "version";
+        private static final Map<String, String> PROPERTIES
+            = CoreUtils.getProperties("azure-resourcemanager-changeanalysis.properties");
 
         private HttpClient httpClient;
         private HttpLogOptions httpLogOptions;
@@ -207,12 +212,14 @@ public final class AzureChangeAnalysisManager {
             Objects.requireNonNull(credential, "'credential' cannot be null.");
             Objects.requireNonNull(profile, "'profile' cannot be null.");
 
+            String clientVersion = PROPERTIES.getOrDefault(SDK_VERSION, "UnknownVersion");
+
             StringBuilder userAgentBuilder = new StringBuilder();
             userAgentBuilder.append("azsdk-java")
                 .append("-")
                 .append("com.azure.resourcemanager.changeanalysis")
                 .append("/")
-                .append("1.1.0");
+                .append(clientVersion);
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder.append(" (")
                     .append(Configuration.getGlobalConfiguration().get("java.version"))
