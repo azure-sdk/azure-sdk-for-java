44516e2b0d13bdb4b751445d43ec3df7191bc3fd
SDKAuto <sdkautomation@microsoft.com>
2025-04-30 17:32:19 +0000

    CodeGen from PR 34371 in Azure/azure-rest-api-specs Merge 5a991e1ddde01c1ad3021a5d06676272f2ac00f5 into 37e1a52a30c527b6ad2d7ef09a9c82ad4491e8a2

--- sdk/mariadb/azure-resourcemanager-mariadb/src/main/java/com/azure/resourcemanager/mariadb/MariaDBManager.java
+++ sdk/mariadb/azure-resourcemanager-mariadb/src/main/java/com/azure/resourcemanager/mariadb/MariaDBManager.java
@@ -22,6 +22,7 @@ import com.azure.core.http.policy.RetryPolicy;
 import com.azure.core.http.policy.UserAgentPolicy;
 import com.azure.core.management.profile.AzureProfile;
 import com.azure.core.util.Configuration;
+import com.azure.core.util.CoreUtils;
 import com.azure.core.util.logging.ClientLogger;
 import com.azure.resourcemanager.mariadb.fluent.MariaDBManagementClient;
 import com.azure.resourcemanager.mariadb.implementation.AdvisorsImpl;
@@ -77,6 +78,7 @@ import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
@@ -187,6 +189,9 @@ public final class MariaDBManager {
      */
     public static final class Configurable {
         private static final ClientLogger LOGGER = new ClientLogger(Configurable.class);
+        private static final String SDK_VERSION = "version";
+        private static final Map<String, String> PROPERTIES
+            = CoreUtils.getProperties("azure-resourcemanager-mariadb.properties");
 
         private HttpClient httpClient;
         private HttpLogOptions httpLogOptions;
@@ -294,12 +299,14 @@ public final class MariaDBManager {
             Objects.requireNonNull(credential, "'credential' cannot be null.");
             Objects.requireNonNull(profile, "'profile' cannot be null.");
 
+            String clientVersion = PROPERTIES.getOrDefault(SDK_VERSION, "UnknownVersion");
+
             StringBuilder userAgentBuilder = new StringBuilder();
             userAgentBuilder.append("azsdk-java")
                 .append("-")
                 .append("com.azure.resourcemanager.mariadb")
                 .append("/")
-                .append("1.0.0");
+                .append(clientVersion);
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder.append(" (")
                     .append(Configuration.getGlobalConfiguration().get("java.version"))
