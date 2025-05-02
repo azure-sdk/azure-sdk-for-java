68fd18dbe55a3a52ab309f050242b905d3b9f6ab
SDKAuto <sdkautomation@microsoft.com>
2025-05-02 16:17:20 +0000

    CodeGen from PR 33950 in Azure/azure-rest-api-specs Merge e7e4b3f8b451ce4eee917cb0472fb31a085a46f0 into dc00d9432a594d132e861ef2b7ee804850a7fccf

--- sdk/appcontainers/azure-resourcemanager-appcontainers/src/samples/java/com/azure/resourcemanager/appcontainers/generated/ManagedEnvironmentsStoragesCreateOrUpdateSamples.java
+++ sdk/appcontainers/azure-resourcemanager-appcontainers/src/samples/java/com/azure/resourcemanager/appcontainers/generated/ManagedEnvironmentsStoragesCreateOrUpdateSamples.java
@@ -14,7 +14,7 @@ import com.azure.resourcemanager.appcontainers.models.NfsAzureFileProperties;
  */
 public final class ManagedEnvironmentsStoragesCreateOrUpdateSamples {
     /*
-     * x-ms-original-file: specification/app/resource-manager/Microsoft.App/stable/2025-01-01/examples/
+     * x-ms-original-file: specification/app/resource-manager/Microsoft.App/preview/2025-02-02-preview/examples/
      * ManagedEnvironmentsStorages_CreateOrUpdate_NfsAzureFile.json
      */
     /**
@@ -35,7 +35,7 @@ public final class ManagedEnvironmentsStoragesCreateOrUpdateSamples {
     }
 
     /*
-     * x-ms-original-file: specification/app/resource-manager/Microsoft.App/stable/2025-01-01/examples/
+     * x-ms-original-file: specification/app/resource-manager/Microsoft.App/preview/2025-02-02-preview/examples/
      * ManagedEnvironmentsStorages_CreateOrUpdate.json
      */
     /**
